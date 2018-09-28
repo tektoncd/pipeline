ff6d5e4f15eccc1d6d8944a6715ed781037b0485
Tejal Desai <tejaldesai@google.com>
2018-09-28 10:55:21 -0700

    Running dep ensure on master (#79)
    In commit 2a03869fefe46d19ee75222451ca2faac7993b60, I ran `dep prune`
    which did not respect the [[prune.projects]] and removed
    "github.com/knative/test-infra"
    Running dep ensure on master fixes this issue.

---
+++ vendor/github.com/knative/caching/hack/update-codegen.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Copyright 2018 The Knative Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+REPO_ROOT=$(dirname ${BASH_SOURCE})/..
+CODEGEN_PKG=${CODEGEN_PKG:-$(cd ${REPO_ROOT}; ls -d -1 ./vendor/k8s.io/code-generator 2>/dev/null || echo ../code-generator)}
+
+# generate the code with:
+# --output-base    because this script should also be able to run inside the vendor dir of
+#                  k8s.io/kubernetes. The output-base is needed for the generators to output into the vendor dir
+#                  instead of the $GOPATH directly. For normal projects this can be dropped.
+${CODEGEN_PKG}/generate-groups.sh "deepcopy,client,informer,lister" \
+  github.com/knative/caching/pkg/client github.com/knative/caching/pkg/apis \
+  "caching:v1alpha1" \
+  --go-header-file ${REPO_ROOT}/hack/boilerplate/boilerplate.go.txt
+
+# Make sure our dependencies are up-to-date
+${REPO_ROOT}/hack/update-deps.sh
