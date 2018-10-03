f46c4797c0a333b242ab1f595a31ad78403c68db
Nader Ziada <nziada@pivotal.io>
2018-10-03 16:43:53 -0400

    git resource for pipeline #58  - add resource to Build as an input source

---
+++ pkg/reconciler/v1alpha1/taskrun/resources/input_resource_test.go
@@ -0,0 +1,313 @@
+/*
+Copyright 2018 The Knative Authors
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package resources
+
+import (
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+	v1alpha1 "github.com/knative/build-pipeline/pkg/apis/pipeline/v1alpha1"
+	fakeclientset "github.com/knative/build-pipeline/pkg/client/clientset/versioned/fake"
+	informers "github.com/knative/build-pipeline/pkg/client/informers/externalversions"
+	listers "github.com/knative/build-pipeline/pkg/client/listers/pipeline/v1alpha1"
+	"github.com/knative/build-pipeline/pkg/logging"
+	buildv1alpha1 "github.com/knative/build/pkg/apis/build/v1alpha1"
+	"go.uber.org/zap"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+var (
+	pipelineResourceLister listers.PipelineResourceLister
+	logger                 *zap.SugaredLogger
+)
+
+func setUp() {
+	logger, _ = logging.NewLogger("", "")
+	fakeClient := fakeclientset.NewSimpleClientset()
+	sharedInfomer := informers.NewSharedInformerFactory(fakeClient, 0)
+	pipelineResourceInformer := sharedInfomer.Pipeline().V1alpha1().PipelineResources()
+	pipelineResourceLister = pipelineResourceInformer.Lister()
+
+	res := &v1alpha1.PipelineResource{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "workspace",
+			Namespace: "marshmallow",
+		},
+		Spec: v1alpha1.PipelineResourceSpec{
+			Type: "git",
+			Params: []v1alpha1.Param{
+				v1alpha1.Param{
+					Name:  "Url",
+					Value: "https://github.com/grafeas/kritis",
+				},
+			},
+		},
+	}
+	resWithServiceAccount := &v1alpha1.PipelineResource{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "workspace-sa",
+			Namespace: "marshmallow",
+		},
+		Spec: v1alpha1.PipelineResourceSpec{
+			Type: "git",
+			Params: []v1alpha1.Param{
+				v1alpha1.Param{
+					Name:  "Url",
+					Value: "https://github.com/grafeas/kritis",
+				},
+				v1alpha1.Param{
+					Name:  "ServiceAccount",
+					Value: "kritis-service-account",
+				},
+			},
+		},
+	}
+	pipelineResourceInformer.Informer().GetIndexer().Add(res)
+	pipelineResourceInformer.Informer().GetIndexer().Add(resWithServiceAccount)
+}
+
+func TestAddResourceToBuild(t *testing.T) {
+	boolTrue := true
+
+	task := &v1alpha1.Task{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "build-from-repo",
+			Namespace: "marshmallow",
+		},
+		Spec: v1alpha1.TaskSpec{
+			Inputs: &v1alpha1.Inputs{
+				Sources: []v1alpha1.Source{
+					v1alpha1.Source{
+						Name: "workspace",
+						Type: "git",
+					},
+				},
+			},
+		},
+	}
+	taskRun := &v1alpha1.TaskRun{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "build-from-repo-run",
+			Namespace: "marshmallow",
+		},
+		Spec: v1alpha1.TaskRunSpec{
+			TaskRef: v1alpha1.TaskRef{
+				Name: "simpleTask",
+			},
+			Inputs: v1alpha1.TaskRunInputs{
+				Resources: []v1alpha1.PipelineResourceVersion{
+					v1alpha1.PipelineResourceVersion{
+						ResourceRef: v1alpha1.PipelineResourceRef{
+							Name: "workspace",
+						},
+						Version: "master",
+					},
+				},
+			},
+		},
+	}
+	build := &buildv1alpha1.Build{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "Build",
+			APIVersion: "build.knative.dev/v1alpha1"},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "build-from-repo",
+			Namespace: "marshmallow",
+			OwnerReferences: []metav1.OwnerReference{
+				{
+					APIVersion:         "pipeline.knative.dev/v1alpha1",
+					Kind:               "TaskRun",
+					Name:               "build-from-repo-run",
+					Controller:         &boolTrue,
+					BlockOwnerDeletion: &boolTrue,
+				},
+			},
+		},
+		Spec: buildv1alpha1.BuildSpec{},
+	}
+
+	for _, c := range []struct {
+		desc    string
+		task    *v1alpha1.Task
+		taskRun *v1alpha1.TaskRun
+		build   *buildv1alpha1.Build
+		wantErr bool
+		want    *buildv1alpha1.Build
+	}{{
+		desc:    "simple",
+		task:    task,
+		taskRun: taskRun,
+		build:   build,
+		wantErr: false,
+		want: &buildv1alpha1.Build{
+			TypeMeta: metav1.TypeMeta{
+				Kind:       "Build",
+				APIVersion: "build.knative.dev/v1alpha1"},
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      "build-from-repo",
+				Namespace: "marshmallow",
+				OwnerReferences: []metav1.OwnerReference{
+					{
+						APIVersion:         "pipeline.knative.dev/v1alpha1",
+						Kind:               "TaskRun",
+						Name:               "build-from-repo-run",
+						Controller:         &boolTrue,
+						BlockOwnerDeletion: &boolTrue,
+					},
+				},
+			},
+			Spec: buildv1alpha1.BuildSpec{
+				Source: &buildv1alpha1.SourceSpec{
+					Git: &buildv1alpha1.GitSourceSpec{
+						Url:      "https://github.com/grafeas/kritis",
+						Revision: "master",
+					},
+				},
+			},
+		},
+	}, {
+		desc: "set revision to default value",
+		task: task,
+		taskRun: &v1alpha1.TaskRun{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      "build-from-repo-run",
+				Namespace: "marshmallow",
+			},
+			Spec: v1alpha1.TaskRunSpec{
+				TaskRef: v1alpha1.TaskRef{
+					Name: "simpleTask",
+				},
+				Inputs: v1alpha1.TaskRunInputs{
+					Resources: []v1alpha1.PipelineResourceVersion{
+						v1alpha1.PipelineResourceVersion{
+							ResourceRef: v1alpha1.PipelineResourceRef{
+								Name: "workspace",
+							},
+						},
+					},
+				},
+			},
+		},
+		build:   build,
+		wantErr: false,
+		want: &buildv1alpha1.Build{
+			TypeMeta: metav1.TypeMeta{
+				Kind:       "Build",
+				APIVersion: "build.knative.dev/v1alpha1"},
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      "build-from-repo",
+				Namespace: "marshmallow",
+				OwnerReferences: []metav1.OwnerReference{
+					{
+						APIVersion:         "pipeline.knative.dev/v1alpha1",
+						Kind:               "TaskRun",
+						Name:               "build-from-repo-run",
+						Controller:         &boolTrue,
+						BlockOwnerDeletion: &boolTrue,
+					},
+				},
+			},
+			Spec: buildv1alpha1.BuildSpec{
+				Source: &buildv1alpha1.SourceSpec{
+					Git: &buildv1alpha1.GitSourceSpec{
+						Url:      "https://github.com/grafeas/kritis",
+						Revision: "master",
+					},
+				},
+			},
+		},
+	}, {
+		desc: "set service account if provided",
+		task: &v1alpha1.Task{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      "build-from-repo",
+				Namespace: "marshmallow",
+			},
+			Spec: v1alpha1.TaskSpec{
+				Inputs: &v1alpha1.Inputs{
+					Sources: []v1alpha1.Source{
+						v1alpha1.Source{
+							Name: "workspace-sa",
+							Type: "git",
+						},
+					},
+				},
+			},
+		},
+		taskRun: taskRun,
+		build:   build,
+		wantErr: false,
+		want: &buildv1alpha1.Build{
+			TypeMeta: metav1.TypeMeta{
+				Kind:       "Build",
+				APIVersion: "build.knative.dev/v1alpha1"},
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      "build-from-repo",
+				Namespace: "marshmallow",
+				OwnerReferences: []metav1.OwnerReference{
+					{
+						APIVersion:         "pipeline.knative.dev/v1alpha1",
+						Kind:               "TaskRun",
+						Name:               "build-from-repo-run",
+						Controller:         &boolTrue,
+						BlockOwnerDeletion: &boolTrue,
+					},
+				},
+			},
+			Spec: buildv1alpha1.BuildSpec{
+				Source: &buildv1alpha1.SourceSpec{
+					Git: &buildv1alpha1.GitSourceSpec{
+						Url:      "https://github.com/grafeas/kritis",
+						Revision: "master",
+					},
+				},
+				ServiceAccountName: "kritis-service-account",
+			},
+		},
+	}, {
+		desc: "invalid resource name",
+		task: &v1alpha1.Task{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      "build-from-repo",
+				Namespace: "marshmallow",
+			},
+			Spec: v1alpha1.TaskSpec{
+				Inputs: &v1alpha1.Inputs{
+					Sources: []v1alpha1.Source{
+						v1alpha1.Source{
+							Name: "workspace-invalid",
+							Type: "git",
+						},
+					},
+				},
+			},
+		},
+		taskRun: taskRun,
+		build:   build,
+		wantErr: true,
+		want:    nil,
+	},
+	} {
+		setUp()
+		t.Run(c.desc, func(t *testing.T) {
+			got, err := AddInputResource(c.build, c.task, c.taskRun, pipelineResourceLister, logger)
+			if (err != nil) != c.wantErr {
+				t.Errorf("Test: %q; NewControllerConfigFromConfigMap() error = %v, WantErr %v", c.desc, err, c.wantErr)
+			}
+			if d := cmp.Diff(got, c.want); d != "" {
+				t.Errorf("Diff:\n%s", d)
+			}
+		})
+	}
+}
---
+++ pkg/reconciler/v1alpha1/taskrun/resources/input_resources.go
@@ -0,0 +1,73 @@
+/*
+Copyright 2018 The Knative Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package resources
+
+import (
+	v1alpha1 "github.com/knative/build-pipeline/pkg/apis/pipeline/v1alpha1"
+	listers "github.com/knative/build-pipeline/pkg/client/listers/pipeline/v1alpha1"
+	buildv1alpha1 "github.com/knative/build/pkg/apis/build/v1alpha1"
+	"go.uber.org/zap"
+)
+
+// AddInputResource will update the input build with the input resource from the task
+func AddInputResource(
+	build *buildv1alpha1.Build,
+	task *v1alpha1.Task,
+	taskRun *v1alpha1.TaskRun,
+	pipelineResourceLister listers.PipelineResourceLister,
+	logger *zap.SugaredLogger,
+) (*buildv1alpha1.Build, error) {
+
+	var gitResource *v1alpha1.GitResource
+
+	for _, input := range task.Spec.Inputs.Sources {
+		resource, err := pipelineResourceLister.PipelineResources(task.Namespace).Get(input.Name)
+		if err != nil {
+
+			logger.Errorf("Failed to reconcile TaskRun: %q failed to Get Pipeline Resource: %q", task.Name, input.Name)
+			return nil, err
+		}
+		if resource.Spec.Type == v1alpha1.PipelineResourceTypeGit {
+			gitResource = v1alpha1.NewGitResource(resource)
+			for _, trInput := range taskRun.Spec.Inputs.Resources {
+				if trInput.ResourceRef.Name == input.Name {
+					gitResource.Revision = trInput.Version
+					break
+				}
+			}
+			break
+		}
+	}
+	// default revision to master is nothing is provided
+	if gitResource.Revision == "" {
+		gitResource.Revision = "master"
+	}
+
+	gitSourceSpec := &buildv1alpha1.GitSourceSpec{
+		Url:      gitResource.URL,
+		Revision: gitResource.Revision,
+	}
+
+	build.Spec.Source = &buildv1alpha1.SourceSpec{Git: gitSourceSpec}
+	// add service account name if available, otherwise Build will
+	// use the default service account in the namespace
+	if gitResource.ServiceAccount != "" {
+		build.Spec.ServiceAccountName = gitResource.ServiceAccount
+	}
+
+	return build, nil
+}
