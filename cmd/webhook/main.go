6eab668850c2ad4a6a7b479124244d0242c5f50c
Shash Reddy <sreddy@pivotal.io>
2018-10-11 15:20:30 -0400

    Update webhook to include TaskRun
    - Applying any taskrun object will invoke the validate function

--- cmd/webhook/main.go
+++ cmd/webhook/main.go
@@ -84,8 +84,9 @@ func main() {
 		Options: options,
 		Handlers: map[schema.GroupVersionKind]webhook.GenericCRD{
 			v1alpha1.SchemeGroupVersion.WithKind("Pipeline"):         &v1alpha1.Pipeline{},
-			v1alpha1.SchemeGroupVersion.WithKind("Task"):             &v1alpha1.Task{},
 			v1alpha1.SchemeGroupVersion.WithKind("PipelineResource"): &v1alpha1.PipelineResource{},
+			v1alpha1.SchemeGroupVersion.WithKind("Task"):             &v1alpha1.Task{},
+			v1alpha1.SchemeGroupVersion.WithKind("TaskRun"):          &v1alpha1.TaskRun{},
 		},
 		Logger: logger,
 	}
