c04f6b4cf0bceb5a00bf3878adc28f838d6906c6
Shash Reddy <sreddy@pivotal.io>
2018-10-11 14:58:34 -0400

    Add validation for taskrun CRD
    - Add tests for metadata validation
    - Add tests for taskrun validation CRD

--- pkg/apis/pipeline/v1alpha1/metadata_validation_test.go
+++ pkg/apis/pipeline/v1alpha1/metadata_validation_test.go
@@ -15,28 +15,20 @@ package v1alpha1
 
 import (
 	"strings"
+	"testing"
 
-	"github.com/knative/pkg/apis"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-const maxLength = 63
+func TestMetadataInvalidLongName(t *testing.T) {
 
-func validateObjectMetadata(meta metav1.Object) *apis.FieldError {
-	name := meta.GetName()
-
-	if strings.Contains(name, ".") {
-		return &apis.FieldError{
-			Message: "Invalid resource name: special character . must not be present",
-			Paths:   []string{"name"},
+	invalidMetas := []*metav1.ObjectMeta{
+		&metav1.ObjectMeta{Name: strings.Repeat("s", maxLength+1)},
+		&metav1.ObjectMeta{Name: "bad.name"},
+	}
+	for _, invalidMeta := range invalidMetas {
+		if err := validateObjectMetadata(invalidMeta); err == nil {
+			t.Errorf("Failed to validate object meta data: %s", err)
 		}
 	}
-
-	if len(name) > maxLength {
-		return &apis.FieldError{
-			Message: "Invalid resource name: length must be no more than 63 characters",
-			Paths:   []string{"name"},
-		}
-	}
-	return nil
 }
---
+++ pkg/apis/pipeline/v1alpha1/taskrun_validation.go
@@ -0,0 +1,139 @@
+/*
+Copyright 2018 The Knative Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1alpha1
+
+import (
+	"fmt"
+
+	"github.com/knative/pkg/apis"
+	"k8s.io/apimachinery/pkg/api/equality"
+)
+
+func (t *TaskRun) Validate() *apis.FieldError {
+	if err := validateObjectMetadata(t.GetObjectMeta()).ViaField("metadata"); err != nil {
+		return err
+	}
+	return t.Spec.Validate()
+}
+
+func (ts *TaskRunSpec) Validate() *apis.FieldError {
+	if equality.Semantic.DeepEqual(ts, &TaskRunSpec{}) {
+		return apis.ErrMissingField("spec")
+	}
+
+	// Check for TaskRef
+	if ts.TaskRef.Name == "" {
+		return apis.ErrMissingField("spec.taskref.name")
+	}
+
+	// Check for Trigger
+	if err := validateTaskTriggerType(ts.Trigger.TriggerRef, "spec.trigger.triggerref.type"); err != nil {
+		return err
+	}
+
+	// check for input resources
+	if err := checkForipelineResourceDuplicates(ts.Inputs.Resources, "spec.Inputs.Resources.Name"); err != nil {
+		return err
+	}
+	if err := validateParameters(ts.Inputs.Params); err != nil {
+		return err
+	}
+
+	// check for outputs
+	for _, source := range ts.Outputs.Resources {
+		if err := validateResourceType(source, fmt.Sprintf("spec.Outputs.Resources.%s.Type", source.Name)); err != nil {
+			return err
+		}
+		if err := checkForDuplicates(ts.Outputs.Resources, "spec.Outputs.Resources.Name"); err != nil {
+			return err
+		}
+	}
+
+	// check for results
+	if err := validateResultTarget(ts.Results.Logs, "spec.results.logs"); err != nil {
+		return err
+	}
+	if err := validateResultTarget(ts.Results.Runs, "spec.results.runs"); err != nil {
+		return err
+	}
+	if ts.Results.Tests != nil {
+		if err := validateResultTarget(*ts.Results.Tests, "spec.results.tests"); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func validateResultTarget(r ResultTarget, path string) *apis.FieldError {
+	// if result target is not set then do not error
+	var emptyTarget = ResultTarget{}
+	if r == emptyTarget {
+		return nil
+	}
+	// If set then verify all variables pass the validation
+	if r.Name == "" {
+		return apis.ErrMissingField(fmt.Sprintf("%s.name", path))
+	}
+
+	if r.Type != ResultTargetTypeGCS {
+		return apis.ErrInvalidValue(string(r.Type), fmt.Sprintf("%s.Type", path))
+	}
+
+	if r.URL == "" {
+		return apis.ErrMissingField(fmt.Sprintf("%s.URL", path))
+	}
+	return nil
+}
+
+func checkForipelineResourceDuplicates(resources []PipelineResourceVersion, path string) *apis.FieldError {
+	encountered := map[string]struct{}{}
+	for _, r := range resources {
+		// Check the unique combination of resource+version. Covers the use case of inputs with same resource name
+		// and different versions
+		key := fmt.Sprintf("%s%s", r.ResourceRef.Name, r.Version)
+		if _, ok := encountered[key]; ok {
+			return apis.ErrMultipleOneOf(path)
+		}
+		encountered[key] = struct{}{}
+	}
+	return nil
+}
+
+func validateTaskTriggerType(r TaskTriggerRef, path string) *apis.FieldError {
+	if r.Type == "" {
+		return nil
+	}
+	for _, allowed := range []TaskTriggerType{TaskTriggerTypePipelineRun, TaskTriggerTypeManual} {
+		if r.Type == allowed {
+			return nil
+		}
+	}
+	return apis.ErrInvalidValue(string(r.Type), path)
+}
+
+func validateParameters(params []Param) *apis.FieldError {
+	// Template must not duplicate parameter names.
+	seen := map[string]struct{}{}
+	for _, p := range params {
+		if _, ok := seen[p.Name]; ok {
+			return apis.ErrMultipleOneOf("spec.inputs.params")
+		}
+		seen[p.Name] = struct{}{}
+	}
+	return nil
+}
