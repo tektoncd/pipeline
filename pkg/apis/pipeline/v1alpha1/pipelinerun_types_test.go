72794af6f3c77aca1983c680b0f4d97c27b7c05d
Shash Reddy <sreddy@pivotal.io>
2018-10-19 17:18:57 -0400

    Declare gettaskrunref on pipeline object
    - Makes codes more readable
    - Add tests on pipeline run status condition and taskrun ref
    - TestPipelineRunStatusConditions tests that `SetCondition` function
       on pipeline status can be used to set any ducktype condition and
       similarly `GetCondition` function can be used to fetch any ducktype condition.
       Test includes setting multiple conditions to demonstrate that we are not limiting
       to just setting one condition even though pipeline run sets only one condition.

---
+++ pkg/apis/pipeline/v1alpha1/pipelinerun_types_test.go
@@ -0,0 +1,64 @@
+package v1alpha1
+
+import (
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+	"github.com/knative/pkg/apis"
+	duckv1alpha1 "github.com/knative/pkg/apis/duck/v1alpha1"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+func TestPipelineRunStatusConditions(t *testing.T) {
+	p := &PipelineRun{}
+	foo := &duckv1alpha1.Condition{
+		Type:   "Foo",
+		Status: "True",
+	}
+	bar := &duckv1alpha1.Condition{
+		Type:   "Bar",
+		Status: "True",
+	}
+
+	var ignoreVolatileTime = cmp.Comparer(func(_, _ apis.VolatileTime) bool {
+		return true
+	})
+
+	// Add a new condition.
+	p.Status.SetCondition(foo)
+
+	fooStatus := p.Status.GetCondition(foo.Type)
+	if d := cmp.Diff(fooStatus, foo, ignoreVolatileTime); d != "" {
+		t.Errorf("Unexpected pipeline run condition type; want %v got %v; diff %v", fooStatus, foo, d)
+	}
+
+	// Add a second condition.
+	p.Status.SetCondition(bar)
+
+	barStatus := p.Status.GetCondition(bar.Type)
+
+	if d := cmp.Diff(barStatus, bar, ignoreVolatileTime); d != "" {
+		t.Fatalf("Unexpected pipeline run condition type; want %v got %v; diff %s", barStatus, bar, d)
+	}
+}
+
+func TestPipelineRun_TaskRunref(t *testing.T) {
+	p := &PipelineRun{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "test-name",
+			Namespace: "test-ns",
+		},
+	}
+
+	expectTaskRunRef := corev1.ObjectReference{
+		APIVersion: "build-pipeline.knative.dev/v1alpha1",
+		Kind:       "TaskRun",
+		Namespace:  p.Namespace,
+		Name:       p.Name,
+	}
+
+	if d := cmp.Diff(p.GetTaskRunRef(), expectTaskRunRef); d != "" {
+		t.Fatalf("Taskrun reference mismatch; want %v got %v; diff %s", expectTaskRunRef, p.GetTaskRunRef(), d)
+	}
+}
