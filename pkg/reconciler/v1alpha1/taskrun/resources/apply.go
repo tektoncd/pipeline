7009f02d56ce99534cf625afdade47c24d9e135f
Shash Reddy <sreddy@pivotal.io>
2018-11-09 11:38:12 -0500

    Use nightly release of build
    Changes
    - Use multiple sources feature of build
    - Inline apply replacements function because in recent build refactor
    this part of code is pulled into reconciler and vendoring huge package
    for a simple doesn't seem logical anymore.

--- pkg/reconciler/v1alpha1/taskrun/resources/apply.go
+++ pkg/reconciler/v1alpha1/taskrun/resources/apply.go
@@ -18,10 +18,11 @@ package resources
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/knative/build-pipeline/pkg/apis/pipeline/v1alpha1"
 	buildv1alpha1 "github.com/knative/build/pkg/apis/build/v1alpha1"
-	"github.com/knative/build/pkg/builder"
+	corev1 "k8s.io/api/core/v1"
 )
 
 // ApplyParameters applies the params from a TaskRun.Input.Parameters to a BuildSpec.
@@ -39,7 +40,7 @@ func ApplyParameters(b *buildv1alpha1.Build, tr *v1alpha1.TaskRun, defaults ...v
 		replacements[fmt.Sprintf("inputs.params.%s", p.Name)] = p.Value
 	}
 
-	return builder.ApplyReplacements(b, replacements)
+	return ApplyReplacements(b, replacements)
 }
 
 // ResourceGetter is the interface used to retrieve resources which are references via a TaskRunResourceVersion.
@@ -66,5 +67,69 @@ func ApplyResources(b *buildv1alpha1.Build, resources []v1alpha1.TaskRunResource
 			replacements[fmt.Sprintf("%s.resources.%s.%s", replacementStr, r.Name, k)] = v
 		}
 	}
-	return builder.ApplyReplacements(b, replacements), nil
+	return ApplyReplacements(b, replacements), nil
+}
+
+// ApplyReplacements replaces placeholders for declared parameters with the specified replacements.
+func ApplyReplacements(build *buildv1alpha1.Build, replacements map[string]string) *buildv1alpha1.Build {
+	build = build.DeepCopy()
+
+	applyReplacements := func(in string) string {
+		for k, v := range replacements {
+			in = strings.Replace(in, fmt.Sprintf("${%s}", k), v, -1)
+		}
+		return in
+	}
+
+	// Apply variable expansion to steps fields.
+	steps := build.Spec.Steps
+	for i := range steps {
+		steps[i].Name = applyReplacements(steps[i].Name)
+		steps[i].Image = applyReplacements(steps[i].Image)
+		for ia, a := range steps[i].Args {
+			steps[i].Args[ia] = applyReplacements(a)
+		}
+		for ie, e := range steps[i].Env {
+			steps[i].Env[ie].Value = applyReplacements(e.Value)
+		}
+		steps[i].WorkingDir = applyReplacements(steps[i].WorkingDir)
+		for ic, c := range steps[i].Command {
+			steps[i].Command[ic] = applyReplacements(c)
+		}
+		for iv, v := range steps[i].VolumeMounts {
+			steps[i].VolumeMounts[iv].Name = applyReplacements(v.Name)
+			steps[i].VolumeMounts[iv].MountPath = applyReplacements(v.MountPath)
+			steps[i].VolumeMounts[iv].SubPath = applyReplacements(v.SubPath)
+		}
+	}
+
+	if buildTmpl := build.Spec.Template; buildTmpl != nil && len(buildTmpl.Env) > 0 {
+		// Apply variable expansion to the build's overridden
+		// environment variables
+		for i, e := range buildTmpl.Env {
+			buildTmpl.Env[i].Value = applyReplacements(e.Value)
+		}
+
+		for i := range steps {
+			steps[i].Env = applyEnvOverride(steps[i].Env, buildTmpl.Env)
+		}
+	}
+	return build
+}
+
+func applyEnvOverride(src, override []corev1.EnvVar) []corev1.EnvVar {
+	result := make([]corev1.EnvVar, 0, len(src)+len(override))
+	overrides := make(map[string]bool)
+
+	for _, env := range override {
+		overrides[env.Name] = true
+	}
+
+	for _, env := range src {
+		if _, present := overrides[env.Name]; !present {
+			result = append(result, env)
+		}
+	}
+
+	return append(result, override...)
 }
