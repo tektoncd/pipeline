7a43741cb3033b059e4c7f0c7886051d3b397c1c
liuwei <liuweixa@cn.ibm.com>
2019-03-20 15:45:36 +0800

    export SOURCE_DATE_EPOCH env variable to avoid wrong image created time

--- hack/release.sh
+++ hack/release.sh
@@ -14,6 +14,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+export SOURCE_DATE_EPOCH=`date +%s`
+
 source $(dirname $0)/../vendor/github.com/knative/test-infra/scripts/release.sh
 
 # Local generated yaml file
