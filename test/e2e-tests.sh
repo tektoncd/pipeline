b2968d77c921f37073245ec02d70658239ae3647
Christie Wilson <bobcatfish@gmail.com>
2019-04-17 15:37:33 -0700

    Can I reproduce it with just the timeout test alone? 🎵
    One is the loneliest number
    One is the loneliest number
    One is the loneliest number that you'll ever do
    One is the loneliest
    One is the loneliest
    One is the loneliest number that you'll ever do

--- test/e2e-tests.sh
+++ test/e2e-tests.sh
@@ -31,12 +31,7 @@ failed=0
 
 # Run the integration tests
 header "Running Go e2e tests"
-go_test_e2e -timeout=20m ./test || failed=1
-
-# Run these _after_ the integration tests b/c they don't quite work all the way
-# and they cause a lot of noise in the logs, making it harder to debug integration
-# test failures.
-${REPO_ROOT_DIR}/test/e2e-tests-yaml.sh --run-tests || failed=1
+go_test_e2e -timeout=20m ./test  -run=TestTaskRunTimeout || failed=1
 
 (( failed )) && fail_test
 success
