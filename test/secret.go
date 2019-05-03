3800632df5501a20660244228a77b2617fc5e673
Scott <sbws@google.com>
2019-05-03 13:18:47 -0400

    Storage end to end test can run without env var credentials ✨
    The storage end to end test required that an environment variable be
    configured that points to json credentials on disk which can be used to
    pull from a private GCS bucket. These are not actually required to run
    the test if the defualt credentials in the namespace have the
    appropriate permissions - which is the case when the test runs in CI via
    Prow!
    
    With this change, the test will now be run in CI. This partially
    addresses #821, but TestStorageBucketPipelineRun still requires this env
    var to run.
    
    Also removed the `read-secret-env` step from the storage test. If the
    credential isn't present, the previous step in the Task would fail. Also
    nothing is verifying the output of this step. (Maybe this was for
    debugging?)
    
    Co-authored-by: Christie Wilson <bobcatfish@gmail.com>

---
+++ test/secret.go
@@ -0,0 +1,60 @@
+// +build e2e
+
+/*
+Copyright 2019 Tekton Authors LLC
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package test
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"testing"
+
+	knativetest "github.com/knative/pkg/test"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// CreateGCPServiceAccountSecret will create a kube secret called secretName in namespace
+// from the value in the GCP_SERVICE_ACCOUNT_KEY_PATH environment variable. If the env var
+// doesn't exist, no secret will be created. Returns true if the secret was created, false
+// otherwise.
+func CreateGCPServiceAccountSecret(t *testing.T, c *knativetest.KubeClient, namespace string, secretName string) (bool, error) {
+	t.Helper()
+	file := os.Getenv("GCP_SERVICE_ACCOUNT_KEY_PATH")
+	if file == "" {
+		t.Logf("Not creating service account secret, relying on default credentials in namespace %s.", namespace)
+		return false, nil
+	}
+
+	sec := &corev1.Secret{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      secretName,
+			Namespace: namespace,
+		},
+	}
+
+	bs, err := ioutil.ReadFile(file)
+	if err != nil {
+		return false, fmt.Errorf("couldn't read secret json from %s: %v", file, err)
+	}
+
+	sec.Data = map[string][]byte{
+		"config.json": bs,
+	}
+	_, err = c.Kube.CoreV1().Secrets(namespace).Create(sec)
+
+	t.Log("Creating service account secret")
+	return true, err
+}
