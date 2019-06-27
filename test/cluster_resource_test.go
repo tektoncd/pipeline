80d447216b9d391c9badc9b1aff65e72a13f4a69
Vincent Demeester <vdemeest@redhat.com>
2019-06-27 17:30:34 +0200

    Update copyright headers from Knative to Tekton
    Signed-off-by: Vincent Demeester <vdemeest@redhat.com>

--- test/cluster_resource_test.go
+++ test/cluster_resource_test.go
@@ -1,7 +1,7 @@
 // +build e2e
 
 /*
-Copyright 2018 Knative Authors LLC
+Copyright 2019 Tekton Authors LLC
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
