10f4ded1a3a83dc32233956b630833147f9baece
Vincent Demeester <vdemeest@redhat.com>
2019-07-03 10:25:45 +0200

    Add hack/* scripts to verify vendored and generated files
    This adds 3 scripts :
    
    - `./hack/update-codegen.sh` to update generated code (if any — right
      now it's almost a no-op as it only calls `update-deps.sh`
    - `./hack/update-deps.sh` to update the `vendor` folder based on the
      go module files (`go.mod`)
    - `./hack/verify-codegen.sh` that runs on the CI, to make sure the
      vendor folder and generated files are up-to-date in pull requests.
    
    Signed-off-by: Vincent Demeester <vdemeest@redhat.com>

---
+++ hack/update-codegen.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+# Copyright 2019 The Tekton Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+source $(dirname $0)/../vendor/github.com/tektoncd/plumbing/scripts/library.sh
+
+CODEGEN_PKG=${CODEGEN_PKG:-$(cd ${REPO_ROOT_DIR}; ls -d -1 ./vendor/k8s.io/code-generator 2>/dev/null || echo ../../../k8s.io/code-generator)}
+
+# Put generation commands here if we ever have some
+
+# Make sure our dependencies are up-to-date
+${REPO_ROOT_DIR}/hack/update-deps.sh
