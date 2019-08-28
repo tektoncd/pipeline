375950b56f91051b103dd426d1a37f6caf65749d
16yuki0702 <hs19870702@gmail.com>
2019-08-28 12:48:42 +0900

    Add test patterns

--- pkg/cmd/pipeline/delete_test.go
+++ pkg/cmd/pipeline/delete_test.go
@@ -15,6 +15,8 @@
 package pipeline
 
 import (
+	"io"
+	"strings"
 	"testing"
 	"time"
 
@@ -26,34 +28,93 @@ import (
 	tb "github.com/tektoncd/pipeline/test/builder"
 )
 
-func TestPipelineDelete_Empty(t *testing.T) {
-	cs, _ := test.SeedTestData(t, pipelinetest.Data{})
-	p := &test.Params{Tekton: cs.Pipeline}
-
-	pipeline := Command(p)
-	_, err := test.ExecuteCommand(pipeline, "rm", "bar")
-	if err == nil {
-		t.Errorf("Error expected here")
-	}
-	expected := "Failed to delete pipeline \"bar\": pipelines.tekton.dev \"bar\" not found"
-	test.AssertOutput(t, expected, err.Error())
-}
-
-func TestPipelineDelete_WithParams(t *testing.T) {
+func TestPipelineDelete(t *testing.T) {
 	clock := clockwork.NewFakeClock()
 
-	cs, _ := test.SeedTestData(t, pipelinetest.Data{
-		Pipelines: []*v1alpha1.Pipeline{
-			tb.Pipeline("pipeline", "ns",
-				// created  5 minutes back
-				cb.PipelineCreationTimestamp(clock.Now().Add(-5*time.Minute)),
-			),
-		},
-	})
+	seeds := make([]pipelinetest.Clients, 0)
+	for i := 0; i < 3; i++ {
+		cs, _ := test.SeedTestData(t, pipelinetest.Data{
+			Pipelines: []*v1alpha1.Pipeline{
+				tb.Pipeline("pipeline", "ns",
+					// created  5 minutes back
+					cb.PipelineCreationTimestamp(clock.Now().Add(-5*time.Minute)),
+				),
+			},
+		})
+		seeds = append(seeds, cs)
+	}
 
-	p := &test.Params{Tekton: cs.Pipeline}
-	pipeline := Command(p)
-	out, _ := test.ExecuteCommand(pipeline, "rm", "pipeline", "-n", "ns", "-f")
-	expected := "Pipeline deleted: pipeline\n"
-	test.AssertOutput(t, expected, out)
+	testParams := []struct {
+		name        string
+		command     []string
+		input       pipelinetest.Clients
+		inputStream io.Reader
+		wantError   bool
+		want        string
+	}{
+		{
+			name:        "With force delete flag (shorthand)",
+			command:     []string{"rm", "pipeline", "-n", "ns", "-f"},
+			input:       seeds[0],
+			inputStream: nil,
+			wantError:   false,
+			want:        "Pipeline deleted: pipeline\n",
+		},
+		{
+			name:        "With force delete flag",
+			command:     []string{"rm", "pipeline", "-n", "ns", "--force"},
+			input:       seeds[1],
+			inputStream: nil,
+			wantError:   false,
+			want:        "Pipeline deleted: pipeline\n",
+		},
+		{
+			name:        "Without force delete flag, reply no",
+			command:     []string{"rm", "pipeline", "-n", "ns"},
+			input:       seeds[2],
+			inputStream: strings.NewReader("n"),
+			wantError:   true,
+			want:        "Canceled deleting pipeline \"pipeline\"",
+		},
+		{
+			name:        "Without force delete flag, reply yes",
+			command:     []string{"rm", "pipeline", "-n", "ns"},
+			input:       seeds[2],
+			inputStream: strings.NewReader("y"),
+			wantError:   false,
+			want:        "Make sure you really want to delete pipeline \"pipeline\" (y/n): Pipeline deleted: pipeline\n",
+		},
+		{
+			name:        "Remove non existent resource",
+			command:     []string{"rm", "nonexistent", "-n", "ns"},
+			input:       seeds[2],
+			inputStream: strings.NewReader("y"),
+			wantError:   true,
+			want:        "Failed to delete pipeline \"nonexistent\": pipelines.tekton.dev \"nonexistent\" not found",
+		},
+	}
+
+	for _, tp := range testParams {
+		t.Run(tp.name, func(t *testing.T) {
+			p := &test.Params{Tekton: tp.input.Pipeline}
+			pipeline := Command(p)
+
+			if tp.inputStream != nil {
+				pipeline.SetIn(tp.inputStream)
+			}
+
+			out, err := test.ExecuteCommand(pipeline, tp.command...)
+			if tp.wantError {
+				if err == nil {
+					t.Errorf("Error expected here")
+				}
+				test.AssertOutput(t, tp.want, err.Error())
+			} else {
+				if err != nil {
+					t.Errorf("Unexpected Error")
+				}
+				test.AssertOutput(t, tp.want, out)
+			}
+		})
+	}
 }
