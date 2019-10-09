a6663fb3042713b5543a5fbb9e1ddc3208a85e66
jbarrick@mesosphere.com <jbarrick@mesosphere.com>
2019-10-09 14:11:42 -0700

    run gofmt

--- pkg/artifacts/artifact_storage_test.go
+++ pkg/artifacts/artifact_storage_test.go
@@ -311,7 +311,7 @@ func TestInitializeArtifactStorageWithConfigMap(t *testing.T) {
 		},
 		pipelinerun: pipelinerun,
 		expectedArtifactStorage: &v1alpha1.ArtifactBucket{
-			Location: "s3://fake-bucket",
+			Location:      "s3://fake-bucket",
 			BashNoopImage: "override-with-bash-noop:latest",
 			GsutilImage:   "override-with-gsutil-image:latest",
 			Secrets: []v1alpha1.SecretParam{{
