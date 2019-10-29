c8dd3b9a9339c6c4967370fec9627f8e0721cb36
Chmouel Boudjnah <chmouel@redhat.com>
2019-10-29 16:17:18 +0100

    Allow specify a specific release.yaml when setting up pipelines
    Signed-off-by: Chmouel Boudjnah <chmouel@redhat.com>

--- test/e2e-common.sh
+++ test/e2e-common.sh
@@ -16,6 +16,10 @@
 
 # Helper functions for E2E tests.
 
+# Check if we have a specific RELEASE_YAML global environment variable to use
+# instead of detecting the latest released one from tektoncd/pipeline releases
+RELEASE_YAML=${RELEASE_YAML:-}
+
 source $(dirname $0)/../vendor/github.com/tektoncd/plumbing/scripts/e2e-tests.sh
 
 function teardown() {
@@ -123,8 +127,13 @@ function run_yaml_tests() {
 }
 
 function install_pipeline_crd() {
+  local latestreleaseyaml
   echo ">> Deploying Tekton Pipelines"
-  local latestreleaseyaml=$(curl -s https://api.github.com/repos/tektoncd/pipeline/releases|python -c "import sys, json;x=json.load(sys.stdin);ass=x[0]['assets'];print([ x['browser_download_url'] for x in ass if x['name'] == 'release.yaml'][0])")
+  if [[ -n ${RELEASE_YAML} ]];then
+	latestreleaseyaml=${RELEASE_YAML}
+  else
+    latestreleaseyaml=$(curl -s https://api.github.com/repos/tektoncd/pipeline/releases|python -c "import sys, json;x=json.load(sys.stdin);ass=x[0]['assets'];print([ x['browser_download_url'] for x in ass if x['name'] == 'release.yaml'][0])")
+  fi
   [[ -z ${latestreleaseyaml} ]] && fail_test "Could not get latest released release.yaml"
   kubectl apply -f ${latestreleaseyaml} ||
     fail_test "Build pipeline installation failed"
