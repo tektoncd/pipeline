53ef439d0245db95205c077c74e6f47c6e268bf5
Jason Hall <jasonhall@google.com>
2019-12-04 14:04:08 -0500

    Move image data extraction into entrypoint_lookup.go
    Image data extraction (pulling entrypoint and digest) isn't specific to
    the real implementation of fetching image metadata using K8s
    imagePullSecrets, so having this only implemented and tested in that
    context didn't make sense.
    
    This change also adds cache population (Set) moved into the
    EntrypointCache interface, rather than having it be a side effect of
    Get. This means that entrypoint_lookup.go must call cache.Set after it
    extracts digest.
    
    The result is a cleaner separation of concerns between generic
    entrypoint lookup logic and K8s-specific implementation, which is now
    more narrowly scoped.

--- pkg/pod/entrypoint_lookup.go
+++ pkg/pod/entrypoint_lookup.go
@@ -17,14 +17,23 @@ limitations under the License.
 package pod
 
 import (
+	"fmt"
+
 	"github.com/google/go-containerregistry/pkg/name"
+	v1 "github.com/google/go-containerregistry/pkg/v1"
 	corev1 "k8s.io/api/core/v1"
 )
 
 // EntrypointCache looks up an image's entrypoint (command) in a container
 // image registry, possibly using the given service account's credentials.
 type EntrypointCache interface {
-	Get(imageName, namespace, serviceAccountName string) (cmd []string, d name.Digest, err error)
+	// Get the Image data for the given image reference. If the value is
+	// not found in the cache, it will be fetched from the image registry,
+	// possibly using K8s service account imagePullSecrets.
+	Get(ref name.Reference, namespace, serviceAccountName string) (v1.Image, error)
+	// Update the cache with a new digest->Image mapping. This will avoid a
+	// remote registry lookup next time Get is called.
+	Set(digest name.Digest, img v1.Image)
 }
 
 // resolveEntrypoints looks up container image ENTRYPOINTs for all steps that
@@ -33,38 +42,70 @@ type EntrypointCache interface {
 // Images that are not specified by digest will be specified by digest after
 // lookup in the resulting list of containers.
 func resolveEntrypoints(cache EntrypointCache, namespace, serviceAccountName string, steps []corev1.Container) ([]corev1.Container, error) {
-	// Keep a local cache of image->digest lookups, just for the scope of
-	// resolving this set of steps. If the image is pushed to, we need to
-	// resolve its digest and entrypoint again, but we can skip lookups
-	// while resolving the same TaskRun.
-	type result struct {
-		digest name.Digest
-		ep     []string
-	}
-	localCache := map[string]result{}
+	// Keep a local cache of name->image lookups, just for the scope of
+	// resolving this set of steps. If the image is pushed to before the
+	// next run, we need to resolve its digest and entrypoint again, but we
+	// can skip lookups while resolving the same TaskRun.
+	localCache := map[name.Reference]v1.Image{}
 	for i, s := range steps {
 		if len(s.Command) != 0 {
 			// Nothing to resolve.
 			continue
 		}
 
-		var digest name.Digest
-		var ep []string
-		var err error
-		if r, found := localCache[s.Image]; found {
-			digest = r.digest
-			ep = r.ep
+		origRef, err := name.ParseReference(s.Image, name.WeakValidation)
+		if err != nil {
+			return nil, err
+		}
+		var img v1.Image
+		if cimg, found := localCache[origRef]; found {
+			img = cimg
 		} else {
-			// Look it up in the cache, which will also resolve the digest.
-			ep, digest, err = cache.Get(s.Image, namespace, serviceAccountName)
+			// Look it up in the cache. If it's not found in the
+			// cache, it will be resolved from the registry.
+			img, err = cache.Get(origRef, namespace, serviceAccountName)
 			if err != nil {
 				return nil, err
 			}
-			localCache[s.Image] = result{digest, ep} // Cache it locally in case another step specifies the same image.
+			// Cache it locally in case another step specifies the same image.
+			localCache[origRef] = img
 		}
+
+		ep, digest, err := imageData(origRef, img)
+		if err != nil {
+			return nil, err
+		}
+
+		cache.Set(digest, img) // Cache the lookup for next time this image is looked up by digest.
 
 		steps[i].Image = digest.String() // Specify image by digest, since we know it now.
 		steps[i].Command = ep            // Specify the command explicitly.
 	}
 	return steps, nil
+}
+
+// imageData pulls the entrypoint from the image, and returns the given
+// original reference, with image digest resolved.
+func imageData(ref name.Reference, img v1.Image) ([]string, name.Digest, error) {
+	digest, err := img.Digest()
+	if err != nil {
+		return nil, name.Digest{}, fmt.Errorf("Error getting image digest: %v", err)
+	}
+	cfg, err := img.ConfigFile()
+	if err != nil {
+		return nil, name.Digest{}, fmt.Errorf("Error getting image config: %v", err)
+	}
+
+	// Entrypoint can be specified in either .Config.Entrypoint or
+	// .Config.Cmd.
+	ep := cfg.Config.Entrypoint
+	if len(ep) == 0 {
+		ep = cfg.Config.Cmd
+	}
+
+	d, err := name.NewDigest(ref.Context().String()+"@"+digest.String(), name.WeakValidation)
+	if err != nil {
+		return nil, name.Digest{}, fmt.Errorf("Error constructing resulting digest: %v", err)
+	}
+	return ep, d, nil
 }
