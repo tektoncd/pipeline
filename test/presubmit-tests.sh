c1574bd745ee8d169d11225f23644c61b6fcd81c
Vincent Demeester <vdemeest@redhat.com>
2019-12-12 16:55:04 +0100

    (Temporarly) disable markdown link check 😅
    At least we are sure 😜
    
    Signed-off-by: Vincent Demeester <vdemeest@redhat.com>

--- test/presubmit-tests.sh
+++ test/presubmit-tests.sh
@@ -24,6 +24,7 @@
 # Markdown linting failures don't show up properly in Gubernator resulting
 # in a net-negative contributor experience.
 export DISABLE_MD_LINTING=1
+export DISABLE_MD_LINK_CHECK=1
 
 source $(dirname $0)/../vendor/github.com/tektoncd/plumbing/scripts/presubmit-tests.sh
 
