c5b6f57e28930a527502b6f9f212265bde7c7588
Vincent Demeester <vdemeest@redhat.com>
2020-01-10 10:14:11 +0100

    De-duplicate Workspace* between v1alpha1 and v1alpha2 🦆
    Signed-off-by: Vincent Demeester <vdemeest@redhat.com>

--- pkg/apis/pipeline/v1alpha1/workspace_types.go
+++ pkg/apis/pipeline/v1alpha1/workspace_types.go
@@ -17,52 +17,12 @@ limitations under the License.
 package v1alpha1
 
 import (
-	"path/filepath"
-
-	"github.com/tektoncd/pipeline/pkg/apis/pipeline"
-	corev1 "k8s.io/api/core/v1"
+	"github.com/tektoncd/pipeline/pkg/apis/pipeline/v1alpha2"
 )
 
 // WorkspaceDeclaration is a declaration of a volume that a Task requires.
-type WorkspaceDeclaration struct {
-	// Name is the name by which you can bind the volume at runtime.
-	Name string `json:"name"`
-	// Description is an optional human readable description of this volume.
-	// +optional
-	Description string `json:"description,omitempty"`
-	// MountPath overrides the directory that the volume will be made available at.
-	// +optional
-	MountPath string `json:"mountPath,omitempty"`
-	// ReadOnly dictates whether a mounted volume is writable. By default this
-	// field is false and so mounted volumes are writable.
-	ReadOnly bool `json:"readOnly,omitempty"`
-}
-
-// GetMountPath returns the mountPath for w which is the MountPath if provided or the
-// default if not.
-func (w *WorkspaceDeclaration) GetMountPath() string {
-	if w.MountPath != "" {
-		return w.MountPath
-	}
-	return filepath.Join(pipeline.WorkspaceDir, w.Name)
-}
+type WorkspaceDeclaration = v1alpha2.WorkspaceDeclaration
 
 // WorkspaceBinding maps a Task's declared workspace to a Volume.
 // Currently we only support PersistentVolumeClaims and EmptyDir.
-type WorkspaceBinding struct {
-	// Name is the name of the workspace populated by the volume.
-	Name string `json:"name"`
-	// SubPath is optionally a directory on the volume which should be used
-	// for this binding (i.e. the volume will be mounted at this sub directory).
-	// +optional
-	SubPath string `json:"subPath,omitempty"`
-	// PersistentVolumeClaimVolumeSource represents a reference to a
-	// PersistentVolumeClaim in the same namespace. Either this OR EmptyDir can be used.
-	// +optional
-	PersistentVolumeClaim *corev1.PersistentVolumeClaimVolumeSource `json:"persistentVolumeClaim,omitempty"`
-	// EmptyDir represents a temporary directory that shares a Task's lifetime.
-	// More info: https://kubernetes.io/docs/concepts/storage/volumes#emptydir
-	// Either this OR PersistentVolumeClaim can be used.
-	// +optional
-	EmptyDir *corev1.EmptyDirVolumeSource `json:"emptyDir,omitempty"`
-}
+type WorkspaceBinding = v1alpha2.WorkspaceBinding
