fd385fab753ea3d09c6ce469c5d92995896fc4a1
Vincent Demeester <vdemeest@redhat.com>
2020-01-10 10:14:11 +0100

    De-duplicate Workspace* between v1alpha1 and v1alpha2 🦆
    Signed-off-by: Vincent Demeester <vdemeest@redhat.com>

--- pkg/apis/pipeline/v1alpha1/workspace_validation.go
+++ pkg/apis/pipeline/v1alpha1/workspace_validation.go
@@ -15,35 +15,3 @@ limitations under the License.
 */
 
 package v1alpha1
-
-import (
-	"context"
-
-	"k8s.io/apimachinery/pkg/api/equality"
-	"knative.dev/pkg/apis"
-)
-
-// Validate looks at the Volume provided in wb and makes sure that it is valid.
-// This means that only one VolumeSource can be specified, and also that the
-// supported VolumeSource is itself valid.
-func (b *WorkspaceBinding) Validate(ctx context.Context) *apis.FieldError {
-	if equality.Semantic.DeepEqual(b, &WorkspaceBinding{}) || b == nil {
-		return apis.ErrMissingField(apis.CurrentField)
-	}
-
-	// Users should only provide one supported VolumeSource.
-	if b.PersistentVolumeClaim != nil && b.EmptyDir != nil {
-		return apis.ErrMultipleOneOf("workspace.persistentvolumeclaim", "workspace.emptydir")
-	}
-
-	// Users must provide at least one supported VolumeSource.
-	if b.PersistentVolumeClaim == nil && b.EmptyDir == nil {
-		return apis.ErrMissingOneOf("workspace.persistentvolumeclaim", "workspace.emptydir")
-	}
-
-	// For a PersistentVolumeClaim to work, you must at least provide the name of the PVC to use.
-	if b.PersistentVolumeClaim != nil && b.PersistentVolumeClaim.ClaimName == "" {
-		return apis.ErrMissingField("workspace.persistentvolumeclaim.claimname")
-	}
-	return nil
-}
