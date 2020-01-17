9d6e44f495df6759615f1ce385a480180c5884bd
Sunil Thaha <sthaha@redhat.com>
2020-01-17 10:43:27 +1000

    CI: Fix shell expansion when creating resources
    Previously, `create_resource` function in test/e2e-common.sh would
    fail to apply yaml files if the root directory (for some reason)
    contained a yaml file as
    
    ```
      find ${REPO_ROOT_DIR}/examples/${resource}s/ -name *.yaml
    ```
    will expand to `find ${REPO_ROOT_DIR}/examples/${resource}s/ -name <filenames...>.yaml
    This is fixed by properly quoting to prevent shell expansion.
    
    Signed-off-by: Sunil Thaha <sthaha@redhat.com>

--- test/e2e-common.sh
+++ test/e2e-common.sh
@@ -93,8 +93,9 @@ function create_resources() {
   local resource=$1
   echo ">> Creating resources ${resource}"
 
-  # Applying the resources, either *taskruns or * *pipelineruns
-  for file in $(find ${REPO_ROOT_DIR}/examples/${resource}s/ -name *.yaml -not -path "${REPO_ROOT_DIR}/examples/${resource}s/no-ci/*" | sort); do
+  # Applying the resources, either *taskruns or * *pipelineruns except those
+  # in the no-ci directory
+  for file in $(find ${REPO_ROOT_DIR}/examples/${resource}s/ -name '*.yaml' -not -path '*/no-ci/*' | sort); do
     perl -p -e 's/gcr.io\/christiewilson-catfactory/$ENV{KO_DOCKER_REPO}/g' ${file} | ko create -f - || return 1
   done
 }
