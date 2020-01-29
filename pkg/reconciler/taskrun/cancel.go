7e9034d1c1f88fb7289aef144502fbd2ba49551b
Vincent Demeester <vdemeest@redhat.com>
2020-01-29 14:15:17 +0100

    Revert "Don't rely on .status.podName to find Pod associated with a TaskRun"
    This reverts commit 0f20c3539f25ede46dfe58b83924e28db1fd783e.

--- pkg/reconciler/taskrun/cancel.go
+++ pkg/reconciler/taskrun/cancel.go
@@ -26,8 +26,14 @@ import (
 	"knative.dev/pkg/apis"
 )
 
-// cancelTaskRun marks the TaskRun as cancelled and deletes pods linked to it.
-func cancelTaskRun(tr *v1alpha1.TaskRun, clientset kubernetes.Interface) error {
+type logger interface {
+	Warn(args ...interface{})
+	Warnf(template string, args ...interface{})
+}
+
+// cancelTaskRun marks the TaskRun as cancelled and delete pods linked to it.
+func cancelTaskRun(tr *v1alpha1.TaskRun, clientSet kubernetes.Interface, logger logger) error {
+	logger.Warn("task run %q has been cancelled", tr.Name)
 	tr.Status.SetCondition(&apis.Condition{
 		Type:    apis.ConditionSucceeded,
 		Status:  corev1.ConditionFalse,
@@ -35,10 +41,13 @@ func cancelTaskRun(tr *v1alpha1.TaskRun, clientset kubernetes.Interface) error {
 		Message: fmt.Sprintf("TaskRun %q was cancelled", tr.Name),
 	})
 
-	pod, err := getPod(tr, clientset)
-	if err != nil {
-		return err
+	if tr.Status.PodName == "" {
+		logger.Warnf("task run %q has no pod running yet", tr.Name)
+		return nil
 	}
 
-	return clientset.CoreV1().Pods(tr.Namespace).Delete(pod.Name, &metav1.DeleteOptions{})
+	if err := clientSet.CoreV1().Pods(tr.Namespace).Delete(tr.Status.PodName, &metav1.DeleteOptions{}); err != nil {
+		return err
+	}
+	return nil
 }
