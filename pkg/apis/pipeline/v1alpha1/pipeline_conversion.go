5ed254774a32ed0c649acc392514b931b2aa9dfb
Vincent Demeester <vdemeest@redhat.com>
2020-02-06 10:00:55 +0100

    Pipeline auto-conversion from v1alpha1 to v1alpha2 🎋
    This adds auto-conversion methods and tests for Pipeline types in v1alpha1
    and v1alpha2.
    
    Signed-off-by: Vincent Demeester <vdemeest@redhat.com>

--- pkg/apis/pipeline/v1alpha1/pipeline_conversion.go
+++ pkg/apis/pipeline/v1alpha1/pipeline_conversion.go
@@ -31,10 +31,43 @@ func (source *Pipeline) ConvertUp(ctx context.Context, obj apis.Convertible) err
 	switch sink := obj.(type) {
 	case *v1alpha2.Pipeline:
 		sink.ObjectMeta = source.ObjectMeta
-		return nil // source.Spec.ConvertUp(ctx, &sink.Spec)
+		return source.Spec.ConvertUp(ctx, &sink.Spec)
 	default:
 		return fmt.Errorf("unknown version, got: %T", sink)
 	}
+}
+
+func (source *PipelineSpec) ConvertUp(ctx context.Context, sink *v1alpha2.PipelineSpec) error {
+	sink.Resources = source.Resources
+	sink.Params = source.Params
+	sink.Workspaces = source.Workspaces
+	if len(source.Tasks) > 0 {
+		sink.Tasks = make([]v1alpha2.PipelineTask, len(source.Tasks))
+		for i := range source.Tasks {
+			if err := source.Tasks[i].ConvertUp(ctx, &sink.Tasks[i]); err != nil {
+				return err
+			}
+		}
+	}
+	return nil
+}
+
+func (source *PipelineTask) ConvertUp(ctx context.Context, sink *v1alpha2.PipelineTask) error {
+	sink.Name = source.Name
+	sink.TaskRef = source.TaskRef
+	if source.TaskSpec != nil {
+		sink.TaskSpec = &v1alpha2.TaskSpec{}
+		if err := source.TaskSpec.ConvertUp(ctx, sink.TaskSpec); err != nil {
+			return err
+		}
+	}
+	sink.Conditions = source.Conditions
+	sink.Retries = source.Retries
+	sink.RunAfter = source.RunAfter
+	sink.Resources = source.Resources
+	sink.Params = source.Params
+	sink.Workspaces = source.Workspaces
+	return nil
 }
 
 // ConvertDown implements api.Convertible
@@ -42,8 +75,41 @@ func (sink *Pipeline) ConvertDown(ctx context.Context, obj apis.Convertible) err
 	switch source := obj.(type) {
 	case *v1alpha2.Pipeline:
 		sink.ObjectMeta = source.ObjectMeta
-		return nil // sink.Spec.ConvertDown(ctx, &source.Spec)
+		return sink.Spec.ConvertDown(ctx, source.Spec)
 	default:
 		return fmt.Errorf("unknown version, got: %T", sink)
 	}
+}
+
+func (sink *PipelineSpec) ConvertDown(ctx context.Context, source v1alpha2.PipelineSpec) error {
+	sink.Resources = source.Resources
+	sink.Params = source.Params
+	sink.Workspaces = source.Workspaces
+	if len(source.Tasks) > 0 {
+		sink.Tasks = make([]PipelineTask, len(source.Tasks))
+		for i := range source.Tasks {
+			if err := sink.Tasks[i].ConvertDown(ctx, source.Tasks[i]); err != nil {
+				return err
+			}
+		}
+	}
+	return nil
+}
+
+func (sink *PipelineTask) ConvertDown(ctx context.Context, source v1alpha2.PipelineTask) error {
+	sink.Name = source.Name
+	sink.TaskRef = source.TaskRef
+	if source.TaskSpec != nil {
+		sink.TaskSpec = &TaskSpec{}
+		if err := sink.TaskSpec.ConvertDown(ctx, source.TaskSpec); err != nil {
+			return err
+		}
+	}
+	sink.Conditions = source.Conditions
+	sink.Retries = source.Retries
+	sink.RunAfter = source.RunAfter
+	sink.Resources = source.Resources
+	sink.Params = source.Params
+	sink.Workspaces = source.Workspaces
+	return nil
 }
