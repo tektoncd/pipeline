ba79f404ef89da0c2406cb27026ba9d05b9cc4c4
Vincent Demeester <vdemeest@redhat.com>
2020-02-25 10:58:36 +0100

    Fix pullrequest-init tests 🛃
    - Remove the pullrequest-init binary and add it to .gitignore
    - add the e2e buildtag as it is in the `test` folder (which is
      e2e) **and** it is name `e2e_test.go`
    
    Signed-off-by: Vincent Demeester <vdemeest@redhat.com>

--- test/pullrequest/e2e_test.go
+++ test/pullrequest/e2e_test.go
@@ -1,3 +1,21 @@
+// +build e2e
+
+/*
+Copyright 2020 The Tekton Authors
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 // Package pullrequest tests the pullrequest-init binary end-to-end by invoking
 // the binary and observing the resulting filesystem state.
 package pullrequest
