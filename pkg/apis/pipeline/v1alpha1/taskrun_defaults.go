a9a8c225ca842c2cf94879da1e57410f54e55efb
Daniel Helfand <helfand.4@gmail.com>
2020-04-15 16:07:37 -0400

    fix default-timeout from config not correctly set for v1alpha1

--- pkg/apis/pipeline/v1alpha1/taskrun_defaults.go
+++ pkg/apis/pipeline/v1alpha1/taskrun_defaults.go
@@ -63,16 +63,7 @@ func (trs *TaskRunSpec) SetDefaults(ctx context.Context) {
 	}
 
 	if trs.Timeout == nil {
-		var timeout *metav1.Duration
-		if contexts.IsUpgradeViaDefaulting(ctx) {
-			// This case is for preexisting `TaskRun` before 0.5.0, so let's
-			// add the old default timeout.
-			// Most likely those TaskRun passing here are already done and/or already running
-			timeout = &metav1.Duration{Duration: config.DefaultTimeoutMinutes * time.Minute}
-		} else {
-			timeout = &metav1.Duration{Duration: time.Duration(cfg.Defaults.DefaultTimeoutMinutes) * time.Minute}
-		}
-		trs.Timeout = timeout
+		trs.Timeout = &metav1.Duration{Duration: time.Duration(cfg.Defaults.DefaultTimeoutMinutes) * time.Minute}
 	}
 
 	defaultSA := cfg.Defaults.DefaultServiceAccount
