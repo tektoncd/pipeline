4ecb74cc212956831f82af92373bf9e1ac027b10
Vibhav Bobade <vibhav.bobde@gmail.com>
2020-04-17 03:51:23 +0530

    Add Signal Forwarding to Entrypoint Runner
    The Entrypoint process should be notified and signals
    received should be propogated as necessary. This signal
    forwarding mimics the one in
    https://github.com/pablo-ruth/go-init which is an golang
    implementation of https://github.com/Yelp/dumb-init .
    
    The cmd.Run() has also been replaced with a cmd.Start()
    and cmd.Wait() to systematically start the command
    and Wait for it's completion without prematurely exiting.

--- cmd/entrypoint/runner.go
+++ cmd/entrypoint/runner.go
@@ -3,6 +3,8 @@ package main
 import (
 	"os"
 	"os/exec"
+	"os/signal"
+	"syscall"
 
 	"github.com/tektoncd/pipeline/pkg/entrypoint"
 )
@@ -11,22 +13,52 @@ import (
 // stdout/stderr are collected -- needs e2e tests.
 
 // realRunner actually runs commands.
-type realRunner struct{}
+type realRunner struct {
+	signals chan os.Signal
+}
 
 var _ entrypoint.Runner = (*realRunner)(nil)
 
-func (*realRunner) Run(args ...string) error {
+func (rr *realRunner) Run(args ...string) error {
 	if len(args) == 0 {
 		return nil
 	}
 	name, args := args[0], args[1:]
 
+	// Receive system signals on "rr.signals"
+	if rr.signals == nil {
+		rr.signals = make(chan os.Signal, 1)
+	}
+	defer close(rr.signals)
+	signal.Notify(rr.signals)
+	defer signal.Reset()
+
 	cmd := exec.Command(name, args...)
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
+	// dedicated PID group used to forward signals to
+	// main process and all children
+	cmd.SysProcAttr = &syscall.SysProcAttr{Setpgid: true}
 
-	if err := cmd.Run(); err != nil {
+	// Start defined command
+	if err := cmd.Start(); err != nil {
 		return err
 	}
+
+	// Goroutine for signals forwarding
+	go func() {
+		for s := range rr.signals {
+			// Forward signal to main process and all children
+			if s != syscall.SIGCHLD {
+				_ = syscall.Kill(-cmd.Process.Pid, s.(syscall.Signal))
+			}
+		}
+	}()
+
+	// Wait for command to exit
+	if err := cmd.Wait(); err != nil {
+		return err
+	}
+
 	return nil
 }
