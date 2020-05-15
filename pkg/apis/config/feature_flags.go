10b2f9a07564f644e7c3edc9982a8c64eb781743
Arash Deshmeh <adeshmeh@ca.ibm.com>
2020-05-15 10:07:32 +0000

    Add feature flags to config store
    Feature flags configuration is now tracked by the config store. This
    allows watching for the corresponding config map changes, removing the
    need to call kubernetes API to get the feature flags config map every
    time a pod is created.
    
    Signed-off-by: Arash Deshmeh <adeshmeh@ca.ibm.com>

---
+++ pkg/apis/config/feature_flags.go
@@ -0,0 +1,90 @@
+/*
+Copyright 2020 The Tekton Authors
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package config
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+
+	corev1 "k8s.io/api/core/v1"
+)
+
+const (
+	disableHomeEnvOverwriteKey              = "disable-home-env-overwrite"
+	disableWorkingDirOverwriteKey           = "disable-working-directory-overwrite"
+	disableAffinityAssistantKey             = "disable-affinity-assistant"
+	runningInEnvWithInjectedSidecarsKey     = "running-in-environment-with-injected-sidecars"
+	DefaultDisableHomeEnvOverwrite          = false
+	DefaultDisableWorkingDirOverwrite       = false
+	DefaultDisableAffinityAssistant         = false
+	DefaultRunningInEnvWithInjectedSidecars = true
+)
+
+// FeatureFlags holds the features configurations
+// +k8s:deepcopy-gen=true
+type FeatureFlags struct {
+	DisableHomeEnvOverwrite          bool
+	DisableWorkingDirOverwrite       bool
+	DisableAffinityAssistant         bool
+	RunningInEnvWithInjectedSidecars bool
+}
+
+// GetFeatureFlagsConfigName returns the name of the configmap containing all
+// feature flags.
+func GetFeatureFlagsConfigName() string {
+	if e := os.Getenv("CONFIG_FEATURE_FLAGS_NAME"); e != "" {
+		return e
+	}
+	return "feature-flags"
+}
+
+// NewFeatureFlagsFromMap returns a Config given a map corresponding to a ConfigMap
+func NewFeatureFlagsFromMap(cfgMap map[string]string) (*FeatureFlags, error) {
+	setFeature := func(key string, defaultValue bool, feature *bool) error {
+		if cfg, ok := cfgMap[key]; ok {
+			value, err := strconv.ParseBool(cfg)
+			if err != nil {
+				return fmt.Errorf("failed parsing feature flags config %q: %v", cfg, err)
+			}
+			*feature = value
+			return nil
+		}
+		*feature = defaultValue
+		return nil
+	}
+
+	tc := FeatureFlags{}
+	if err := setFeature(disableHomeEnvOverwriteKey, DefaultDisableHomeEnvOverwrite, &tc.DisableHomeEnvOverwrite); err != nil {
+		return nil, err
+	}
+	if err := setFeature(disableWorkingDirOverwriteKey, DefaultDisableWorkingDirOverwrite, &tc.DisableWorkingDirOverwrite); err != nil {
+		return nil, err
+	}
+	if err := setFeature(disableAffinityAssistantKey, DefaultDisableAffinityAssistant, &tc.DisableAffinityAssistant); err != nil {
+		return nil, err
+	}
+	if err := setFeature(runningInEnvWithInjectedSidecarsKey, DefaultRunningInEnvWithInjectedSidecars, &tc.RunningInEnvWithInjectedSidecars); err != nil {
+		return nil, err
+	}
+	return &tc, nil
+}
+
+// NewFeatureFlagsFromConfigMap returns a Config for the given configmap
+func NewFeatureFlagsFromConfigMap(config *corev1.ConfigMap) (*FeatureFlags, error) {
+	return NewFeatureFlagsFromMap(config.Data)
+}
---
+++ pkg/apis/config/feature_flags_test.go
@@ -0,0 +1,105 @@
+/*
+Copyright 2020 The Tekton Authors
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package config
+
+import (
+	"os"
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+	test "github.com/tektoncd/pipeline/pkg/reconciler/testing"
+	"github.com/tektoncd/pipeline/test/diff"
+)
+
+func TestNewFeatureFlagsFromConfigMap(t *testing.T) {
+	type testCase struct {
+		expectedConfig *FeatureFlags
+		fileName       string
+	}
+
+	testCases := []testCase{
+		{
+			expectedConfig: &FeatureFlags{
+				RunningInEnvWithInjectedSidecars: DefaultRunningInEnvWithInjectedSidecars,
+			},
+			fileName: GetFeatureFlagsConfigName(),
+		},
+		{
+			expectedConfig: &FeatureFlags{
+				DisableHomeEnvOverwrite:          true,
+				DisableWorkingDirOverwrite:       true,
+				DisableAffinityAssistant:         true,
+				RunningInEnvWithInjectedSidecars: false,
+			},
+			fileName: "feature-flags-all-flags-set",
+		},
+	}
+
+	for _, tc := range testCases {
+		verifyConfigFileWithExpectedFeatureFlagsConfig(t, tc.fileName, tc.expectedConfig)
+	}
+}
+
+func TestNewFeatureFlagsFromEmptyConfigMap(t *testing.T) {
+	FeatureFlagsConfigEmptyName := "feature-flags-empty"
+	expectedConfig := &FeatureFlags{
+		RunningInEnvWithInjectedSidecars: true,
+	}
+	verifyConfigFileWithExpectedFeatureFlagsConfig(t, FeatureFlagsConfigEmptyName, expectedConfig)
+}
+
+func TestGetFeatureFlagsConfigName(t *testing.T) {
+	for _, tc := range []struct {
+		description         string
+		featureFlagEnvValue string
+		expected            string
+	}{{
+		description:         "Feature flags config value not set",
+		featureFlagEnvValue: "",
+		expected:            "feature-flags",
+	}, {
+		description:         "Feature flags config value set",
+		featureFlagEnvValue: "feature-flags-test",
+		expected:            "feature-flags-test",
+	}} {
+		t.Run(tc.description, func(t *testing.T) {
+			original := os.Getenv("CONFIG_FEATURE_FLAGS_NAME")
+			defer t.Cleanup(func() {
+				os.Setenv("CONFIG_FEATURE_FLAGS_NAME", original)
+			})
+			if tc.featureFlagEnvValue != "" {
+				os.Setenv("CONFIG_FEATURE_FLAGS_NAME", tc.featureFlagEnvValue)
+			}
+			got := GetFeatureFlagsConfigName()
+			want := tc.expected
+			if got != want {
+				t.Errorf("GetFeatureFlagsConfigName() = %s, want %s", got, want)
+			}
+		})
+	}
+}
+
+func verifyConfigFileWithExpectedFeatureFlagsConfig(t *testing.T, fileName string, expectedConfig *FeatureFlags) {
+	cm := test.ConfigMapFromTestFile(t, fileName)
+	if flags, err := NewFeatureFlagsFromConfigMap(cm); err == nil {
+		if d := cmp.Diff(flags, expectedConfig); d != "" {
+			t.Errorf("Diff:\n%s", diff.PrintWantGot(d))
+		}
+	} else {
+		t.Errorf("NewFeatureFlagsFromConfigMap(actual) = %v", err)
+	}
+}
