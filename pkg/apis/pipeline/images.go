2d7d529d9f1025670d8d05831b4ec5eb7615e998
Jason Hall <jasonhall@google.com>
2020-07-27 13:13:23 -0400

    Remove default flag values for image names
    These values were previously defined in two places (here in flag
    defaults, and in config/controller.yaml), which could lead to confusing
    mismatches (#3007)

--- pkg/apis/pipeline/images.go
+++ pkg/apis/pipeline/images.go
@@ -16,6 +16,11 @@ limitations under the License.
 
 package pipeline
 
+import (
+	"fmt"
+	"sort"
+)
+
 // Images holds the images reference for a number of container images used
 // across tektoncd pipelines.
 type Images struct {
@@ -39,4 +44,34 @@ type Images struct {
 	PRImage string
 	// ImageDigestExporterImage is the container image containing our image digest exporter binary.
 	ImageDigestExporterImage string
+
+	// NOTE: Make sure to add any new images to Validate below!
+}
+
+// Validate returns an error if any image is not set.
+func (i Images) Validate() error {
+	var unset []string
+	for _, f := range []struct {
+		v, name string
+	}{
+		{i.EntrypointImage, "entrypoint"},
+		{i.NopImage, "nop"},
+		{i.GitImage, "git"},
+		{i.CredsImage, "creds"},
+		{i.KubeconfigWriterImage, "kubeconfig-writer"},
+		{i.ShellImage, "shell"},
+		{i.GsutilImage, "gsutil"},
+		{i.BuildGCSFetcherImage, "build-gcs-fetcher"},
+		{i.PRImage, "pr"},
+		{i.ImageDigestExporterImage, "imagedigest-exporter"},
+	} {
+		if f.v == "" {
+			unset = append(unset, f.name)
+		}
+	}
+	if len(unset) > 0 {
+		sort.Strings(unset)
+		return fmt.Errorf("found unset image flags: %s", unset)
+	}
+	return nil
 }
