f19daec8cc7ada9861502ea73f7eecbc3888add3
Piyush Garg <pgarg@redhat.com>
2020-08-14 14:11:47 +0530

    Add yamllint check and fix errors
    This will enable the yamllint check in CI
    for all yaml files except in vendor and
    fix all the yamllint errors

--- test/presubmit-tests.sh
+++ test/presubmit-tests.sh
@@ -29,10 +29,37 @@ export DISABLE_YAML_LINTING=1
 
 source $(git rev-parse --show-toplevel)/vendor/github.com/tektoncd/plumbing/scripts/presubmit-tests.sh
 
+function check_go_lint() {
+    header "Testing if golint has been done"
+
+    # deadline of 5m, and show all the issues
+    golangci-lint -j 1 --color=never run
+
+    if [[ $? != 0 ]]; then
+        results_banner "Go Lint" 1
+        exit 1
+    fi
+
+    results_banner "Go Lint" 0
+}
+
+function check_yaml_lint() {
+    header "Testing if yamllint has been done"
+
+    local YAML_FILES=$(find . -path ./vendor -prune -o -type f -regex ".*y[a]ml" -print)
+    yamllint -c .yamllint ${YAML_FILES}
+
+    if [[ $? != 0 ]]; then
+        results_banner "YAML Lint" 1
+        exit 1
+    fi
+
+    results_banner "YAML Lint" 0
+}
+
 function post_build_tests() {
-  header "running golangci-lint"
-  # deadline of 5m, and show all the issues
-  golangci-lint -j 1 --color=never run
+  check_go_lint
+  check_yaml_lint
 }
 
 # We use the default build, unit and integration test runners.
