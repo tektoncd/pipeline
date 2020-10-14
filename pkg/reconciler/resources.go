d8d69cedc6b06a6560acd88367ca111145a0cb6c
Matt Moore <mattmoor@vmware.com>
2020-10-14 08:54:41 -0700

    Don't fail immediately on missing resources.
    As the PipelineRun reconciler executes, it resolves resources using the informer's lister cache.  Currently, when that cache is behind the pipeline run will immediately fail.  This change builds in a buffer of `resources.MinimumAge` and a helper `resources.IsYoung` that elide this check, returning the error to the controller framework to requeue the key for later processing (with backoff).
    
    Fixes: https://github.com/tektoncd/pipeline/issues/3378

---
+++ pkg/reconciler/resources.go
@@ -0,0 +1,33 @@
+/*
+Copyright 2019 The Tekton Authors
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package reconciler
+
+import (
+	"time"
+
+	"knative.dev/pkg/kmeta"
+)
+
+const (
+	// MinimumResourceAge is the age at which resources stop being IsYoungResource.
+	MinimumResourceAge = 30 * time.Second
+)
+
+// IsYoungResource checks whether the resource is younger than MinimumResourceAge, based on its creation timestamp.
+func IsYoungResource(obj kmeta.Accessor) bool {
+	return time.Now().Sub(obj.GetCreationTimestamp().Time) < MinimumResourceAge
+}
