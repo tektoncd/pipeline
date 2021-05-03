029a16bf14dce6a68f2266c8e4ca2e5f53639c1c
Prashant Sharma <prashsh1@in.ibm.com>
2021-05-03 13:32:09 +0530

    TEP-0061, updated custom-task e2e to test embedded custom task

--- test/custom_task_test.go
+++ test/custom_task_test.go
@@ -20,6 +20,7 @@ package test
 
 import (
 	"context"
+	"strings"
 	"testing"
 	"time"
 
@@ -29,6 +30,7 @@ import (
 	"github.com/tektoncd/pipeline/test/diff"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
 	"knative.dev/pkg/apis"
 	duckv1 "knative.dev/pkg/apis/duck/v1"
 	knativetest "knative.dev/pkg/test"
@@ -51,7 +53,8 @@ func TestCustomTask(t *testing.T) {
 	c, namespace := setup(ctx, t, requireAnyGate(supportedFeatureGates))
 	knativetest.CleanupOnInterrupt(func() { tearDown(ctx, t, c, namespace) }, t.Logf)
 	defer tearDown(ctx, t, c, namespace)
-
+	customTaskRawSpec := []byte(`{"field1":123,"field2":"value"}`)
+	metadataLabel := map[string]string{"test-label": "test"}
 	// Create a PipelineRun that runs a Custom Task.
 	pipelineRunName := "custom-task-pipeline"
 	if _, err := c.PipelineRunClient.Create(
@@ -61,7 +64,7 @@ func TestCustomTask(t *testing.T) {
 			Spec: v1beta1.PipelineRunSpec{
 				PipelineSpec: &v1beta1.PipelineSpec{
 					Tasks: []v1beta1.PipelineTask{{
-						Name: "custom-task",
+						Name: "custom-task-ref",
 						TaskRef: &v1beta1.TaskRef{
 							APIVersion: apiVersion,
 							Kind:       kind,
@@ -69,35 +72,41 @@ func TestCustomTask(t *testing.T) {
 					}, {
 						Name: "custom-task-spec",
 						TaskSpec: &v1beta1.EmbeddedTask{
-							EmbeddedSpec: v1beta1.EmbeddedSpec{
-								TypeMeta: runtime.TypeMeta{
-									APIVersion: apiVersion,
-									Kind:       kind,
-								},
-								Spec: runtime.RawExtension{
-									Raw: customTaskRawSpec,
-								},
+							TypeMeta: runtime.TypeMeta{
+								APIVersion: apiVersion,
+								Kind:       kind,
+							},
+							Metadata: v1beta1.PipelineTaskMetadata{Labels: metadataLabel},
+							Spec: runtime.RawExtension{
+								Raw: customTaskRawSpec,
 							},
 						},
 					}, {
 						Name: "result-consumer",
 						Params: []v1beta1.Param{{
-							Name: "input-result-from-custom-task", Value: *v1beta1.NewArrayOrString("$(tasks.custom-task.results.runResult)"),
+							Name: "input-result-from-custom-task-ref", Value: *v1beta1.NewArrayOrString("$(tasks.custom-task-ref.results.runResult)"),
+						}, {
+							Name: "input-result-from-custom-task-spec", Value: *v1beta1.NewArrayOrString("$(tasks.custom-task-spec.results.runResult)"),
 						}},
 						TaskSpec: &v1beta1.EmbeddedTask{TaskSpec: v1beta1.TaskSpec{
 							Params: []v1beta1.ParamSpec{{
-								Name: "input-result-from-custom-task", Type: v1beta1.ParamTypeString,
+								Name: "input-result-from-custom-task-ref", Type: v1beta1.ParamTypeString,
+							}, {
+								Name: "input-result-from-custom-task-spec", Type: v1beta1.ParamTypeString,
 							}},
 							Steps: []v1beta1.Step{{Container: corev1.Container{
 								Image:   "ubuntu",
 								Command: []string{"/bin/bash"},
-								Args:    []string{"-c", "echo $(input-result-from-custom-task)"},
+								Args:    []string{"-c", "echo $(input-result-from-custom-task-ref) $(input-result-from-custom-task-spec)"},
 							}}},
 						}},
 					}},
 					Results: []v1beta1.PipelineResult{{
-						Name:  "prResult",
-						Value: "$(tasks.custom-task.results.runResult)",
+						Name:  "prResult-ref",
+						Value: "$(tasks.custom-task-ref.results.runResult)",
+					}, {
+						Name:  "prResult-spec",
+						Value: "$(tasks.custom-task-spec.results.runResult)",
 					}},
 				},
 			},
@@ -118,53 +127,58 @@ func TestCustomTask(t *testing.T) {
 	}
 
 	// Get the Run name.
-	if len(pr.Status.Runs) != 1 {
-		t.Fatalf("PipelineRun had unexpected .status.runs; got %d, want 1", len(pr.Status.Runs))
-	}
-	var runName string
-	for k := range pr.Status.Runs {
-		runName = k
-		break
+	if len(pr.Status.Runs) != 2 {
+		t.Fatalf("PipelineRun had unexpected .status.runs; got %d, want 2", len(pr.Status.Runs))
 	}
 
-	// Get the Run.
-	r, err := c.RunClient.Get(ctx, runName, metav1.GetOptions{})
-	if err != nil {
-		t.Fatalf("Failed to get Run %q: %v", runName, err)
-	}
-	if r.IsDone() {
-		t.Fatalf("Run unexpectedly done: %v", r.Status.GetCondition(apis.ConditionSucceeded))
-	}
+	for runName := range pr.Status.Runs {
+		// Get the Run.
+		r, err := c.RunClient.Get(ctx, runName, metav1.GetOptions{})
+		if err != nil {
+			t.Fatalf("Failed to get Run %q: %v", runName, err)
+		}
+		if r.IsDone() {
+			t.Fatalf("Run unexpectedly done: %v", r.Status.GetCondition(apis.ConditionSucceeded))
+		}
 
-	// Simulate a Custom Task controller updating the Run to done/successful.
-	r.Status = v1alpha1.RunStatus{
-		Status: duckv1.Status{
-			Conditions: duckv1.Conditions{{
-				Type:   apis.ConditionSucceeded,
-				Status: corev1.ConditionTrue,
-			}},
-		},
-		RunStatusFields: v1alpha1.RunStatusFields{
-			Results: []v1alpha1.RunResult{{
-				Name:  "runResult",
-				Value: "aResultValue",
-			}},
-		},
-	}
+		// Simulate a Custom Task controller updating the Run to done/successful.
+		r.Status = v1alpha1.RunStatus{
+			Status: duckv1.Status{
+				Conditions: duckv1.Conditions{{
+					Type:   apis.ConditionSucceeded,
+					Status: corev1.ConditionTrue,
+				}},
+			},
+			RunStatusFields: v1alpha1.RunStatusFields{
+				Results: []v1alpha1.RunResult{{
+					Name:  "runResult",
+					Value: "aResultValue",
+				}},
+			},
+		}
 
-	if _, err := c.RunClient.UpdateStatus(ctx, r, metav1.UpdateOptions{}); err != nil {
-		t.Fatalf("Failed to update Run to successful: %v", err)
-	}
+		if _, err := c.RunClient.UpdateStatus(ctx, r, metav1.UpdateOptions{}); err != nil {
+			t.Fatalf("Failed to update Run to successful: %v", err)
+		}
 
-	// Get the Run.
-	r, err = c.RunClient.Get(ctx, runName, metav1.GetOptions{})
-	if err != nil {
-		t.Fatalf("Failed to get Run %q: %v", runName, err)
-	}
-	if !r.IsDone() {
-		t.Fatalf("Run unexpectedly not done after update (UpdateStatus didn't work): %v", r.Status)
-	}
+		// Get the Run.
+		r, err = c.RunClient.Get(ctx, runName, metav1.GetOptions{})
 
+		if strings.Contains(runName, "custom-task-spec") {
+			if d := cmp.Diff(customTaskRawSpec, r.Spec.Spec.Spec.Raw); d != "" {
+				t.Fatalf("Unexpected value of Spec.Raw: %s", diff.PrintWantGot(d))
+			}
+			if d := cmp.Diff(metadataLabel, r.Spec.Spec.Metadata.Labels); d != "" {
+				t.Fatalf("Unexpected value of Metadata.Labels: %s", diff.PrintWantGot(d))
+			}
+		}
+		if err != nil {
+			t.Fatalf("Failed to get Run %q: %v", runName, err)
+		}
+		if !r.IsDone() {
+			t.Fatalf("Run unexpectedly not done after update (UpdateStatus didn't work): %v", r.Status)
+		}
+	}
 	// Wait for the PipelineRun to become done/successful.
 	if err := WaitForPipelineRunState(ctx, c, pipelineRunName, time.Minute, PipelineRunSucceed(pipelineRunName), "PipelineRunCompleted"); err != nil {
 		t.Fatalf("Waiting for PipelineRun to complete successfully: %v", err)
@@ -194,7 +208,9 @@ func TestCustomTask(t *testing.T) {
 
 	// Validate the task's result reference to the custom task's result was resolved.
 	expectedTaskRunParams := []v1beta1.Param{{
-		Name: "input-result-from-custom-task", Value: *v1beta1.NewArrayOrString("aResultValue"),
+		Name: "input-result-from-custom-task-ref", Value: *v1beta1.NewArrayOrString("aResultValue"),
+	}, {
+		Name: "input-result-from-custom-task-spec", Value: *v1beta1.NewArrayOrString("aResultValue"),
 	}}
 
 	if d := cmp.Diff(expectedTaskRunParams, taskRun.Spec.Params); d != "" {
@@ -204,12 +220,15 @@ func TestCustomTask(t *testing.T) {
 	// Validate that the pipeline's result reference to the custom task's result was resolved.
 
 	expectedPipelineResults := []v1beta1.PipelineRunResult{{
-		Name:  "prResult",
+		Name:  "prResult-ref",
+		Value: "aResultValue",
+	}, {
+		Name:  "prResult-spec",
 		Value: "aResultValue",
 	}}
 
-	if len(pr.Status.PipelineResults) == 0 {
-		t.Fatalf("Expected PipelineResults but there are none")
+	if len(pr.Status.PipelineResults) != 2 {
+		t.Fatalf("Expected 2 PipelineResults but there are %d.", len(pr.Status.PipelineResults))
 	}
 	if d := cmp.Diff(expectedPipelineResults, pr.Status.PipelineResults); d != "" {
 		t.Fatalf("Unexpected PipelineResults: %s", diff.PrintWantGot(d))
