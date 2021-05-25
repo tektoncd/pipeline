101fdbd0e5383a806b2a1e169fa8a25cab975550
Priya Wadhwa <priyawadhwa@google.com>
2021-05-25 11:37:43 -0700

    Add hermetic test running as non-root user

--- test/hermetic_taskrun_test.go
+++ test/hermetic_taskrun_test.go
@@ -20,12 +20,14 @@ package test
 
 import (
 	"context"
+	"fmt"
 	"testing"
 	"time"
 
 	"github.com/tektoncd/pipeline/pkg/apis/pipeline/v1beta1"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/utils/pointer"
 )
 
 // TestHermeticTaskRun make sure that the hermetic execution mode actually drops network from a TaskRun step
@@ -40,27 +42,44 @@ func TestHermeticTaskRun(t *testing.T) {
 	t.Parallel()
 	defer tearDown(ctx, t, c, namespace)
 
-	// first, run the task run with hermetic=false to prove that it succeeds
-	regularTaskRunName := "not-hermetic"
-	regularTaskRun := taskRun(regularTaskRunName, namespace, "")
-	t.Logf("Creating TaskRun %s, hermetic=false", regularTaskRunName)
-	if _, err := c.TaskRunClient.Create(ctx, regularTaskRun, metav1.CreateOptions{}); err != nil {
-		t.Fatalf("Failed to create TaskRun `%s`: %s", regularTaskRunName, err)
-	}
-	if err := WaitForTaskRunState(ctx, c, regularTaskRunName, Succeed(regularTaskRunName), "TaskRunCompleted"); err != nil {
-		t.Fatalf("Error waiting for TaskRun %s to finish: %s", regularTaskRunName, err)
+	tests := []struct {
+		desc       string
+		getTaskRun func(string, string, string) *v1beta1.TaskRun
+	}{
+		{
+			desc:       "run-as-root",
+			getTaskRun: taskRun,
+		}, {
+			desc:       "run-as-nonroot",
+			getTaskRun: unpriviligedTaskRun,
+		},
 	}
 
-	// now, run the task mode with hermetic mode
-	// it should fail, since it shouldn't be able to access any network
-	hermeticTaskRunName := "hermetic-should-fail"
-	hermeticTaskRun := taskRun(hermeticTaskRunName, namespace, "hermetic")
-	t.Logf("Creating TaskRun %s, hermetic=true", hermeticTaskRunName)
-	if _, err := c.TaskRunClient.Create(ctx, hermeticTaskRun, metav1.CreateOptions{}); err != nil {
-		t.Fatalf("Failed to create TaskRun `%s`: %s", regularTaskRun.Name, err)
-	}
-	if err := WaitForTaskRunState(ctx, c, hermeticTaskRunName, Failed(hermeticTaskRunName), "Failed"); err != nil {
-		t.Fatalf("Error waiting for TaskRun %s to fail: %s", hermeticTaskRunName, err)
+	for _, test := range tests {
+		t.Run(test.desc, func(t *testing.T) {
+			// first, run the task run with hermetic=false to prove that it succeeds
+			regularTaskRunName := fmt.Sprintf("not-hermetic-%s", test.desc)
+			regularTaskRun := test.getTaskRun(regularTaskRunName, namespace, "")
+			t.Logf("Creating TaskRun %s, hermetic=false", regularTaskRunName)
+			if _, err := c.TaskRunClient.Create(ctx, regularTaskRun, metav1.CreateOptions{}); err != nil {
+				t.Fatalf("Failed to create TaskRun `%s`: %s", regularTaskRunName, err)
+			}
+			if err := WaitForTaskRunState(ctx, c, regularTaskRunName, Succeed(regularTaskRunName), "TaskRunCompleted"); err != nil {
+				t.Fatalf("Error waiting for TaskRun %s to finish: %s", regularTaskRunName, err)
+			}
+
+			// now, run the task mode with hermetic mode
+			// it should fail, since it shouldn't be able to access any network
+			hermeticTaskRunName := fmt.Sprintf("hermetic-should-fail-%s", test.desc)
+			hermeticTaskRun := test.getTaskRun(hermeticTaskRunName, namespace, "hermetic")
+			t.Logf("Creating TaskRun %s, hermetic=true", hermeticTaskRunName)
+			if _, err := c.TaskRunClient.Create(ctx, hermeticTaskRun, metav1.CreateOptions{}); err != nil {
+				t.Fatalf("Failed to create TaskRun `%s`: %s", regularTaskRun.Name, err)
+			}
+			if err := WaitForTaskRunState(ctx, c, hermeticTaskRunName, Failed(hermeticTaskRunName), "Failed"); err != nil {
+				t.Fatalf("Error waiting for TaskRun %s to fail: %s", hermeticTaskRunName, err)
+			}
+		})
 	}
 }
 
@@ -85,6 +104,38 @@ func taskRun(name, namespace, executionMode string) *v1beta1.TaskRun {
 set -ex
 apt-get update
 apt-get install -y curl`,
+					},
+				},
+			},
+		},
+	}
+}
+
+func unpriviligedTaskRun(name, namespace, executionMode string) *v1beta1.TaskRun {
+	return &v1beta1.TaskRun{
+		ObjectMeta: metav1.ObjectMeta{Name: name,
+			Namespace: namespace,
+			Annotations: map[string]string{
+				"experimental.tekton.dev/execution-mode": executionMode,
+			},
+		},
+		Spec: v1beta1.TaskRunSpec{
+			Timeout: &metav1.Duration{Duration: time.Minute},
+			TaskSpec: &v1beta1.TaskSpec{
+				Steps: []v1beta1.Step{
+					{
+						Container: corev1.Container{
+							Name:  "curl",
+							Image: "gcr.io/cloud-builders/curl",
+							SecurityContext: &corev1.SecurityContext{
+								RunAsUser:                pointer.Int64Ptr(1000),
+								RunAsNonRoot:             pointer.BoolPtr(true),
+								AllowPrivilegeEscalation: pointer.BoolPtr(false),
+							},
+						},
+						Script: `#!/bin/bash
+set -ex
+curl google.com`,
 					},
 				},
 			},
