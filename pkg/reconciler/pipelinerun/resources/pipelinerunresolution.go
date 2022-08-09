8a396c3120e597acbf36150b7e2a81d74916386b
Andrew Bayer <andrew.bayer@gmail.com>
2022-08-09 13:03:26 -0400

    Prevent infinite recursion in skipBecauseResultReferencesAreMissing
    fixes #5271
    
    While I have not been able to determine why setting `enable-api-fields` to `alpha` while `PipelineRun`s using results are in flight hits this, the immediate symptom in the linked issue is that an infinite recursive loop is hit in `skipBecauseResultReferencesAreMissing`, due to `skipBecauseResultReferencesAreMissing` calling `rpt.Skip(facts)`, which calls `rpt.skip(facts)`, which ends up calling `rpt.skipBecauseResultReferencesAreMissing` again, etc. Changing the call to `rpt.Skip(facts).SkippingReason == v1beta1.WhenExpressionsSkip` to `rpt.skipBecauseWhenExpressionsEvaluatedToFalse(facts)` not only gets rid of the potential for infinite recursion, it also more directly checks the condition we care about there in the first place.
    
    Signed-off-by: Andrew Bayer <andrew.bayer@gmail.com>

--- pkg/reconciler/pipelinerun/resources/pipelinerunresolution.go
+++ pkg/reconciler/pipelinerun/resources/pipelinerunresolution.go
@@ -412,7 +412,7 @@ func (t *ResolvedPipelineTask) skipBecauseResultReferencesAreMissing(facts *Pipe
 		resolvedResultRefs, pt, err := ResolveResultRefs(facts.State, PipelineRunState{t})
 		rpt := facts.State.ToMap()[pt]
 		if rpt != nil {
-			if err != nil && (t.IsFinalTask(facts) || rpt.Skip(facts).SkippingReason == v1beta1.WhenExpressionsSkip) {
+			if err != nil && (t.IsFinalTask(facts) || rpt.skipBecauseWhenExpressionsEvaluatedToFalse(facts)) {
 				return true
 			}
 		}
