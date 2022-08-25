2ab7fa9e616db2a732f1925ea39ccffdda77c87e
Jerome Ju <jeromeju@google.com>
2022-08-25 20:32:40 +0000

    V1: Add conversion for TaskRef.Bundle
    This commit adds support for TaskRef.Bundle when converting between
    v1beta1 and v1 versions of TaskRef. This allows us to release v1 TaskRun
    and PipelineRun in a backwards compatible way by ensuring that v1beta1
    TaskRuns and PipelineRuns with Bundle converted into ResolverRef and
    params on the v1 TaskRuns and PipelineRuns on conversion.

--- pkg/apis/pipeline/v1beta1/taskref_conversion.go
+++ pkg/apis/pipeline/v1beta1/taskref_conversion.go
@@ -10,18 +10,38 @@ func (tr TaskRef) convertTo(ctx context.Context, sink *v1.TaskRef) {
 	sink.Name = tr.Name
 	sink.Kind = v1.TaskKind(tr.Kind)
 	sink.APIVersion = tr.APIVersion
-	// TODO: handle bundle in #4546
 	new := v1.ResolverRef{}
 	tr.ResolverRef.convertTo(ctx, &new)
 	sink.ResolverRef = new
+	tr.convertBundleToResolver(sink)
 }
 
 func (tr *TaskRef) convertFrom(ctx context.Context, source v1.TaskRef) {
 	tr.Name = source.Name
 	tr.Kind = TaskKind(source.Kind)
 	tr.APIVersion = source.APIVersion
-	// TODO: handle bundle in #4546
 	new := ResolverRef{}
 	new.convertFrom(ctx, source.ResolverRef)
 	tr.ResolverRef = new
+}
+
+// convertBundleToResolver converts v1beta1 bundle string to a remote reference with the bundle resolver in v1.
+// The conversion from Resolver to Bundle is not being supported since remote resolution would be turned on by
+// default and it will be in beta before the stored version of CRD getting swapped to v1.
+func (tr TaskRef) convertBundleToResolver(sink *v1.TaskRef) {
+	if tr.Bundle != "" {
+		sink.ResolverRef = v1.ResolverRef{
+			Resolver: "bundles",
+			Params: []v1.Param{{
+				Name:  "bundle",
+				Value: v1.ParamValue{StringVal: tr.Bundle},
+			}, {
+				Name:  "name",
+				Value: v1.ParamValue{StringVal: tr.Name},
+			}, {
+				Name:  "kind",
+				Value: v1.ParamValue{StringVal: "Task"},
+			}},
+		}
+	}
 }
