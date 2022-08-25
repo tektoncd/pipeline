016fed97e44f0ca5597173fbcc7d0522e5476ad3
Chuang Wang <chuangw@google.com>
2022-08-25 14:49:06 -0700

    Get rid 'pipelienrun'
    Fix typo

--- pkg/apis/config/metrics.go
+++ pkg/apis/config/metrics.go
@@ -50,7 +50,7 @@ const (
 	// when it isn't specified in configmap
 	DefaultPipelinerunLevel = PipelinerunLevelAtPipeline
 	// PipelinerunLevelAtPipelinerun specify that aggregation will be done at
-	// pipelienrun level
+	// pipelinerun level
 	PipelinerunLevelAtPipelinerun = "pipelinerun"
 	// PipelinerunLevelAtPipeline specify that aggregation will be done at
 	// pipeline level
@@ -132,8 +132,8 @@ func newMetricsFromMap(cfgMap map[string]string) (*Metrics, error) {
 	if durationTaskrun, ok := cfgMap[metricsDurationTaskrunType]; ok {
 		tc.DurationTaskrunType = durationTaskrun
 	}
-	if durationPipelienrun, ok := cfgMap[metricsDurationPipelinerunType]; ok {
-		tc.DurationPipelinerunType = durationPipelienrun
+	if durationPipelinerun, ok := cfgMap[metricsDurationPipelinerunType]; ok {
+		tc.DurationPipelinerunType = durationPipelinerun
 	}
 	return &tc, nil
 }
