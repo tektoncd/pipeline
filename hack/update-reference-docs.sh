29834fc4b4b001447beaa827054703b71e35c4fa
Andrew Bayer <andrew.bayer@gmail.com>
2022-09-01 16:56:25 -0400

    Add docs/pipeline-api.md.backup to .gitignore
    It'd be nice to get rid of this file entirely, but it's a result of https://github.com/tektoncd/pipeline/commit/8381d0d9d83326724ab260dc7ab36632afb1451f and I'm not sure how to tweak `sed` args to not generate a backup file but also work right on Macs...
    
    Signed-off-by: Andrew Bayer <andrew.bayer@gmail.com>

--- hack/update-reference-docs.sh
+++ hack/update-reference-docs.sh
@@ -23,4 +23,4 @@ go run github.com/ahmetb/gen-crd-api-reference-docs \
     -api-dir "github.com/tektoncd/pipeline/pkg/apis" \
     -template-dir "./hack/reference-docs-template" \
     -out-file "./docs/pipeline-api.md"
-sed -i".backup" '1s/^/<!--\n---\ntitle: Pipeline API\nlinkTitle: Pipeline API\nweight: 1000\n---\n-->\n\n/' ./docs/pipeline-api.md
+sed -i".backup" '1s/^/<!--\n---\ntitle: Pipeline API\nlinkTitle: Pipeline API\nweight: 1000\n---\n-->\n\n/' ./docs/pipeline-api.md && rm docs/pipeline-api.md.backup
