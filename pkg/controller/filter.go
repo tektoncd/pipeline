f83cd1f63bb216c76a3af46d58614d517113d1c5
Andrew Bayer <andrew.bayer@gmail.com>
2022-12-01 14:22:07 -0500

    Add FilterFuncs for CustomRun
    We're going to need these for actual custom task controllers watching for `v1beta1.CustomRun`s, so let's add them now.
    
    Signed-off-by: Andrew Bayer <andrew.bayer@gmail.com>

--- pkg/controller/filter.go
+++ pkg/controller/filter.go
@@ -23,6 +23,7 @@ import (
 	"github.com/tektoncd/pipeline/pkg/apis/pipeline/v1alpha1"
 	"github.com/tektoncd/pipeline/pkg/apis/pipeline/v1beta1"
 	listersalpha "github.com/tektoncd/pipeline/pkg/client/listers/pipeline/v1alpha1"
+	listersbeta "github.com/tektoncd/pipeline/pkg/client/listers/pipeline/v1beta1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
@@ -101,6 +102,86 @@ func FilterOwnerRunRef(runLister listersalpha.RunLister, apiVersion, kind string
 			result = run.Spec.Ref.APIVersion == apiVersion && run.Spec.Ref.Kind == v1beta1.TaskKind(kind)
 		} else if run.Spec.Spec != nil {
 			result = run.Spec.Spec.APIVersion == apiVersion && run.Spec.Spec.Kind == kind
+		}
+		return result
+	}
+}
+
+// FilterCustomRunRef returns a filter that can be passed to a CustomRun Informer, which
+// filters out CustomRuns for apiVersion and kinds that a controller doesn't care
+// about.
+//
+// For example, a controller impl that wants to be notified of updates to CustomRuns
+// which reference a Task with apiVersion "example.dev/v0" and kind "Example":
+//
+//     customruninformer.Get(ctx).Informer().AddEventHandler(cache.FilteringResourceEventHandler{
+//       FilterFunc: FilterCustomRunRef("example.dev/v0", "Example"),
+//       Handler:    controller.HandleAll(impl.Enqueue),
+//     })
+func FilterCustomRunRef(apiVersion, kind string) func(interface{}) bool {
+	return func(obj interface{}) bool {
+		r, ok := obj.(*v1beta1.CustomRun)
+		if !ok {
+			// Somehow got informed of a non-CustomRun object.
+			// Ignore.
+			return false
+		}
+		if r == nil || (r.Spec.CustomRef == nil && r.Spec.CustomSpec == nil) {
+			// These are invalid, but just in case they get
+			// created somehow, don't panic.
+			return false
+		}
+		result := false
+		if r.Spec.CustomRef != nil {
+			result = r.Spec.CustomRef.APIVersion == apiVersion && r.Spec.CustomRef.Kind == v1beta1.TaskKind(kind)
+		} else if r.Spec.CustomSpec != nil {
+			result = r.Spec.CustomSpec.APIVersion == apiVersion && r.Spec.CustomSpec.Kind == kind
+		}
+		return result
+	}
+}
+
+// FilterOwnerCustomRunRef returns a filter that can be passed to an Informer for any runtime object, which
+// filters out objects that aren't controlled by a CustomRun that references a particular apiVersion and kind.
+//
+// For example, a controller impl that wants to be notified of updates to TaskRuns that are controlled by
+// a CustomRun which references a custom task with apiVersion "example.dev/v0" and kind "Example":
+//
+//     taskruninformer.Get(ctx).Informer().AddEventHandler(cache.FilteringResourceEventHandler{
+//       FilterFunc: FilterOwnerCustomRunRef("example.dev/v0", "Example"),
+//       Handler:    controller.HandleAll(impl.Enqueue),
+//     })
+func FilterOwnerCustomRunRef(customRunLister listersbeta.CustomRunLister, apiVersion, kind string) func(interface{}) bool {
+	return func(obj interface{}) bool {
+		object, ok := obj.(metav1.Object)
+		if !ok {
+			return false
+		}
+		owner := metav1.GetControllerOf(object)
+		if owner == nil {
+			return false
+		}
+		if owner.APIVersion != v1beta1.SchemeGroupVersion.String() || owner.Kind != pipeline.CustomRunControllerName {
+			// Not owned by a CustomRun
+			return false
+		}
+		run, err := customRunLister.CustomRuns(object.GetNamespace()).Get(owner.Name)
+		if err != nil {
+			return false
+		}
+		if run.Spec.CustomRef == nil && run.Spec.CustomSpec == nil {
+			// These are invalid, but just in case they get created somehow, don't panic.
+			return false
+		}
+		if run.Spec.CustomRef != nil && run.Spec.CustomSpec != nil {
+			// These are invalid.
+			return false
+		}
+		result := false
+		if run.Spec.CustomRef != nil {
+			result = run.Spec.CustomRef.APIVersion == apiVersion && run.Spec.CustomRef.Kind == v1beta1.TaskKind(kind)
+		} else if run.Spec.CustomSpec != nil {
+			result = run.Spec.CustomSpec.APIVersion == apiVersion && run.Spec.CustomSpec.Kind == kind
 		}
 		return result
 	}
