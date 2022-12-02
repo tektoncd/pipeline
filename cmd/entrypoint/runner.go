e7f07eaee75c18554c631465c3741b2cb2fc18a9
Yongxuan Zhang <yongxuanzhang@google.com>
2022-12-02 18:03:35 +0000

    test runner test

--- cmd/entrypoint/runner.go
+++ cmd/entrypoint/runner.go
@@ -8,7 +8,7 @@ Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
 
-    http://www.apache.org/licenses/LICENSE-2.0
+	http://www.apache.org/licenses/LICENSE-2.0
 
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
@@ -119,6 +119,7 @@ func (rr *realRunner) Run(ctx context.Context, args ...string) error {
 	// Start defined command
 	if err := cmd.Start(); err != nil {
 		if ctx.Err() == context.DeadlineExceeded {
+			fmt.Println("####err return at start")
 			return context.DeadlineExceeded
 		}
 		return err
@@ -153,6 +154,7 @@ func (rr *realRunner) Run(ctx context.Context, args ...string) error {
 	// Wait for command to exit
 	if err := cmd.Wait(); err != nil {
 		if ctx.Err() == context.DeadlineExceeded {
+			fmt.Println("####err return at wait")
 			return context.DeadlineExceeded
 		}
 		return err
