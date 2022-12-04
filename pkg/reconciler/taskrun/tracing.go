05801e8587212e78ceca6b66ab46358d999da53e
Jayadeep KM <kmjayadeep@gmail.com>
2022-12-04 08:47:31 +0100

    Add tracing spancontext to taskrun status
    Signed-off-by: Jayadeep KM <kmjayadeep@gmail.com>

--- pkg/reconciler/taskrun/tracing.go
+++ pkg/reconciler/taskrun/tracing.go
@@ -39,45 +39,38 @@ const (
 // spanContext is propogated through annotations in the CR
 func initTracing(ctx context.Context, tracerProvider trace.TracerProvider, tr *v1beta1.TaskRun) context.Context {
 	logger := logging.FromContext(ctx)
-	carrier := make(map[string]string)
-
 	pro := otel.GetTextMapPropagator()
-	trAnnotations := tr.Annotations
-	if trAnnotations == nil {
-		trAnnotations = make(map[string]string)
+
+	// SpanContext was created already
+	if tr.Status.SpanContext != nil && len(tr.Status.SpanContext) > 0 {
+		return pro.Extract(ctx, propagation.MapCarrier(tr.Status.SpanContext))
 	}
 
-	// if spanContext is not present in the CR, create new parent trace and
-	// add the marshalled spancontext to the annotations
-	if _, e := trAnnotations[SpanContextAnnotation]; !e {
-		ctxWithTrace, span := tracerProvider.Tracer(TracerName).Start(ctx, "TaskRun:Reconciler")
-		defer span.End()
-		span.SetAttributes(attribute.String("task", tr.Name), attribute.String("namespace", tr.Namespace))
+	tr.Status.SpanContext = make(map[string]string)
 
-		pro.Inject(ctxWithTrace, propagation.MapCarrier(carrier))
-
-		logger.Info("got carrier", carrier)
-		if len(carrier) == 0 {
-			logger.Debug("tracerProvider doesn't provide a traceId, tracing is disabled")
-			return ctx
-		}
-
-		marshalled, err := json.Marshal(carrier)
+	// SpanContext was propogated through annotations
+	if tr.Annotations != nil && tr.Annotations[SpanContextAnnotation] != "" {
+		err := json.Unmarshal([]byte(tr.Annotations[SpanContextAnnotation]), &tr.Status.SpanContext)
 		if err != nil {
-			logger.Error("unable to marshal spancontext, err: %s", err)
-			return ctx
+			logger.Error("unable to unmarshal spancontext, err: %s", err)
 		}
-		logger.Debug("adding spancontext", "ctx", string(marshalled))
-		trAnnotations[SpanContextAnnotation] = string(marshalled)
-		tr.Annotations = trAnnotations
-		span.AddEvent("updating TaskRun CR with SpanContext annotations")
-		return ctxWithTrace
+
+		return pro.Extract(ctx, propagation.MapCarrier(tr.Status.SpanContext))
 	}
 
-	err := json.Unmarshal([]byte(tr.Annotations[SpanContextAnnotation]), &carrier)
-	if err != nil {
-		logger.Error("unable to unmarshal spancontext, err: %s", err)
+	// Create a new root span since there was no parent spanContext provided through annotations
+	ctxWithTrace, span := tracerProvider.Tracer(TracerName).Start(ctx, "TaskRun:Reconciler")
+	defer span.End()
+	span.SetAttributes(attribute.String("taskrun", tr.Name), attribute.String("namespace", tr.Namespace))
+
+	pro.Inject(ctxWithTrace, propagation.MapCarrier(tr.Status.SpanContext))
+
+	logger.Debug("got tracing carrier", tr.Status.SpanContext)
+	if len(tr.Status.SpanContext) == 0 {
+		logger.Debug("tracerProvider doesn't provide a traceId, tracing is disabled")
+		return ctx
 	}
 
-	return pro.Extract(ctx, propagation.MapCarrier(carrier))
+	span.AddEvent("updating TaskRun status with SpanContext")
+	return ctxWithTrace
 }
