a11b158f8fdeb14004e327c64065123769a36fcf
Lee Bernick <lee.a.bernick@gmail.com>
2023-03-09 15:43:08 -0500

    Check generated API docs during CI
    This commit updates hack/verify-codegen.sh to verify that the generated API reference docs
    match any changes to the API and its documentation, and re-runs reference doc generation.
    It does not verify that licenses have been updated (this will be a separate commit).

--- hack/verify-codegen.sh
+++ hack/verify-codegen.sh
@@ -37,15 +37,20 @@ cp -aR "${REPO_ROOT_DIR}/pkg" "${TMP_DIFFROOT}"
 mkdir -p "${TMP_DIFFROOT}/vendor"
 cp -aR "${REPO_ROOT_DIR}/vendor" "${TMP_DIFFROOT}"
 
+mkdir -p "${TMP_DIFFROOT}/docs"
+cp -aR "${REPO_ROOT_DIR}/docs" "${TMP_DIFFROOT}"
+
 "${REPO_ROOT_DIR}/hack/update-codegen.sh"
 echo "Diffing ${REPO_ROOT_DIR} against freshly generated codegen"
 ret=0
 diff -Naupr "${REPO_ROOT_DIR}/pkg" "${TMP_DIFFROOT}/pkg" || ret=1
 diff -Naupr "${REPO_ROOT_DIR}/vendor" "${TMP_DIFFROOT}/vendor" || ret=1
+diff -Naupr "${REPO_ROOT_DIR}/docs/pipeline-api.md" "${TMP_DIFFROOT}/docs/pipeline-api.md" || ret=1
 
 # Restore working tree state
 rm -fr "${REPO_ROOT_DIR}/pkg"
 rm -fr "${REPO_ROOT_DIR}/vendor"
+rm -fr "${REPO_ROOT_DIR}/docs"
 cp -aR "${TMP_DIFFROOT}"/* "${REPO_ROOT_DIR}"
 
 if [[ $ret -eq 0 ]]
