215d56fa348bbf3f86f0ae1fc7a8552e441b37e2
Yongxuan Zhang <yongxuanzhang@google.com>
2023-03-15 20:09:05 +0000

    fix error msg in taskref.resolver.params validation
    This commit is a small fix to the error message of
    taskref.resolver.params validation. It adds "resolver" to the "params"
    validation message to avoid confusion.
    
    Signed-off-by: Yongxuan Zhang yongxuanzhang@google.com

--- pkg/apis/pipeline/v1/taskref_validation_test.go
+++ pkg/apis/pipeline/v1/taskref_validation_test.go
@@ -100,7 +100,7 @@ func TestTaskRef_Invalid(t *testing.T) {
 				Params: []v1.Param{},
 			},
 		},
-		wantErr: apis.ErrMissingField("resolver").Also(apis.ErrGeneric("params requires \"enable-api-fields\" feature gate to be \"alpha\" or \"beta\" but it is \"stable\"")),
+		wantErr: apis.ErrMissingField("resolver").Also(apis.ErrGeneric("resolver params requires \"enable-api-fields\" feature gate to be \"alpha\" or \"beta\" but it is \"stable\"")),
 	}, {
 		name: "taskref params disallowed without resolver",
 		taskRef: &v1.TaskRef{
