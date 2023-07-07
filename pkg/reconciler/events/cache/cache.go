ca1ad1925e04e0e0a64f8434f7a32126626da1e9
Andrea Frittoli <andrea.frittoli@uk.ibm.com>
2023-07-07 12:16:04 +0100

    Rework the events controller cache
    Switch the cache implementation from a simple LRU to the bigcache
    library which provides better performance and reduces the need
    for garbage collection.
    
    Further improve the cache by reducing the size of keys, by
    adopting hashing of keys and thus storing smaller data in the cache.
    Hashing also allows for detecting changes in a larger surface of
    the data. While todays keys are based on the event type and
    resource metadata, we can now include conditions in the equation
    as well, to provide and enhanced logic for event sending.
    
    Storing a resource metadata and its condition in the hashed keys
    means we can detect immediately if no relevant change was made
    in a resource, and quickly finish the reconcile cycle.
    Additionally storing the event type along with the resource
    metadata, means that we can detect if a specific message was
    already sent for a specific resource.
    
    To be able to rely on the cache to decide which event to send,
    we need to ensure that reconcile cycles for the same resource
    are never executed concurrently. To achieve this, the thread
    number (or concurrency) is set to 1 in the controller.
    
    Scalability needs can be addressed by scaling the events
    controller horizontally. The sharding of resources across
    instances of the reconciler is based on the resource key, so
    the same resource will always be directed to the same controller
    thus avoiding any cache miss.
    
    Signed-off-by: Andrea Frittoli <andrea.frittoli@uk.ibm.com>

--- pkg/reconciler/events/cache/cache.go
+++ pkg/reconciler/events/cache/cache.go
@@ -17,52 +17,130 @@ limitations under the License.
 package cache
 
 import (
-	"encoding/json"
 	"errors"
 	"fmt"
+	"hash/fnv"
+	"strconv"
+	"time"
 
+	bc "github.com/allegro/bigcache/v3"
 	cloudevents "github.com/cloudevents/sdk-go/v2"
-	lru "github.com/hashicorp/golang-lru"
 	"github.com/tektoncd/pipeline/pkg/apis/pipeline/v1beta1"
+	"knative.dev/pkg/apis"
 )
 
-// Struct to unmarshal the event data
-type eventData struct {
-	CustomRun *v1beta1.CustomRun `json:"customRun,omitempty"`
-}
-
-// ContainsOrAddCloudEvent checks if the event exists in the cache
-func ContainsOrAddCloudEvent(cacheClient *lru.Cache, event *cloudevents.Event) (bool, error) {
+// containsOrAddKey checks if the key exists in the cache.
+// Returns true (already cached) if the key was found; returns false (new) and adds
+// the key if it was not present. Either way, the current timestamp is appended as value.
+func containsOrAddKey(cacheClient *bc.BigCache, key string) (bool, error) {
 	if cacheClient == nil {
 		return false, errors.New("cache client is nil")
 	}
-	eventKey, err := EventKey(event)
+	// Set the new key
+	timeNow, err := hash(time.Now().Format(time.RFC3339Nano))
 	if err != nil {
 		return false, err
 	}
-	isPresent, _ := cacheClient.ContainsOrAdd(eventKey, nil)
-	return isPresent, nil
+	err = cacheClient.Append(key, []byte(timeNow))
+	if err != nil {
+		return false, err
+	}
+	// Get the value - if it matches what we added, the key was new
+	value, err := cacheClient.Get(key)
+	if err != nil {
+		return false, err
+	}
+	return (string(value) != timeNow), nil
 }
 
-// EventKey defines whether an event is considered different from another
-// in future we might want to let specific event types override this
-func EventKey(event *cloudevents.Event) (string, error) {
-	var (
-		data              eventData
-		resourceName      string
-		resourceNamespace string
-		resourceKind      string
-	)
-	err := json.Unmarshal(event.Data(), &data)
+// ContainsOrAddObject adds an object key (ObjectKey) to the cache and returns true if
+// the key was already present, false if it was new.
+// A cache miss requires a new object or change in the condition (including message-only changes)
+func ContainsOrAddObject(cacheClient *bc.BigCache, object v1beta1.RunObject) (bool, error) {
+	if cacheClient == nil {
+		return false, errors.New("cache client is nil")
+	}
+	objectKey, err := ObjectKey(object)
+	if err != nil {
+		return false, err
+	}
+	return containsOrAddKey(cacheClient, objectKey)
+}
+
+// ContainsOrAddCloudEvent adds an event key (EventKey) to the cache and returns true if
+// the key was already present, false if it was new.
+// A cache miss requires a new object or a new event type for the same object
+func ContainsOrAddCloudEvent(cacheClient *bc.BigCache, event *cloudevents.Event, object v1beta1.RunObject) (bool, error) {
+	if cacheClient == nil {
+		return false, errors.New("cache client is nil")
+	}
+	eventKey, err := EventKey(event, object)
+	if err != nil {
+		return false, err
+	}
+	return containsOrAddKey(cacheClient, eventKey)
+}
+
+// EventKey encodes the event type and object identity (GVK, namespace, name).
+// Once an event type is recorded here it is never re-sent for the same object,
+// regardless of condition changes.
+func EventKey(event *cloudevents.Event, object v1beta1.RunObject) (string, error) {
+	if object == nil || event == nil {
+		return "", fmt.Errorf("both object (%v) and event (%v) must be not nil", object, event)
+	}
+	if object.GetObjectKind() == nil {
+		return "", fmt.Errorf("object %v has nil object kind", object)
+	}
+	if object.GetObjectMeta() == nil {
+		return "", fmt.Errorf("object %v has nil object meta", object)
+	}
+	return hash(fmt.Sprintf("%s/%s/%s/%s/%s/%s",
+		event.Type(),
+		object.GetObjectKind().GroupVersionKind().Group,
+		object.GetObjectKind().GroupVersionKind().Version,
+		object.GetObjectKind().GroupVersionKind().Kind,
+		object.GetObjectMeta().GetNamespace(),
+		object.GetObjectMeta().GetName()))
+}
+
+// ObjectKey encodes the full condition snapshot (Status, Reason, Message)
+// plus object identity (GVK, namespace, name).
+// Any condition change — including a message-only change — produces a new key.
+func ObjectKey(object v1beta1.RunObject) (string, error) {
+	var condition *apis.Condition
+	if object == nil {
+		return "", errors.New("object must be not nil")
+	}
+	// The condition may not be set, in that case return an empty condition
+	if object.GetStatusCondition() != nil {
+		condition = object.GetStatusCondition().GetCondition(apis.ConditionSucceeded)
+	}
+	if condition == nil {
+		condition = &apis.Condition{}
+	}
+	if object.GetObjectKind() == nil {
+		return "", fmt.Errorf("object %v has nil object kind", object)
+	}
+	if object.GetObjectMeta() == nil {
+		return "", fmt.Errorf("object %v has nil object meta", object)
+	}
+	return hash(fmt.Sprintf("%s/%s/%s/%s/%s/%s/%s/%s",
+		condition.Status,
+		condition.Reason,
+		condition.Message,
+		object.GetObjectKind().GroupVersionKind().Group,
+		object.GetObjectKind().GroupVersionKind().Version,
+		object.GetObjectKind().GroupVersionKind().Kind,
+		object.GetObjectMeta().GetNamespace(),
+		object.GetObjectMeta().GetName()))
+}
+
+// hash provide fnv64 hash converted to string in base36
+func hash(input string) (string, error) {
+	hasher := fnv.New64a()
+	_, err := hasher.Write([]byte(input))
 	if err != nil {
 		return "", err
 	}
-	if data.CustomRun == nil {
-		return "", fmt.Errorf("invalid CustomRun data in %v", event)
-	}
-	resourceName = data.CustomRun.Name
-	resourceNamespace = data.CustomRun.Namespace
-	resourceKind = "customrun"
-	eventType := event.Type()
-	return fmt.Sprintf("%s/%s/%s/%s", eventType, resourceKind, resourceNamespace, resourceName), nil
+	return strconv.FormatUint(hasher.Sum64(), 36), nil
 }
--- pkg/reconciler/events/cache/cache_test.go
+++ pkg/reconciler/events/cache/cache_test.go
@@ -17,24 +17,59 @@ limitations under the License.
 package cache_test
 
 import (
+	"context"
+	"fmt"
+	"hash/fnv"
 	"net/url"
+	"strconv"
 	"testing"
 	"time"
 
+	bc "github.com/allegro/bigcache/v3"
 	cloudevents "github.com/cloudevents/sdk-go/v2"
 	"github.com/cloudevents/sdk-go/v2/event"
 	cetypes "github.com/cloudevents/sdk-go/v2/types"
 	"github.com/google/go-cmp/cmp"
-	lru "github.com/hashicorp/golang-lru"
+	v1 "github.com/tektoncd/pipeline/pkg/apis/pipeline/v1"
 	"github.com/tektoncd/pipeline/pkg/apis/pipeline/v1beta1"
 	cache "github.com/tektoncd/pipeline/pkg/reconciler/events/cache"
 	"github.com/tektoncd/pipeline/test/diff"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"github.com/tektoncd/pipeline/test/parse"
+	corev1 "k8s.io/api/core/v1"
+)
+
+const (
+	META_TEMPLATE = `
+metadata:
+  name: %s
+  namespace: %s`
+	CONDITION_BASE = `
+status:
+  conditions:
+  - lastTransitionTime: "2023-07-04T09:40:03Z"
+    type: Succeeded`
+	STATUS_TEMPLATE = `
+    status: "%s"`
+	REASON_TEMPLATE = `
+    reason: %s`
+	MESSAGE_TEMPLATE = `
+    message: %s`
 )
 
 func strptr(s string) *string { return &s }
 
-func getEventData(run interface{}) map[string]interface{} {
+// hash provide fnv64 hash converted to string in base36
+func hash(input string) string {
+	hasher := fnv.New64a()
+	_, err := hasher.Write([]byte(input))
+	if err != nil {
+		return ""
+	}
+	return strconv.FormatUint(hasher.Sum64(), 36)
+}
+
+func getEventData(t *testing.T, run interface{}) map[string]interface{} {
+	t.Helper()
 	cloudEventData := map[string]interface{}{}
 	if v, ok := run.(*v1beta1.CustomRun); ok {
 		cloudEventData["customRun"] = v
@@ -42,7 +77,8 @@ func getEventData(run interface{}) map[string]interface{} {
 	return cloudEventData
 }
 
-func getEventToTest(eventtype string, run interface{}) *event.Event {
+func getEventToTest(t *testing.T, eventtype string, run interface{}) *event.Event {
+	t.Helper()
 	e := event.Event{
 		Context: event.EventContextV1{
 			Type:    eventtype,
@@ -52,43 +88,104 @@ func getEventToTest(eventtype string, run interface{}) *event.Event {
 			Subject: strptr("topic"),
 		}.AsV1(),
 	}
-	if err := e.SetData(cloudevents.ApplicationJSON, getEventData(run)); err != nil {
+	if err := e.SetData(cloudevents.ApplicationJSON, getEventData(t, run)); err != nil {
 		panic(err)
 	}
 	return &e
 }
 
-func getCustomRunByMeta(name string, namespace string) *v1beta1.CustomRun {
-	return &v1beta1.CustomRun{
-		TypeMeta: metav1.TypeMeta{
-			Kind:       "CustomRun",
-			APIVersion: "v1beta1",
-		},
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      name,
-			Namespace: namespace,
-		},
-		Spec:   v1beta1.CustomRunSpec{},
-		Status: v1beta1.CustomRunStatus{},
+func getRunYamlByMeta(t *testing.T, name, namespace, message, reason, status string) string {
+	t.Helper()
+	yaml := fmt.Sprintf(META_TEMPLATE, name, namespace)
+	// If the status is not set, ignore the rest
+	if status != "" {
+		yaml += CONDITION_BASE
+		yaml += fmt.Sprintf(STATUS_TEMPLATE, status)
+		if message != "" {
+			yaml += fmt.Sprintf(MESSAGE_TEMPLATE, message)
+		}
+		if reason != "" {
+			yaml += fmt.Sprintf(REASON_TEMPLATE, reason)
+		}
 	}
+	return yaml
 }
 
-// TestEventsKey verifies that keys are extracted correctly from events
+func getCustomRunByMeta(t *testing.T, name, namespace, message, reason, status string) *v1beta1.CustomRun {
+	t.Helper()
+	return parse.MustParseCustomRun(t,
+		getRunYamlByMeta(t, name, namespace, message, reason, status),
+	)
+}
+
+func getTaskRunByMeta(t *testing.T, name, namespace, message, reason, status string) *v1.TaskRun {
+	t.Helper()
+	return parse.MustParseV1TaskRun(t,
+		getRunYamlByMeta(t, name, namespace, message, reason, status),
+	)
+}
+
+func getTaskRunBetaByMeta(t *testing.T, name, namespace, message, reason, status string) *v1beta1.TaskRun {
+	t.Helper()
+	return parse.MustParseV1beta1TaskRun(t,
+		getRunYamlByMeta(t, name, namespace, message, reason, status),
+	)
+}
+
+func getPipelineRunByMeta(t *testing.T, name, namespace, message, reason, status string) *v1.PipelineRun {
+	t.Helper()
+	return parse.MustParseV1PipelineRun(t,
+		getRunYamlByMeta(t, name, namespace, message, reason, status),
+	)
+}
+
+func getPipelineRunBetaByMeta(t *testing.T, name, namespace, message, reason, status string) *v1beta1.PipelineRun {
+	t.Helper()
+	return parse.MustParseV1beta1PipelineRun(t,
+		getRunYamlByMeta(t, name, namespace, message, reason, status),
+	)
+}
+
+// TestEventsKey verifies that keys are extracted correctly from events and runs
 func TestEventsKey(t *testing.T) {
 	testcases := []struct {
 		name      string
 		eventtype string
-		run       interface{}
+		run       v1beta1.RunObject
 		wantKey   string
 		wantErr   bool
 	}{{
 		name:      "customrun event",
 		eventtype: "my.test.run.event",
-		run:       getCustomRunByMeta("myrun", "mynamespace"),
-		wantKey:   "my.test.run.event/customrun/mynamespace/myrun",
+		run:       getCustomRunByMeta(t, "myrun", "mynamespace", "", "", ""),
+		wantKey:   "my.test.run.event/tekton.dev/v1beta1/CustomRun/mynamespace/myrun",
 		wantErr:   false,
 	}, {
-		name:      "run event missing data",
+		name:      "taskrun v1beta1 event",
+		eventtype: "my.test.run.event",
+		run:       getTaskRunBetaByMeta(t, "mytaskrun", "mynamespace", "", "", ""),
+		wantKey:   "my.test.run.event/tekton.dev/v1beta1/TaskRun/mynamespace/mytaskrun",
+		wantErr:   false,
+	}, {
+		name:      "taskrun event",
+		eventtype: "my.test.run.event",
+		run:       getTaskRunByMeta(t, "mytaskrun", "mynamespace", "", "", ""),
+		wantKey:   "my.test.run.event/tekton.dev/v1/TaskRun/mynamespace/mytaskrun",
+		wantErr:   false,
+	}, {
+		name:      "pipelinerun v1beta1 event",
+		eventtype: "my.test.run.event",
+		run:       getPipelineRunBetaByMeta(t, "mypipelinerun", "mynamespace", "", "", ""),
+		wantKey:   "my.test.run.event/tekton.dev/v1beta1/PipelineRun/mynamespace/mypipelinerun",
+		wantErr:   false,
+	}, {
+		name:      "pipelinerun event",
+		eventtype: "my.test.run.event",
+		run:       getPipelineRunByMeta(t, "mypipelinerun", "mynamespace", "", "", ""),
+		wantKey:   "my.test.run.event/tekton.dev/v1/PipelineRun/mynamespace/mypipelinerun",
+		wantErr:   false,
+	}, {
+		name:      "run event missing run",
 		eventtype: "my.test.run.event",
 		run:       nil,
 		wantKey:   "",
@@ -97,60 +194,248 @@ func TestEventsKey(t *testing.T) {
 
 	for _, tc := range testcases {
 		t.Run(tc.name, func(t *testing.T) {
-			gotEvent := getEventToTest(tc.eventtype, tc.run)
-			gotKey, err := cache.EventKey(gotEvent)
+			gotEvent := getEventToTest(t, tc.eventtype, tc.run)
+			gotKey, err := cache.EventKey(gotEvent, tc.run)
 			if err != nil {
 				if !tc.wantErr {
-					t.Fatalf("Expecting an error, got none")
+					t.Errorf("Expecting an error, got none")
 				}
 			}
-			if d := cmp.Diff(tc.wantKey, gotKey); d != "" {
+			// Compare the key, use the hash if don't expect an error
+			wantKey := hash(tc.wantKey)
+			if tc.wantErr {
+				wantKey = tc.wantKey
+			}
+			if d := cmp.Diff(wantKey, gotKey); d != "" {
 				t.Errorf("Wrong Event key %s", diff.PrintWantGot(d))
 			}
 		})
 	}
 }
 
-func TestAddCheckEvent(t *testing.T) {
-	customRun1 := getCustomRunByMeta("arun", "anamespace")
-	customRun2 := getCustomRunByMeta("arun", "bnamespace")
-	baseEvent := getEventToTest("some.event.type", customRun1)
-
+// TestObjectKey verifies that keys are extracted correctly from objects
+func TestObjectKey(t *testing.T) {
 	testcases := []struct {
-		name        string
-		firstEvent  *event.Event
-		secondEvent *event.Event
-		wantFound   bool
+		name    string
+		run     v1beta1.RunObject
+		wantKey string
+		wantErr bool
 	}{{
-		name:        "identical events",
-		firstEvent:  baseEvent,
-		secondEvent: baseEvent,
-		wantFound:   true,
+		name:    "customrun no condition",
+		run:     getCustomRunByMeta(t, "myrun", "mynamespace", "", "", ""),
+		wantKey: "///tekton.dev/v1beta1/CustomRun/mynamespace/myrun",
+		wantErr: false,
 	}, {
-		name:        "new timestamp event",
-		firstEvent:  baseEvent,
-		secondEvent: getEventToTest("some.event.type", customRun1),
-		wantFound:   true,
+		name:    "customrun only status",
+		run:     getCustomRunByMeta(t, "myrun", "mynamespace", "", "", string(corev1.ConditionUnknown)),
+		wantKey: "Unknown///tekton.dev/v1beta1/CustomRun/mynamespace/myrun",
+		wantErr: false,
 	}, {
-		name:        "different namespace",
-		firstEvent:  baseEvent,
-		secondEvent: getEventToTest("some.event.type", customRun2),
-		wantFound:   false,
+		name:    "customrun all",
+		run:     getCustomRunByMeta(t, "myrun", "mynamespace", "mymessage", "myreason", string(corev1.ConditionUnknown)),
+		wantKey: "Unknown/myreason/mymessage/tekton.dev/v1beta1/CustomRun/mynamespace/myrun",
+		wantErr: false,
 	}, {
-		name:        "different event type",
-		firstEvent:  baseEvent,
-		secondEvent: getEventToTest("some.other.event.type", customRun1),
-		wantFound:   false,
+		name:    "taskrun v1beta1",
+		run:     getTaskRunBetaByMeta(t, "mytaskrun", "mynamespace", "mymessage", "myreason", string(corev1.ConditionTrue)),
+		wantKey: "True/myreason/mymessage/tekton.dev/v1beta1/TaskRun/mynamespace/mytaskrun",
+		wantErr: false,
+	}, {
+		name:    "taskrun",
+		run:     getTaskRunByMeta(t, "mytaskrun", "mynamespace", "mymessage", "myreason", string(corev1.ConditionFalse)),
+		wantKey: "False/myreason/mymessage/tekton.dev/v1/TaskRun/mynamespace/mytaskrun",
+		wantErr: false,
+	}, {
+		name:    "pipelinerun v1beta1",
+		run:     getPipelineRunBetaByMeta(t, "mypipelinerun", "mynamespace", "mymessage", "myreason", string(corev1.ConditionUnknown)),
+		wantKey: "Unknown/myreason/mymessage/tekton.dev/v1beta1/PipelineRun/mynamespace/mypipelinerun",
+		wantErr: false,
+	}, {
+		name:    "pipelinerun",
+		run:     getPipelineRunByMeta(t, "mypipelinerun", "mynamespace", "mymessage", "myreason", string(corev1.ConditionUnknown)),
+		wantKey: "Unknown/myreason/mymessage/tekton.dev/v1/PipelineRun/mynamespace/mypipelinerun",
+		wantErr: false,
+	}, {
+		name:    "run event missing run",
+		run:     nil,
+		wantKey: "",
+		wantErr: true,
 	}}
 
 	for _, tc := range testcases {
 		t.Run(tc.name, func(t *testing.T) {
-			testCache, _ := lru.New(10)
-			_, _ = cache.ContainsOrAddCloudEvent(testCache, tc.firstEvent)
-			found, _ := cache.ContainsOrAddCloudEvent(testCache, tc.secondEvent)
-			if d := cmp.Diff(tc.wantFound, found); d != "" {
-				t.Errorf("Cache check failure %s", diff.PrintWantGot(d))
+			gotKey, err := cache.ObjectKey(tc.run)
+			if err != nil {
+				if !tc.wantErr {
+					t.Errorf("Expecting an error, got none")
+				}
+			}
+			// Compare the key, use the hash if don't expect an error
+			wantKey := hash(tc.wantKey)
+			if tc.wantErr {
+				wantKey = tc.wantKey
+			}
+			if d := cmp.Diff(wantKey, gotKey); d != "" {
+				t.Errorf("Wrong Event key %s", diff.PrintWantGot(d))
 			}
 		})
 	}
+}
+
+func TestContainsOrAddCloudEvent(t *testing.T) {
+	customRun1Cond1 := getCustomRunByMeta(t, "arun", "anamespace", "", "", "")
+	customRun1Cond2 := getCustomRunByMeta(t, "arun", "bnamespace", "mymessage", "myreason", string(corev1.ConditionUnknown))
+	customRun1event1 := getEventToTest(t, "some.event.type", customRun1Cond1)
+	customRun1event2 := getEventToTest(t, "some.other.event.type", customRun1Cond2)
+	customRun2Cond1 := getCustomRunByMeta(t, "anotherun", "anamespace", "", "", "")
+	customRun2Cond2 := getCustomRunByMeta(t, "anotherun", "anamespace", "mymessage", "myreason", string(corev1.ConditionUnknown))
+	customRun2event1 := getEventToTest(t, "some.event.type", customRun2Cond1)
+	customRun2event2 := getEventToTest(t, "some.other.event.type", customRun2Cond2)
+
+	testCache, err := bc.New(context.Background(), bc.Config{
+		Shards:       16,
+		MaxEntrySize: 16,
+	})
+	if err != nil {
+		t.Fatalf("unexpected error %v", err)
+	}
+
+	check := func(tc string, expected, found bool, err error) {
+		if err != nil {
+			t.Fatalf("%s: unexpected error adding the event %v", tc, err)
+		}
+		if d := cmp.Diff(expected, found); d != "" {
+			t.Fatalf("%s: unexpected result from cache %s", tc, diff.PrintWantGot(d))
+		}
+	}
+	// Add the first event for the first run
+	found, err := cache.ContainsOrAddCloudEvent(testCache, customRun1event1, customRun1Cond1)
+	check("run1, event1", false, found, err)
+	// Add the first event for the second run
+	found, err = cache.ContainsOrAddCloudEvent(testCache, customRun2event1, customRun2Cond1)
+	check("run2, event1", false, found, err)
+	// Add the first event for the first run (again)
+	found, err = cache.ContainsOrAddCloudEvent(testCache, customRun1event1, customRun1Cond1)
+	check("run1, event1 again", true, found, err)
+	// Add the second event for the second run
+	found, err = cache.ContainsOrAddCloudEvent(testCache, customRun2event2, customRun2Cond2)
+	check("run2, event2", false, found, err)
+	// Add the second event for the first run
+	found, err = cache.ContainsOrAddCloudEvent(testCache, customRun1event2, customRun1Cond2)
+	check("run1, event2", false, found, err)
+	// Add the second event for the first run (again)
+	found, err = cache.ContainsOrAddCloudEvent(testCache, customRun1event2, customRun1Cond2)
+	check("run1, event2 again", true, found, err)
+}
+
+// TestTwoLevelCacheLifecycle verifies the two-level cache contract:
+//   - Level 1 (ObjectKey): gate on full condition state; a message-only change opens
+//     the gate again.
+//   - Level 2 (EventKey):  gate on event type; blocks re-send of terminal events even
+//     after a message change; exempt for "unknown" events.
+func TestTwoLevelCacheLifecycle(t *testing.T) {
+	// Simulate a TaskRun lifecycle:
+	//   1. running, message ""         → L1 miss, L2 miss  → send unknown
+	//   2. running, message "step 2/3" → L1 miss, L2 exempt → send unknown
+	//   3. running, message "step 2/3" → L1 hit             → skip
+	//   4. succeeded                   → L1 miss, L2 miss  → send successful
+	//   5. succeeded, message changed  → L1 miss, L2 hit   → skip (terminal)
+	runRunning1 := getTaskRunByMeta(t, "tr", "ns", "", "Running", string(corev1.ConditionUnknown))
+	runRunning2 := getTaskRunByMeta(t, "tr", "ns", "step 2/3", "Running", string(corev1.ConditionUnknown))
+	runSucceeded1 := getTaskRunByMeta(t, "tr", "ns", "", "Succeeded", string(corev1.ConditionTrue))
+	runSucceeded2 := getTaskRunByMeta(t, "tr", "ns", "all done", "Succeeded", string(corev1.ConditionTrue))
+
+	unknownEvent := getEventToTest(t, "dev.tekton.event.taskrun.unknown.v1", runRunning1)
+	successEvent := getEventToTest(t, "dev.tekton.event.taskrun.successful.v1", runSucceeded1)
+
+	testCache, err := bc.New(context.Background(), bc.DefaultConfig(2*time.Hour))
+	if err != nil {
+		t.Fatalf("unexpected error %v", err)
+	}
+
+	check := func(label string, wantFound, gotFound bool, err error) {
+		t.Helper()
+		if err != nil {
+			t.Fatalf("%s: unexpected error %v", label, err)
+		}
+		if d := cmp.Diff(wantFound, gotFound); d != "" {
+			t.Errorf("%s: %s", label, diff.PrintWantGot(d))
+		}
+	}
+
+	// Step 1: running, no message — L1 miss
+	found, err := cache.ContainsOrAddObject(testCache, runRunning1)
+	check("step1 L1 running/empty", false, found, err)
+	// unknown is exempt from L2 — no L2 check needed; would send
+
+	// Step 2: running, message "step 2/3" — L1 miss (new condition state)
+	found, err = cache.ContainsOrAddObject(testCache, runRunning2)
+	check("step2 L1 running/step2", false, found, err)
+	// unknown is exempt from L2 — would send again
+
+	// Step 3: same state repeated — L1 hit → skip
+	found, err = cache.ContainsOrAddObject(testCache, runRunning2)
+	check("step3 L1 running/step2 again", true, found, err)
+
+	// Step 4: succeeded — L1 miss
+	found, err = cache.ContainsOrAddObject(testCache, runSucceeded1)
+	check("step4 L1 succeeded", false, found, err)
+	// L2 miss for successful event — would send
+	found, err = cache.ContainsOrAddCloudEvent(testCache, successEvent, runSucceeded1)
+	check("step4 L2 successful", false, found, err)
+
+	// Step 5: succeeded with different message — L1 miss (message changed)
+	found, err = cache.ContainsOrAddObject(testCache, runSucceeded2)
+	check("step5 L1 succeeded/new msg", false, found, err)
+	// L2 hit — successful already sent; must NOT re-send
+	found, err = cache.ContainsOrAddCloudEvent(testCache, successEvent, runSucceeded2)
+	check("step5 L2 successful already sent", true, found, err)
+
+	// Unknown event is never blocked by L2 — verify ContainsOrAddCloudEvent
+	// would return false for it (i.e. callers must skip calling L2 for unknown).
+	// We verify this by confirming the unknown EventKey is distinct and not yet cached.
+	found, err = cache.ContainsOrAddCloudEvent(testCache, unknownEvent, runRunning1)
+	check("unknown L2 first call", false, found, err)
+	// Second call would return true — confirming callers must not call L2 for unknown.
+	found, err = cache.ContainsOrAddCloudEvent(testCache, unknownEvent, runRunning1)
+	check("unknown L2 second call (proves caller must skip L2 for unknown)", true, found, err)
+}
+
+func TestContainsOrAddObject(t *testing.T) {
+	customRun1Cond1 := getCustomRunByMeta(t, "arun", "anamespace", "", "", "")
+	customRun1Cond2 := getCustomRunByMeta(t, "arun", "bnamespace", "mymessage", "myreason", string(corev1.ConditionUnknown))
+	customRun2Cond1 := getCustomRunByMeta(t, "anotherun", "anamespace", "", "", "")
+	customRun2Cond2 := getCustomRunByMeta(t, "anotherun", "anamespace", "mymessage", "myreason", string(corev1.ConditionTrue))
+
+	testCache, err := bc.New(context.Background(), bc.DefaultConfig(2*time.Hour))
+	if err != nil {
+		t.Fatalf("unexpected error %v", err)
+	}
+
+	check := func(tc string, expected, found bool, err error) {
+		if err != nil {
+			t.Fatalf("%s: unexpected error adding the event %v", tc, err)
+		}
+		if d := cmp.Diff(expected, found); d != "" {
+			t.Fatalf("%s: unexpected result from cache %s", tc, diff.PrintWantGot(d))
+		}
+	}
+	// Add the first condition for the first run
+	found, err := cache.ContainsOrAddObject(testCache, customRun1Cond1)
+	check("run1, cond1", false, found, err)
+	// Add the first condition for the second run
+	found, err = cache.ContainsOrAddObject(testCache, customRun2Cond1)
+	check("run2, cond1", false, found, err)
+	// Add the first condition for the first run (again)
+	found, err = cache.ContainsOrAddObject(testCache, customRun1Cond1)
+	check("run1, cond1 again", true, found, err)
+	// Add the second condition for the second run
+	found, err = cache.ContainsOrAddObject(testCache, customRun2Cond2)
+	check("run2, cond2", false, found, err)
+	// Add the second condition for the first run
+	found, err = cache.ContainsOrAddObject(testCache, customRun1Cond2)
+	check("run1, cond2", false, found, err)
+	// Add the second condition for the first run (again)
+	found, err = cache.ContainsOrAddObject(testCache, customRun1Cond2)
+	check("run1, cond2 again", true, found, err)
 }
