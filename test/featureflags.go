8beca7795956f0580e129bcbc5783af8cc1dd511
Dhruval7878 <patadiya.23mca10103@vitbhopal.ac.in.com>
2023-11-10 01:39:14 +0530

    rebased-code

--- test/featureflags.go
+++ test/featureflags.go
@@ -116,6 +116,7 @@ func getFeatureFlagsBaseOnAPIFlag(t *testing.T) *config.FeatureFlags {
 		"enable-tekton-oci-bundles":    "true",
 		"enable-step-actions":          "true",
 		"enable-cel-in-whenexpression": "true",
+		"enable-param-enum":            "true",
 	})
 	if err != nil {
 		t.Fatalf("error creating alpha feature flags configmap: %v", err)
