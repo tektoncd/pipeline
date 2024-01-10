e79a2360cf90ee78dcec161f9c251afbe23efde9
gabemontero <gmontero@redhat.com>
2024-01-10 11:41:46 -0500

    allow pipeline runs whose task/custom runs have been deleted still timeout
    Back with PR 5134 Tekton started using the cancel function for cleaning up
    underlying TaskRuns of PipelineRuns that are timing out, though separate
    timeout functions were used (presumably in case the timeout behavior needed to be
    tweaked from the cancel behavior later on).  Then, in PR 5288, improvements to
    the baseline cancel function were made to still complete cancel processing of a
    PipelineRun if the underlying TaskRuns were deleted.  However, that same accomodation
    was not made for the timeout path.  This change addresses that, but still keeps the
    timeout codepaths separate from the 'base' cancel codepaths.

--- pkg/reconciler/pipelinerun/timeout_test.go
+++ pkg/reconciler/pipelinerun/timeout_test.go
@@ -60,6 +60,23 @@ func TestTimeoutPipelineRun(t *testing.T) {
 			{ObjectMeta: metav1.ObjectMeta{Name: "t1"}},
 		},
 	}, {
+		name: "multiple-runs-missing",
+		pipelineRun: &v1.PipelineRun{
+			ObjectMeta: metav1.ObjectMeta{Name: "test-pipeline-run-timedout"},
+			Spec:       v1.PipelineRunSpec{},
+			Status: v1.PipelineRunStatus{PipelineRunStatusFields: v1.PipelineRunStatusFields{
+				ChildReferences: []v1.ChildStatusReference{{
+					TypeMeta:         runtime.TypeMeta{Kind: taskRun},
+					Name:             "t1",
+					PipelineTaskName: "task-1",
+				}, {
+					TypeMeta:         runtime.TypeMeta{Kind: customRun},
+					Name:             "t2",
+					PipelineTaskName: "task-2",
+				}},
+			}},
+		},
+	}, {
 		name: "multiple-taskruns",
 		pipelineRun: &v1.PipelineRun{
 			ObjectMeta: metav1.ObjectMeta{Name: "test-pipeline-run-timedout"},
