6e10518ef12373f0a03b64e8e3f6b63d47d4ce38
qingliu <qingliu@alauda.io>
2024-01-23 17:20:58 +0800

    fix: ensure clustertask annotations are synced to taskrun
    fix #7601

--- pkg/reconciler/taskrun/resources/taskref_test.go
+++ pkg/reconciler/taskrun/resources/taskref_test.go
@@ -228,6 +228,12 @@ func TestLocalTaskRef(t *testing.T) {
 				&v1beta1.ClusterTask{
 					ObjectMeta: metav1.ObjectMeta{
 						Name: "cluster-task",
+						Annotations: map[string]string{
+							"foo": "bar",
+						},
+						Labels: map[string]string{
+							"foo": "bar",
+						},
 					},
 				},
 				&v1beta1.ClusterTask{
@@ -247,6 +253,12 @@ func TestLocalTaskRef(t *testing.T) {
 				},
 				ObjectMeta: metav1.ObjectMeta{
 					Name: "cluster-task",
+					Annotations: map[string]string{
+						"foo": "bar",
+					},
+					Labels: map[string]string{
+						"foo": "bar",
+					},
 				},
 			},
 			wantErr: nil,
