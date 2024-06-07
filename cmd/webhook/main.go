c3e3d64d753341a10872115a784d95a1bb190e01
zhouhaibing089 <zhouhaibing089@gmail.com>
2024-06-06 22:50:28 -0700

    webhook: remove resource_namespace tag in stats reporter
    This addresses #3171.
    
    As `resource_namespace` has the potential to contribute to high metrics
    cardinality while not adding much value from observability perspective,
    it would be ideal to disable it by default.

--- cmd/webhook/main.go
+++ cmd/webhook/main.go
@@ -245,6 +245,12 @@ func main() {
 		webhookName = "webhook.pipeline.tekton.dev"
 	}
 
+	var statsReporterOptions []webhook.StatsReporterOption
+	enableNamespace := os.Getenv("WEBHOOK_METRICS_ENABLE_NAMESPACE")
+	if enableNamespace != "true" {
+		statsReporterOptions = append(statsReporterOptions, webhook.WithoutTags("resource_namespace"))
+	}
+
 	// Scope informers to the webhook's namespace instead of cluster-wide
 	ctx := injection.WithNamespaceScope(signals.NewContext(), system.Namespace())
 
@@ -253,6 +259,8 @@ func main() {
 		ServiceName: serviceName,
 		Port:        webhook.PortFromEnv(8443),
 		SecretName:  secretName,
+
+		StatsReporterOptions: statsReporterOptions,
 	})
 
 	mux := http.NewServeMux()
