21f6b869b7116c437b9cbf4d6ff0ad2d5bc7fda4
Vincent Demeester <vdemeest@redhat.com>
2024-08-06 18:42:16 +0200

    pkg/taskrunmetrics/fake shouldn't be imported…
    … in production code. It is used for unit-tests only. This might mess
    up the injection code and which code is executed when for metrics.
    
    Signed-off-by: Vincent Demeester <vdemeest@redhat.com>

--- pkg/reconciler/taskrun/taskrun_test.go
+++ pkg/reconciler/taskrun/taskrun_test.go
@@ -54,6 +54,7 @@ import (
 	"github.com/tektoncd/pipeline/pkg/reconciler/volumeclaim"
 	resolutioncommon "github.com/tektoncd/pipeline/pkg/resolution/common"
 	remoteresource "github.com/tektoncd/pipeline/pkg/resolution/resource"
+	_ "github.com/tektoncd/pipeline/pkg/taskrunmetrics/fake"
 	"github.com/tektoncd/pipeline/pkg/trustedresources"
 	"github.com/tektoncd/pipeline/pkg/trustedresources/verifier"
 	"github.com/tektoncd/pipeline/pkg/workspace"
