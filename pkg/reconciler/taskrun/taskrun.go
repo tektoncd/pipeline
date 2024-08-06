3e3ec27e622a654dee0b348cf9477139e5e1c717
Vincent Demeester <vdemeest@redhat.com>
2024-08-06 18:42:16 +0200

    pkg/taskrunmetrics/fake shouldn't be imported…
    … in production code. It is used for unit-tests only. This might mess
    up the injection code and which code is executed when for metrics.
    
    Signed-off-by: Vincent Demeester <vdemeest@redhat.com>

--- pkg/reconciler/taskrun/taskrun.go
+++ pkg/reconciler/taskrun/taskrun.go
@@ -50,7 +50,6 @@ import (
 	resolution "github.com/tektoncd/pipeline/pkg/resolution/resource"
 	"github.com/tektoncd/pipeline/pkg/spire"
 	"github.com/tektoncd/pipeline/pkg/taskrunmetrics"
-	_ "github.com/tektoncd/pipeline/pkg/taskrunmetrics/fake" // Make sure the taskrunmetrics are setup
 	"github.com/tektoncd/pipeline/pkg/trustedresources"
 	"github.com/tektoncd/pipeline/pkg/workspace"
 	"go.opentelemetry.io/otel/attribute"
@@ -861,7 +860,6 @@ func (c *Reconciler) createPod(ctx context.Context, ts *v1.TaskSpec, tr *v1.Task
 
 	var err error
 	ts, err = workspace.Apply(ctx, *ts, tr.Spec.Workspaces, workspaceVolumes)
-
 	if err != nil {
 		logger.Errorf("Failed to create a pod for taskrun: %s due to workspace error %v", tr.Name, err)
 		return nil, err
