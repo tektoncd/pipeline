d541b0bb5c23e66f629a326d516120399350ed2a
Vincent Demeester <vdemeest@redhat.com>
2024-09-17 14:21:44 +0200

    Fix isolated workspaces ignored when using StepTemplate
    Prior to this fix, when one would use isolated workspaces (per step
    workspace) in a `Task` *and* a `stepTemplate`, the isolated workspace
    would be ignored and thus mounted in all steps instead.
    
    This is now fixed by ensuring we do not loose the `Workspaces`
    definition from a `Step` when we merge it with a `StepTemplate`.
    
    Signed-off-by: Vincent Demeester <vdemeest@redhat.com>
    (cherry picked from commit 81b4fbfb744787f2399e018d85b6a7cb1e9d2905)

--- pkg/apis/pipeline/v1/merge.go
+++ pkg/apis/pipeline/v1/merge.go
@@ -61,7 +61,14 @@ func MergeStepsWithStepTemplate(template *StepTemplate, steps []Step) ([]Step, e
 		amendConflictingContainerFields(&merged, s)
 
 		// Pass through original step Script, for later conversion.
-		newStep := Step{Script: s.Script, OnError: s.OnError, Timeout: s.Timeout, StdoutConfig: s.StdoutConfig, StderrConfig: s.StderrConfig}
+		newStep := Step{
+			Script:       s.Script,
+			OnError:      s.OnError,
+			Timeout:      s.Timeout,
+			StdoutConfig: s.StdoutConfig,
+			StderrConfig: s.StderrConfig,
+			Workspaces:   s.Workspaces,
+		}
 		newStep.SetContainerFields(merged)
 		steps[i] = newStep
 	}
