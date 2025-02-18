7f7394f3bc95a599a1bc6942057b9f8f583d8a6c
Vincent Demeester <vdemeest@redhat.com>
2025-02-18 11:34:11 +0100

    presubmit-test.sh: use ghcr.io instead of gcr.io
    We migrated those images, we should use the ghcr.io ones

--- test/presubmit-tests.sh
+++ test/presubmit-tests.sh
@@ -51,9 +51,9 @@ function ko_resolve() {
     baseImageOverrides:
       # Use the combined base image for images that should include Windows support.
       # NOTE: Make sure this list of images to use the combined base image is in sync with what's in tekton/publish.yaml's 'create-ko-yaml' Task.
-      github.com/tektoncd/pipeline/cmd/entrypoint: gcr.io/tekton-releases/github.com/tektoncd/pipeline/combined-base-image:latest
-      github.com/tektoncd/pipeline/cmd/nop: gcr.io/tekton-releases/github.com/tektoncd/pipeline/combined-base-image:latest
-      github.com/tektoncd/pipeline/cmd/workingdirinit: gcr.io/tekton-releases/github.com/tektoncd/pipeline/combined-base-image:latest
+      github.com/tektoncd/pipeline/cmd/entrypoint: ghcr.io/tektoncd/pipeline/github.com/tektoncd/pipeline/combined-base-image:latest
+      github.com/tektoncd/pipeline/cmd/nop: ghcr.io/tektoncd/pipeline/github.com/tektoncd/pipeline/combined-base-image:latest
+      github.com/tektoncd/pipeline/cmd/workingdirinit: ghcr.io/tektoncd/pipeline/github.com/tektoncd/pipeline/combined-base-image:latest
 
       github.com/tektoncd/pipeline/cmd/git-init: cgr.dev/chainguard/git
 EOF
