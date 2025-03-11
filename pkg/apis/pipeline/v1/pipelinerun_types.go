1cb87f9171f906fac6d27d71c06a7775f5b2317f
Vibhav Bobade <vibhav.bobde@gmail.com>
2025-03-11 20:05:37 +0530

    feat: override task timeouts in pipelineruns

--- pkg/apis/pipeline/v1/pipelinerun_types.go
+++ pkg/apis/pipeline/v1/pipelinerun_types.go
@@ -648,6 +648,11 @@ type PipelineTaskRunSpec struct {
 
 	// Compute resources to use for this TaskRun
 	ComputeResources *corev1.ResourceRequirements `json:"computeResources,omitempty"`
+
+	// Time after which the TaskRun times out.
+	// Refer Go's ParseDuration documentation for expected format: https://golang.org/pkg/time/#ParseDuration
+	// +optional
+	Timeout *metav1.Duration `json:"timeout,omitempty"`
 }
 
 // GetTaskRunSpec returns the task specific spec for a given
@@ -670,6 +675,7 @@ func (pr *PipelineRun) GetTaskRunSpec(pipelineTaskName string) PipelineTaskRunSp
 			s.SidecarSpecs = task.SidecarSpecs
 			s.Metadata = task.Metadata
 			s.ComputeResources = task.ComputeResources
+			s.Timeout = task.Timeout
 		}
 	}
 	return s
