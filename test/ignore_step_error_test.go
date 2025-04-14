ad5aa94ccc9cbeab0d47728e5c554e465b6472bb
FaniD <fanid.dev@gmail.com>
2025-04-14 22:41:14 +0300

    feat: add testcases for continueAndFail on e2e tests

--- test/ignore_step_error_test.go
+++ test/ignore_step_error_test.go
@@ -31,6 +31,7 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"knative.dev/pkg/apis"
 	knativetest "knative.dev/pkg/test"
+	"knative.dev/pkg/test/helpers"
 )
 
 func TestFailingStepOnContinue(t *testing.T) {
@@ -40,8 +41,35 @@ func TestFailingStepOnContinue(t *testing.T) {
 	c, namespace := setup(ctx, t)
 	knativetest.CleanupOnInterrupt(func() { tearDown(ctx, t, c, namespace) }, t.Logf)
 	defer tearDown(ctx, t, c, namespace)
-	taskRunName := "mytaskrun"
-	tr := parse.MustParseV1TaskRun(t, fmt.Sprintf(`
+
+	tests := []struct {
+		description   string
+		onError       string
+		shouldSucceed bool
+		state         string
+		conditionFn   func(name string) ConditionAccessorFn
+	}{
+		{
+			description:   "continue and succeed",
+			onError:       "continue",
+			shouldSucceed: true,
+			state:         "TaskRunSucceeded",
+			conditionFn:   TaskRunSucceed,
+		},
+		{
+			description:   "continue and fail",
+			onError:       "continueAndFail",
+			shouldSucceed: false,
+			state:         "TaskRunFailed",
+			conditionFn:   TaskRunFailed,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.description, func(t *testing.T) {
+			taskRunName := helpers.ObjectNameForTest(t)
+
+			tr := parse.MustParseV1TaskRun(t, fmt.Sprintf(`
 metadata:
   name: %s
   namespace: %s
@@ -52,36 +80,41 @@ spec:
       - name: result2
     steps:
       - name: failing-step
-        onError: continue
+        onError: %s
         image: mirror.gcr.io/busybox
         script: 'echo -n 123 | tee $(results.result1.path); exit 1; echo -n 456 | tee $(results.result2.path)'
-`, taskRunName, namespace))
+      - name: successful-step
+        image: mirror.gcr.io/busybox
+        script: 'echo hello'
+`, taskRunName, namespace, test.onError))
 
-	if _, err := c.V1TaskRunClient.Create(ctx, tr, metav1.CreateOptions{}); err != nil {
-		t.Fatalf("Failed to create TaskRun: %s", err)
-	}
+			if _, err := c.V1TaskRunClient.Create(ctx, tr, metav1.CreateOptions{}); err != nil {
+				t.Fatalf("Failed to create TaskRun: %s", err)
+			}
 
-	t.Logf("Waiting for TaskRun in namespace %s to finish", namespace)
-	if err := WaitForTaskRunState(ctx, c, taskRunName, TaskRunSucceed(taskRunName), "TaskRunSucceeded", v1Version); err != nil {
-		t.Errorf("Error waiting for TaskRun to finish: %s", err)
-	}
+			t.Logf("Waiting for TaskRun in namespace %s to finish", namespace)
+			if err := WaitForTaskRunState(ctx, c, taskRunName, test.conditionFn(taskRunName), test.state, v1Version); err != nil {
+				t.Errorf("Error waiting for TaskRun to finish: %s", err)
+			}
 
-	taskRun, err := c.V1TaskRunClient.Get(ctx, taskRunName, metav1.GetOptions{})
-	if err != nil {
-		t.Fatalf("Couldn't get expected TaskRun %s: %s", taskRunName, err)
-	}
+			taskRun, err := c.V1TaskRunClient.Get(ctx, taskRunName, metav1.GetOptions{})
+			if err != nil {
+				t.Fatalf("Couldn't get expected TaskRun %s: %s", taskRunName, err)
+			}
 
-	if !taskRun.Status.GetCondition(apis.ConditionSucceeded).IsTrue() {
-		t.Fatalf("Expected TaskRun %s to have succeeded but Status is %v", taskRunName, taskRun.Status)
-	}
+			if taskRun.Status.GetCondition(apis.ConditionSucceeded).IsTrue() != test.shouldSucceed {
+				t.Fatalf("Expected TaskRun %s ConditionSucceeded to be %t, but Status is %v", taskRunName, test.shouldSucceed, taskRun.Status)
+			}
 
-	expectedResults := []v1.TaskRunResult{{
-		Name:  "result1",
-		Type:  "string",
-		Value: *v1.NewStructuredValues("123"),
-	}}
+			expectedResults := []v1.TaskRunResult{{
+				Name:  "result1",
+				Type:  "string",
+				Value: *v1.NewStructuredValues("123"),
+			}}
 
-	if d := cmp.Diff(expectedResults, taskRun.Status.Results); d != "" {
-		t.Errorf("Got unexpected results %s", diff.PrintWantGot(d))
+			if d := cmp.Diff(expectedResults, taskRun.Status.Results); d != "" {
+				t.Errorf("Got unexpected results %s", diff.PrintWantGot(d))
+			}
+		})
 	}
 }
