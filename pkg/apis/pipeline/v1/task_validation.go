b95fb7f9a9f52432f488f43b9093d0d151874398
Stanislav Jakuschevskij <stas@two-giants.com>
2025-04-18 11:12:36 +0200

    refactor: implement Validatable for step
    Add `StepList` with a `Validate` method and implement a `Validate`
    method on `Step`.
    
    Move up check for duplicated Step names into `StepList.Validate` to be
    consistent with the check for duplicated Task names in
    `pipeline_validation.go`.
    
    Issue #8700.
    
    Signed-off-by: Stanislav Jakuschevskij <stas@two-giants.com>

--- pkg/apis/pipeline/v1/task_validation.go
+++ pkg/apis/pipeline/v1/task_validation.go
@@ -87,7 +87,7 @@ func (ts *TaskSpec) Validate(ctx context.Context) (errs *apis.FieldError) {
 		})
 	}
 
-	errs = errs.Also(validateSteps(ctx, mergedSteps).ViaField("steps"))
+	errs = errs.Also(StepList(mergedSteps).Validate(ctx).ViaField("steps"))
 	errs = errs.Also(SidecarList(ts.Sidecars).Validate(ctx).ViaField("sidecars"))
 	errs = errs.Also(ValidateParameterTypes(ctx, ts.Params).ViaField("params"))
 	errs = errs.Also(ValidateParameterVariables(ctx, ts.Steps, ts.Params))
@@ -213,11 +213,20 @@ func ValidateVolumes(volumes []corev1.Volume) (errs *apis.FieldError) {
 	return errs
 }
 
-func validateSteps(ctx context.Context, steps []Step) (errs *apis.FieldError) {
+// Validate implements apis.Validatable
+func (l StepList) Validate(ctx context.Context) (errs *apis.FieldError) {
 	// Task must not have duplicate step names.
 	names := sets.New[string]()
-	for idx, s := range steps {
-		errs = errs.Also(validateStep(ctx, s, names).ViaIndex(idx))
+	for idx, s := range l {
+		// names cannot be duplicated - checking that Step names are unique
+		if s.Name != "" {
+			if names.Has(s.Name) {
+				errs = errs.Also(apis.ErrMultipleOneOf("name").ViaIndex(idx))
+			}
+			names.Insert(s.Name)
+		}
+
+		errs = errs.Also(s.Validate(ctx).ViaIndex(idx))
 		if s.Results != nil {
 			errs = errs.Also(ValidateStepResultsVariables(ctx, s.Results, s.Script).ViaIndex(idx))
 			errs = errs.Also(ValidateStepResults(ctx, s.Results).ViaIndex(idx).ViaField("results"))
