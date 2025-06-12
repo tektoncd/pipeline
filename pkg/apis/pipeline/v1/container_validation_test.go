twoGiants/tekton-pipeline

refactor: move Step StepActionReference test
Now that `Step` implements the `Validatable` interface the tests for the
`Step` validation are moved from `task_validation_test.go` to
`container_validation_test.go`.

The following tests is moved and renamed:
- `TestTaskSpecStepActionReferenceValidate`
  - > `TestStepWithStepActionReferenceValidate`

Issue #8700.

Signed-off-by: Stanislav Jakuschevskij <stas@two-giants.com>

pkg/apis/pipeline/v1/container_validation_test.go
@@ -304,6 +304,43 @@ func TestStepValidateError(t *testing.T) {
 	}
 }
 
+func TestStepWithStepActionReferenceValidate(t *testing.T) {
+	tests := []struct {
+		name string
+		Step v1.Step
+	}{{
+		name: "valid stepAction ref",
+		Step: v1.Step{
+			Name: "mystep",
+			Ref: &v1.Ref{
+				Name: "stepAction",
+			},
+		},
+	}, {
+		name: "valid use of params with Ref",
+		Step: v1.Step{
+			Ref: &v1.Ref{
+				Name: "stepAction",
+			},
+			Params: v1.Params{{
+				Name: "param",
+			}},
+		},
+	}}
+	for _, st := range tests {
+		t.Run(st.name, func(t *testing.T) {
+			ctx := config.ToContext(context.Background(), &config.Config{
+				FeatureFlags: &config.FeatureFlags{
+					EnableStepActions: true,
+				},
+			})
+			if err := st.Step.Validate(ctx); err != nil {
+				t.Errorf("Step.Validate() = %v", err)
+			}
+		})
+	}
+}
+
 func TestStepValidateSuccessWithArtifactsRefFlagEnabled(t *testing.T) {
 	tests := []struct {
 		name string

tektoncd/pipeline

  - `TestStepWithStepActionReferenceValidate`

pkg/apis/pipeline/v1/container_validation_test.go
@@ -985,6 +985,43 @@ func TestStepValidateErrorWithArtifactsRef(t *testing.T) {
 	}
 }
 
+func TestStepWithStepActionReferenceValidate(t *testing.T) {
+	tests := []struct {
+		name string
+		Step v1.Step
+	}{{
+		name: "valid stepAction ref",
+		Step: v1.Step{
+			Name: "mystep",
+			Ref: &v1.Ref{
+				Name: "stepAction",
+			},
+		},
+	}, {
+		name: "valid use of params with Ref",
+		Step: v1.Step{
+			Ref: &v1.Ref{
+				Name: "stepAction",
+			},
+			Params: v1.Params{{
+				Name: "param",
+			}},
+		},
+	}}
+	for _, st := range tests {
+		t.Run(st.name, func(t *testing.T) {
+			ctx := config.ToContext(t.Context(), &config.Config{
+				FeatureFlags: &config.FeatureFlags{
+					EnableStepActions: true,
+				},
+			})
+			if err := st.Step.Validate(ctx); err != nil {
+				t.Errorf("Step.Validate() = %v", err)
+			}
+		})
+	}
+}
+
 func TestSidecarValidate(t *testing.T) {
 	tests := []struct {
 		name    string