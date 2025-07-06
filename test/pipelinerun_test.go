ee01b5a3172a9b44e056f08783cbbab1f303cac3
Stanislav Jakuschevskij <stas@two-giants.com>
2025-07-06 15:33:11 +0200

    refactor(SPR2): remove skipped e2e testcases
    Remove unused code and helper functions from PipelineRun e2e tests.
    
    Fix typos premption => preemption.
    
    Issues #8760, #7166.
    
    Signed-off-by: Stanislav Jakuschevskij <stas@two-giants.com>

--- test/pipelinerun_test.go
+++ test/pipelinerun_test.go
@@ -21,7 +21,6 @@ package test
 
 import (
 	"context"
-	"encoding/base64"
 	"fmt"
 	"strings"
 	"testing"
@@ -31,7 +30,6 @@ import (
 	v1 "github.com/tektoncd/pipeline/pkg/apis/pipeline/v1"
 	"github.com/tektoncd/pipeline/test/parse"
 	corev1 "k8s.io/api/core/v1"
-	k8sres "k8s.io/apimachinery/pkg/api/resource"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 	"knative.dev/pkg/apis"
@@ -201,97 +199,6 @@ func TestPipelineRun(t *testing.T) {
 		expectedTaskRuns: []string{"create-file-kritis", "create-fan-out-1", "create-fan-out-2", "check-fan-in"},
 		// 1 from PipelineRun and 4 from Tasks defined in pipelinerun
 		expectedNumberOfEvents: 5,
-	}, {
-		name: "service account propagation and pipeline param",
-		testSetup: func(ctx context.Context, t *testing.T, c *clients, namespace string, index int) *v1.Pipeline {
-			t.Helper()
-			t.Skip("build-crd-testing project got removed, the secret-sauce doesn't exist anymore, skipping")
-			if _, err := c.KubeClient.CoreV1().Secrets(namespace).Create(ctx, getPipelineRunSecret(index, namespace), metav1.CreateOptions{}); err != nil {
-				t.Fatalf("Failed to create secret `%s`: %s", getName(secretName, index), err)
-			}
-
-			if _, err := c.KubeClient.CoreV1().ServiceAccounts(namespace).Create(ctx, getPipelineRunServiceAccount(index, namespace), metav1.CreateOptions{}); err != nil {
-				t.Fatalf("Failed to create SA `%s`: %s", getName(saName, index), err)
-			}
-
-			task := parse.MustParseV1Task(t, fmt.Sprintf(`
-metadata:
-  name: %s
-  namespace: %s
-spec:
-  params:
-  - name: the.path
-    type: string
-  - name: the.dest
-    type: string
-  steps:
-  - name: config-docker
-    image: gcr.io/tekton-releases/dogfooding/skopeo:latest
-    command: ['skopeo']
-    args: ['copy', '$(params["the.path"])', '$(params["the.dest"])']
-`, helpers.ObjectNameForTest(t), namespace))
-			if _, err := c.V1TaskClient.Create(ctx, task, metav1.CreateOptions{}); err != nil {
-				t.Fatalf("Failed to create Task `%s`: %s", task.Name, err)
-			}
-			p := getHelloWorldPipelineWithSingularTask(t, namespace, task.Name)
-			if _, err := c.V1PipelineClient.Create(ctx, p, metav1.CreateOptions{}); err != nil {
-				t.Fatalf("Failed to create Pipeline `%s`: %s", p.Name, err)
-			}
-
-			return p
-		},
-		expectedTaskRuns: []string{task1Name},
-		// 1 from PipelineRun and 1 from Tasks defined in pipelinerun
-		expectedNumberOfEvents: 2,
-		pipelineRunFunc:        getHelloWorldPipelineRun,
-	}, {
-		name: "pipelinerun succeeds with LimitRange minimum in namespace",
-		testSetup: func(ctx context.Context, t *testing.T, c *clients, namespace string, index int) *v1.Pipeline {
-			t.Helper()
-			t.Skip("build-crd-testing project got removed, the secret-sauce doesn't exist anymore, skipping")
-			if _, err := c.KubeClient.CoreV1().LimitRanges(namespace).Create(ctx, getLimitRange("prlimitrange", namespace, "100m", "99Mi", "100m"), metav1.CreateOptions{}); err != nil {
-				t.Fatalf("Failed to create LimitRange `%s`: %s", "prlimitrange", err)
-			}
-
-			if _, err := c.KubeClient.CoreV1().Secrets(namespace).Create(ctx, getPipelineRunSecret(index, namespace), metav1.CreateOptions{}); err != nil {
-				t.Fatalf("Failed to create secret `%s`: %s", getName(secretName, index), err)
-			}
-
-			if _, err := c.KubeClient.CoreV1().ServiceAccounts(namespace).Create(ctx, getPipelineRunServiceAccount(index, namespace), metav1.CreateOptions{}); err != nil {
-				t.Fatalf("Failed to create SA `%s`: %s", getName(saName, index), err)
-			}
-
-			task := parse.MustParseV1Task(t, fmt.Sprintf(`
-metadata:
-  name: %s
-  namespace: %s
-spec:
-  params:
-  - name: the.path
-    type: string
-  - name: the.dest
-    type: string
-  steps:
-  - name: config-docker
-    image: gcr.io/tekton-releases/dogfooding/skopeo:latest
-    command: ['skopeo']
-    args: ['copy', '$(params["the.path"])', '$(params["the.dest"])']
-`, helpers.ObjectNameForTest(t), namespace))
-			if _, err := c.V1TaskClient.Create(ctx, task, metav1.CreateOptions{}); err != nil {
-				t.Fatalf("Failed to create Task `%s`: %s", fmt.Sprint("task", index), err)
-			}
-
-			p := getHelloWorldPipelineWithSingularTask(t, namespace, task.Name)
-			if _, err := c.V1PipelineClient.Create(ctx, p, metav1.CreateOptions{}); err != nil {
-				t.Fatalf("Failed to create Pipeline `%s`: %s", p.Name, err)
-			}
-
-			return p
-		},
-		expectedTaskRuns: []string{task1Name},
-		// 1 from PipelineRun and 1 from Tasks defined in pipelinerun
-		expectedNumberOfEvents: 2,
-		pipelineRunFunc:        getHelloWorldPipelineRun,
 	}}
 
 	for i, td := range tds {
@@ -364,7 +271,7 @@ spec:
 						collectedEvents += ", "
 					}
 				}
-				t.Fatalf("Expected %d number of successful events from pipelinerun and taskrun but got %d; list of receieved events : %#v", td.expectedNumberOfEvents, len(events), collectedEvents)
+				t.Fatalf("Expected %d number of successful events from pipelinerun and taskrun but got %d; list of received events : %#v", td.expectedNumberOfEvents, len(events), collectedEvents)
 			}
 
 			t.Logf("Successfully finished test %q", td.name)
@@ -390,30 +297,6 @@ spec:
     taskRef:
       name: %s
 `, namespace, task1Name, taskName))
-}
-
-func getHelloWorldPipelineWithSingularTask(t *testing.T, namespace string, taskName string) *v1.Pipeline {
-	t.Helper()
-	return parse.MustParseV1Pipeline(t, fmt.Sprintf(`
-metadata:
-  name: %s
-  namespace: %s
-spec:
-  params:
-  - name: the.path
-    type: string
-  - name: dest
-    type: string
-  tasks:
-  - name: %s
-    params:
-    - name: the.path
-      value: $(params["the.path"])
-    - name: dest
-      value: $(params.dest)
-    taskRef:
-      name: %s
-`, helpers.ObjectNameForTest(t), namespace, task1Name, taskName))
 }
 
 // TestPipelineRunRefDeleted tests that a running PipelineRun doesn't fail when the Pipeline
@@ -690,47 +573,6 @@ spec:
 `, helpers.ObjectNameForTest(t), namespace, pipelineName))
 }
 
-func getPipelineRunServiceAccount(suffix int, namespace string) *corev1.ServiceAccount {
-	return &corev1.ServiceAccount{
-		ObjectMeta: metav1.ObjectMeta{
-			Namespace: namespace,
-			Name:      getName(saName, suffix),
-		},
-		Secrets: []corev1.ObjectReference{{
-			Name: getName(secretName, suffix),
-		}},
-	}
-}
-
-func getPipelineRunSecret(suffix int, namespace string) *corev1.Secret {
-	// Generated by:
-	//   cat /tmp/key.json | base64 -w 0
-	// This service account is JUST a storage reader on gcr.io/build-crd-testing
-	encoedDockercred := "ewogICJ0eXBlIjogInNlcnZpY2VfYWNjb3VudCIsCiAgInByb2plY3RfaWQiOiAiYnVpbGQtY3JkLXRlc3RpbmciLAogICJwcml2YXRlX2tleV9pZCI6ICIwNTAyYTQxYTgxMmZiNjRjZTU2YTY4ZWM1ODMyYWIwYmExMWMxMWU2IiwKICAicHJpdmF0ZV9rZXkiOiAiLS0tLS1CRUdJTiBQUklWQVRFIEtFWS0tLS0tXG5NSUlFdlFJQkFEQU5CZ2txaGtpRzl3MEJBUUVGQUFTQ0JLY3dnZ1NqQWdFQUFvSUJBUUM5WDRFWU9BUmJ4UU04XG5EMnhYY2FaVGsrZ1k4ZWp1OTh0THFDUXFUckdNVzlSZVQyeE9ZNUF5Z2FsUFArcDd5WEVja3dCRC9IaE0wZ2xJXG43TVRMZGVlS1dyK3JBMUx3SFp5V0ZXN0gwT25mN3duWUhFSExXVW1jM0JDT1JFRHRIUlo3WnJQQmYxSFRBQS8zXG5Nblc1bFpIU045b2p6U1NGdzZBVnU2ajZheGJCSUlKNzU0THJnS2VBWXVyd2ZJUTJSTFR1MjAxazJJcUxZYmhiXG4zbVNWRzVSK3RiS3oxQ3ZNNTNuSENiN0NmdVZlV3NyQThrazd4SHJyTFFLTW1JOXYyc2dSdWd5TUF6d3ovNnpOXG5oNS9pTXh4Z2VxNVc4eGtWeDNKMm5ZOEpKZEhhZi9UNkFHc09ORW80M3B4ZWlRVmpuUmYvS24xMFRDYzJFc0lZXG5TNDlVc1o3QkFnTUJBQUVDZ2dFQUF1cGxkdWtDUVF1RDVVL2dhbUh0N0dnVzNBTVYxOGVxbkhuQ2EyamxhaCtTXG5BZVVHbmhnSmpOdkUrcE1GbFN2NXVmMnAySzRlZC9veEQ2K0NwOVpYRFJqZ3ZmdEl5cWpsemJ3dkZjZ3p3TnVEXG55Z1VrdXA3SGVjRHNEOFR0ZUFvYlQvVnB3cTZ6S01yQndDdk5rdnk2YlZsb0VqNXgzYlhzYXhlOTVETy95cHU2XG53MFc5N3p4d3dESlk2S1FjSVdNamhyR3h2d1g3bmlVQ2VNNGxlV0JEeUd0dzF6ZUpuNGhFYzZOM2FqUWFjWEtjXG4rNFFseGNpYW1ZcVFXYlBudHhXUWhoUXpjSFdMaTJsOWNGYlpENyt1SkxGNGlONnk4bVZOVTNLM0sxYlJZclNEXG5SVXAzYVVWQlhtRmcrWi8ycHVWTCttVTNqM0xMV1l5Qk9rZXZ1T21kZ1FLQmdRRGUzR0lRa3lXSVMxNFRkTU9TXG5CaUtCQ0R5OGg5NmVoTDBIa0RieU9rU3RQS2RGOXB1RXhaeGh5N29qSENJTTVGVnJwUk4yNXA0c0V6d0ZhYyt2XG5KSUZnRXZxN21YZm1YaVhJTmllUG9FUWFDbm54RHhXZ21yMEhVS0VtUzlvTWRnTGNHVStrQ1ZHTnN6N0FPdW0wXG5LcVkzczIyUTlsUTY3Rk95cWl1OFdGUTdRUUtCZ1FEWmlGaFRFWmtQRWNxWmpud0pwVEI1NlpXUDlLVHNsWlA3XG53VTRiemk2eSttZXlmM01KKzRMMlN5SGMzY3BTTWJqdE5PWkN0NDdiOTA4RlVtTFhVR05oY3d1WmpFUXhGZXkwXG5tNDFjUzVlNFA0OWI5bjZ5TEJqQnJCb3FzMldCYWwyZWdkaE5KU3NDV29pWlA4L1pUOGVnWHZoN2I5MWp6b0syXG5xMlBVbUE0RGdRS0JnQVdMMklqdkVJME95eDJTMTFjbi9lM1dKYVRQZ05QVEc5MDNVcGErcW56aE9JeCtNYXFoXG5QRjRXc3VBeTBBb2dHSndnTkpiTjhIdktVc0VUdkE1d3l5TjM5WE43dzBjaGFyRkwzN29zVStXT0F6RGpuamNzXG5BcTVPN0dQR21YdWI2RUJRQlBKaEpQMXd5NHYvSzFmSGcvRjQ3cTRmNDBMQUpPa2FZUkpENUh6QkFvR0JBTlVoXG5uSUJQSnFxNElNdlE2Y0M5ZzhCKzF4WURlYTkvWWsxdytTbVBHdndyRVh5M0dLeDRLN2xLcGJQejdtNFgzM3N4XG5zRVUvK1kyVlFtd1JhMXhRbS81M3JLN1YybDVKZi9ENDAwalJtNlpmU0FPdmdEVHJ0Wm5VR0pNcno5RTd1Tnc3XG5sZ1VIM0pyaXZ5Ri9meE1JOHFzelFid1hQMCt4bnlxQXhFQWdkdUtCQW9HQUlNK1BTTllXQ1pYeERwU0hJMThkXG5qS2tvQWJ3Mk1veXdRSWxrZXVBbjFkWEZhZDF6c1hRR2RUcm1YeXY3TlBQKzhHWEJrbkJMaTNjdnhUaWxKSVN5XG51Y05yQ01pcU5BU24vZHE3Y1dERlVBQmdqWDE2SkgyRE5GWi9sL1VWRjNOREFKalhDczFYN3lJSnlYQjZveC96XG5hU2xxbElNVjM1REJEN3F4Unl1S3Nnaz1cbi0tLS0tRU5EIFBSSVZBVEUgS0VZLS0tLS1cbiIsCiAgImNsaWVudF9lbWFpbCI6ICJwdWxsLXNlY3JldC10ZXN0aW5nQGJ1aWxkLWNyZC10ZXN0aW5nLmlhbS5nc2VydmljZWFjY291bnQuY29tIiwKICAiY2xpZW50X2lkIjogIjEwNzkzNTg2MjAzMzAyNTI1MTM1MiIsCiAgImF1dGhfdXJpIjogImh0dHBzOi8vYWNjb3VudHMuZ29vZ2xlLmNvbS9vL29hdXRoMi9hdXRoIiwKICAidG9rZW5fdXJpIjogImh0dHBzOi8vYWNjb3VudHMuZ29vZ2xlLmNvbS9vL29hdXRoMi90b2tlbiIsCiAgImF1dGhfcHJvdmlkZXJfeDUwOV9jZXJ0X3VybCI6ICJodHRwczovL3d3dy5nb29nbGVhcGlzLmNvbS9vYXV0aDIvdjEvY2VydHMiLAogICJjbGllbnRfeDUwOV9jZXJ0X3VybCI6ICJodHRwczovL3d3dy5nb29nbGVhcGlzLmNvbS9yb2JvdC92MS9tZXRhZGF0YS94NTA5L3B1bGwtc2VjcmV0LXRlc3RpbmclNDBidWlsZC1jcmQtdGVzdGluZy5pYW0uZ3NlcnZpY2VhY2NvdW50LmNvbSIKfQo="
-
-	decoded, err := base64.StdEncoding.DecodeString(encoedDockercred)
-	if err != nil {
-		return nil
-	}
-	return &corev1.Secret{
-		ObjectMeta: metav1.ObjectMeta{
-			Namespace: namespace,
-			Name:      getName(secretName, suffix),
-			Annotations: map[string]string{
-				"tekton.dev/docker-0": "https://us.gcr.io",
-				"tekton.dev/docker-1": "https://eu.gcr.io",
-				"tekton.dev/docker-2": "https://asia.gcr.io",
-				"tekton.dev/docker-3": "https://gcr.io",
-			},
-		},
-		Type: "kubernetes.io/basic-auth",
-		Data: map[string][]byte{
-			"username": []byte("_json_key"),
-			"password": decoded,
-		},
-	}
-}
-
 func getUpdatedStatusSpecPipelineRun(t *testing.T, _ int, namespace string, pipelineName string) *v1.PipelineRun {
 	t.Helper()
 	return parse.MustParseV1PipelineRun(t, fmt.Sprintf(`
@@ -744,38 +586,18 @@ spec:
   pipelineRef:
     name: %s
 `, namespace, pipelineName))
-	// `, helpers.ObjectNameForTest(t), namespace, pipelineName))
-}
-
-func getHelloWorldPipelineRun(t *testing.T, suffix int, namespace string, pipelineName string) *v1.PipelineRun {
-	t.Helper()
-	return parse.MustParseV1PipelineRun(t, fmt.Sprintf(`
-metadata:
-  labels:
-    hello-world-key: hello-world-value
-  name: %s
-  namespace: %s
-spec:
-  params:
-  - name: the.path
-    value: docker://gcr.io/build-crd-testing/secret-sauce
-  - name: dest
-    value: dir:///tmp/
-  pipelineRef:
-    name: %s
-  taskRunTemplate:
-    serviceAccountName: %s%d
-`, helpers.ObjectNameForTest(t), namespace, pipelineName, saName, suffix))
-}
-
-func getName(namespace string, suffix int) string {
-	return fmt.Sprintf("%s%d", namespace, suffix)
 }
 
 // collectMatchingEvents collects list of events under 5 seconds that match
 // 1. matchKinds which is a map of Kind of Object with name of objects
 // 2. reason which is the expected reason of event
-func collectMatchingEvents(ctx context.Context, kubeClient kubernetes.Interface, namespace string, kinds map[string][]string, reason string) ([]*corev1.Event, error) {
+func collectMatchingEvents(
+	ctx context.Context,
+	kubeClient kubernetes.Interface,
+	namespace string,
+	kinds map[string][]string,
+	reason string,
+) ([]*corev1.Event, error) {
 	var events []*corev1.Event
 
 	watchEvents, err := kubeClient.CoreV1().Events(namespace).Watch(ctx, metav1.ListOptions{})
@@ -848,7 +670,7 @@ func checkLabelPropagation(ctx context.Context, t *testing.T, c *clients, namesp
 	}
 	assertLabelsMatch(t, labels, tr.ObjectMeta.Labels)
 
-	// PodName is "" iff a retry happened and pod is deleted
+	// PodName is "" if a retry happened and pod is deleted
 	// This label is added to every Pod by the TaskRun controller
 	if tr.Status.PodName != "" {
 		// Check label propagation to Pods.
@@ -929,24 +751,6 @@ func assertAnnotationsMatch(t *testing.T, expectedAnnotations, actualAnnotations
 		if actualVal := actualAnnotations[key]; actualVal != expectedVal {
 			t.Errorf("Expected annotations containing %s=%s but annotations were %v", key, expectedVal, actualAnnotations)
 		}
-	}
-}
-
-func getLimitRange(name, namespace, resourceCPU, resourceMemory, resourceEphemeralStorage string) *corev1.LimitRange {
-	return &corev1.LimitRange{
-		ObjectMeta: metav1.ObjectMeta{Name: name, Namespace: namespace},
-		Spec: corev1.LimitRangeSpec{
-			Limits: []corev1.LimitRangeItem{
-				{
-					Type: corev1.LimitTypeContainer,
-					Min: corev1.ResourceList{
-						corev1.ResourceCPU:              k8sres.MustParse(resourceCPU),
-						corev1.ResourceMemory:           k8sres.MustParse(resourceMemory),
-						corev1.ResourceEphemeralStorage: k8sres.MustParse(resourceEphemeralStorage),
-					},
-				},
-			},
-		},
 	}
 }
 
