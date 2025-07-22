5e69792f971b02c1f1c0a04adbc9d3848ddaeb1e
Brian Cook <bcook@redhat.com>
2025-07-22 13:58:01 -0400

    feat: implement caching for bundle, git and cluster resolvers
    This commit adds caching for bundle and git resolvers to reduce
    chances of being rate limited by registries and git forges.
    
    - Add cache interface and in-memory implementation
    - Add cache configuration options to docs
    - Add a simple 'always on or off' option for cluster resolver
    - Add documentation for cache configuration
    
    Signed-off-by: Brian Cook <bcook@redhat.com>

---
+++ pkg/remoteresolution/cache/annotated_resource.go
@@ -0,0 +1,80 @@
+/*
+Copyright 2024 The Tekton Authors
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package cache
+
+import (
+	"time"
+
+	v1 "github.com/tektoncd/pipeline/pkg/apis/pipeline/v1"
+	resolutionframework "github.com/tektoncd/pipeline/pkg/resolution/resolver/framework"
+)
+
+const (
+	// CacheAnnotationKey is the annotation key indicating if a resource was cached
+	CacheAnnotationKey = "resolution.tekton.dev/cached"
+	// CacheTimestampKey is the annotation key for when the resource was cached
+	CacheTimestampKey = "resolution.tekton.dev/cache-timestamp"
+	// CacheResolverTypeKey is the annotation key for the resolver type that cached it
+	CacheResolverTypeKey = "resolution.tekton.dev/cache-resolver-type"
+	// CacheOperationKey is the annotation key for the cache operation type
+	CacheOperationKey = "resolution.tekton.dev/cache-operation"
+	// CacheValueTrue is the value used for cache annotations
+	CacheValueTrue = "true"
+	// CacheOperationStore is the value for cache store operations
+	CacheOperationStore = "store"
+	// CacheOperationRetrieve is the value for cache retrieve operations
+	CacheOperationRetrieve = "retrieve"
+)
+
+// AnnotatedResource wraps a ResolvedResource with cache annotations
+type AnnotatedResource struct {
+	resource    resolutionframework.ResolvedResource
+	annotations map[string]string
+}
+
+// NewAnnotatedResource creates a new AnnotatedResource with cache annotations
+func NewAnnotatedResource(resource resolutionframework.ResolvedResource, resolverType, operation string) *AnnotatedResource {
+	annotations := resource.Annotations()
+	if annotations == nil {
+		annotations = make(map[string]string)
+	}
+
+	annotations[CacheAnnotationKey] = CacheValueTrue
+	annotations[CacheTimestampKey] = time.Now().Format(time.RFC3339)
+	annotations[CacheResolverTypeKey] = resolverType
+	annotations[CacheOperationKey] = operation
+
+	return &AnnotatedResource{
+		resource:    resource,
+		annotations: annotations,
+	}
+}
+
+// Data returns the bytes of the resource
+func (a *AnnotatedResource) Data() []byte {
+	return a.resource.Data()
+}
+
+// Annotations returns the annotations with cache metadata
+func (a *AnnotatedResource) Annotations() map[string]string {
+	return a.annotations
+}
+
+// RefSource returns the source reference of the remote data
+func (a *AnnotatedResource) RefSource() *v1.RefSource {
+	return a.resource.RefSource()
+}
--- pkg/remoteresolution/resolver/cluster/resolver.go
+++ pkg/remoteresolution/resolver/cluster/resolver.go
@@ -18,14 +18,16 @@ package cluster
 
 import (
 	"context"
-	"errors"
 
+	pipelinev1 "github.com/tektoncd/pipeline/pkg/apis/pipeline/v1"
 	"github.com/tektoncd/pipeline/pkg/apis/resolution/v1beta1"
-	clientset "github.com/tektoncd/pipeline/pkg/client/clientset/versioned"
+	"github.com/tektoncd/pipeline/pkg/client/clientset/versioned"
 	pipelineclient "github.com/tektoncd/pipeline/pkg/client/injection/client"
+	"github.com/tektoncd/pipeline/pkg/remoteresolution/cache"
+	"github.com/tektoncd/pipeline/pkg/remoteresolution/cache/injection"
 	"github.com/tektoncd/pipeline/pkg/remoteresolution/resolver/framework"
 	resolutioncommon "github.com/tektoncd/pipeline/pkg/resolution/common"
-	"github.com/tektoncd/pipeline/pkg/resolution/resolver/cluster"
+	clusterresolution "github.com/tektoncd/pipeline/pkg/resolution/resolver/cluster"
 	resolutionframework "github.com/tektoncd/pipeline/pkg/resolution/resolver/framework"
 )
 
@@ -38,59 +40,121 @@ const (
 	// associated with
 	ClusterResolverName string = "Cluster"
 
-	configMapName = "cluster-resolver-config"
+	// Legacy cache constants for backward compatibility with tests
+	CacheModeAlways = framework.CacheModeAlways
+	CacheModeNever  = framework.CacheModeNever
+	CacheModeAuto   = framework.CacheModeAuto
+	CacheParam      = framework.CacheParam
 )
 
-var _ framework.Resolver = &Resolver{}
-
-// ResolverV2 implements a framework.Resolver that can fetch resources from other namespaces.
+// Resolver implements a framework.Resolver that can fetch resources from the same cluster.
 type Resolver struct {
-	pipelineClientSet clientset.Interface
+	pipelineClientSet versioned.Interface
 }
 
-// Initialize performs any setup required by the cluster resolver.
+// Ensure Resolver implements CacheAwareResolver
+var _ framework.CacheAwareResolver = (*Resolver)(nil)
+
+// Initialize sets up any dependencies needed by the Resolver. None atm.
 func (r *Resolver) Initialize(ctx context.Context) error {
 	r.pipelineClientSet = pipelineclient.Get(ctx)
 	return nil
 }
 
-// GetName returns the string name that the cluster resolver should be
-// associated with.
-func (r *Resolver) GetName(_ context.Context) string {
+// GetName returns a string name to refer to this Resolver by.
+func (r *Resolver) GetName(ctx context.Context) string {
 	return ClusterResolverName
 }
 
-// GetSelector returns the labels that resource requests are required to have for
-// the cluster resolver to process them.
-func (r *Resolver) GetSelector(_ context.Context) map[string]string {
+// GetSelector returns a map of labels to match against tasks requesting
+// resolution from this Resolver.
+func (r *Resolver) GetSelector(ctx context.Context) map[string]string {
 	return map[string]string{
 		resolutioncommon.LabelKeyResolverType: LabelValueClusterResolverType,
 	}
 }
 
-// Validate returns an error if the given parameter map is not
-// valid for a resource request targeting the cluster resolver.
+// Validate ensures parameters from a request are as expected.
 func (r *Resolver) Validate(ctx context.Context, req *v1beta1.ResolutionRequestSpec) error {
-	if len(req.Params) > 0 {
-		return cluster.ValidateParams(ctx, req.Params)
-	}
-	// Remove this error once validate url has been implemented.
-	return errors.New("cannot validate request. the Validate method has not been implemented.")
+	return clusterresolution.ValidateParams(ctx, req.Params)
 }
 
-// Resolve performs the work of fetching a resource from a namespace with the given
-// resolution spec.
+// IsImmutable implements CacheAwareResolver.IsImmutable
+// Returns false because cluster resources don't have immutable references
+func (r *Resolver) IsImmutable(ctx context.Context, req *v1beta1.ResolutionRequestSpec) bool {
+	// Cluster resources (Tasks, Pipelines, etc.) don't have immutable references
+	// like Git commit hashes or bundle digests, so we always return false
+	return false
+}
+
+// Resolve uses the given params to resolve the requested file or resource.
 func (r *Resolver) Resolve(ctx context.Context, req *v1beta1.ResolutionRequestSpec) (resolutionframework.ResolvedResource, error) {
-	if len(req.Params) > 0 {
-		return cluster.ResolveFromParams(ctx, req.Params, r.pipelineClientSet)
+	// Determine if we should use caching using framework logic
+	systemDefault := framework.GetSystemDefaultCacheMode("cluster")
+	useCache := framework.ShouldUseCache(ctx, r, req, systemDefault)
+
+	if useCache {
+		// Get cache instance
+		cacheInstance := injection.Get(ctx)
+		cacheKey, err := cache.GenerateCacheKey(LabelValueClusterResolverType, req.Params)
+		if err != nil {
+			return nil, err
+		}
+
+		// Check cache first
+		if cached, ok := cacheInstance.Get(cacheKey); ok {
+			if resource, ok := cached.(resolutionframework.ResolvedResource); ok {
+				return cache.NewAnnotatedResource(resource, LabelValueClusterResolverType, cache.CacheOperationRetrieve), nil
+			}
+		}
 	}
-	// Remove this error once resolution of url has been implemented.
-	return nil, errors.New("the Resolve method has not been implemented.")
+
+	// If not caching or cache miss, resolve from params
+	resource, err := clusterresolution.ResolveFromParams(ctx, req.Params, r.pipelineClientSet)
+	if err != nil {
+		return nil, err
+	}
+
+	// Cache the result if caching is enabled
+	if useCache {
+		cacheInstance := injection.Get(ctx)
+		cacheKey, err := cache.GenerateCacheKey(LabelValueClusterResolverType, req.Params)
+		if err == nil {
+			// Store annotated resource with store operation
+			annotatedResource := cache.NewAnnotatedResource(resource, LabelValueClusterResolverType, cache.CacheOperationStore)
+			cacheInstance.Add(cacheKey, annotatedResource)
+			// Return annotated resource to indicate it was stored in cache
+			return annotatedResource, nil
+		}
+	}
+
+	return resource, nil
 }
 
 var _ resolutionframework.ConfigWatcher = &Resolver{}
 
 // GetConfigName returns the name of the cluster resolver's configmap.
 func (r *Resolver) GetConfigName(context.Context) string {
-	return configMapName
+	return "cluster-resolver-config"
+}
+
+// ShouldUseCache is a legacy function for backward compatibility with existing tests.
+// It converts the old-style params map to the new framework API.
+func ShouldUseCache(ctx context.Context, params map[string]string, checksum []byte) bool {
+	// Convert params map to ResolutionRequestSpec
+	var reqParams []pipelinev1.Param
+	for key, value := range params {
+		reqParams = append(reqParams, pipelinev1.Param{
+			Name:  key,
+			Value: pipelinev1.ParamValue{StringVal: value},
+		})
+	}
+
+	req := &v1beta1.ResolutionRequestSpec{
+		Params: reqParams,
+	}
+
+	resolver := &Resolver{}
+	systemDefault := framework.GetSystemDefaultCacheMode("cluster")
+	return framework.ShouldUseCache(ctx, resolver, req, systemDefault)
 }
