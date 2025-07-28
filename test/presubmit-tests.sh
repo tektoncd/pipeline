ed6506bff290b2fa2410ff4bc6a388b1a4c2363a
Vincent Demeester <vdemeest@redhat.com>
2025-07-28 15:49:54 +0200

    Use tini-git image for the resolvers deployment
    And make sure the entrypoint (command) is tini.
    
    Signed-off-by: Vincent Demeester <vdemeest@redhat.com>

--- test/presubmit-tests.sh
+++ test/presubmit-tests.sh
@@ -55,7 +55,7 @@ function ko_resolve() {
       github.com/tektoncd/pipeline/cmd/nop: ghcr.io/tektoncd/pipeline/github.com/tektoncd/pipeline/combined-base-image:latest
       github.com/tektoncd/pipeline/cmd/workingdirinit: ghcr.io/tektoncd/pipeline/github.com/tektoncd/pipeline/combined-base-image:latest
 
-      github.com/tektoncd/pipeline/cmd/resolvers: cgr.dev/chainguard/git@sha256:566235a8ef752f37d285042ee05fc37dbb04293e50f116a231984080fb835693
+      github.com/tektoncd/pipeline/cmd/resolvers: ghcr.io/tektoncd/plumbing/tini-git@sha256:3d56865f83236872b0986c6c0716f5de56a1ca8eb3a86f37a46ce6202a6d4a66
 EOF
 
   KO_DOCKER_REPO=example.com ko resolve -l 'app.kubernetes.io/component!=resolvers' --platform=all --push=false -R -f config 1>/dev/null
