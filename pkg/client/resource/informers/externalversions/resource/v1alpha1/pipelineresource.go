733c01ae0322aa37fadc5d748ff14677a8a570df
Khurram Baig <kbaig@redhat.com>
2025-12-18 20:33:29 +0530

    Update generated code and CRDs
    Run hack/update-codegen.sh to update generated client code, deepcopy functions, and CRD manifests. This aligns the generated code with recent dependency updates, including changes to finalizer management and context usage in informers.

--- pkg/client/resource/informers/externalversions/resource/v1alpha1/pipelineresource.go
+++ pkg/client/resource/informers/externalversions/resource/v1alpha1/pipelineresource.go
@@ -62,13 +62,25 @@ func NewFilteredPipelineResourceInformer(client versioned.Interface, namespace s
 				if tweakListOptions != nil {
 					tweakListOptions(&options)
 				}
-				return client.TektonV1alpha1().PipelineResources(namespace).List(context.TODO(), options)
+				return client.TektonV1alpha1().PipelineResources(namespace).List(context.Background(), options)
 			},
 			WatchFunc: func(options v1.ListOptions) (watch.Interface, error) {
 				if tweakListOptions != nil {
 					tweakListOptions(&options)
 				}
-				return client.TektonV1alpha1().PipelineResources(namespace).Watch(context.TODO(), options)
+				return client.TektonV1alpha1().PipelineResources(namespace).Watch(context.Background(), options)
+			},
+			ListWithContextFunc: func(ctx context.Context, options v1.ListOptions) (runtime.Object, error) {
+				if tweakListOptions != nil {
+					tweakListOptions(&options)
+				}
+				return client.TektonV1alpha1().PipelineResources(namespace).List(ctx, options)
+			},
+			WatchFuncWithContext: func(ctx context.Context, options v1.ListOptions) (watch.Interface, error) {
+				if tweakListOptions != nil {
+					tweakListOptions(&options)
+				}
+				return client.TektonV1alpha1().PipelineResources(namespace).Watch(ctx, options)
 			},
 		},
 		&apisresourcev1alpha1.PipelineResource{},
