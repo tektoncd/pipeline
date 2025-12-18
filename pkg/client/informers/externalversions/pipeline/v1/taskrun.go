c57ea534021bd56a02c97185090a347a8b76b7e5
Khurram Baig <kbaig@redhat.com>
2025-12-18 20:33:29 +0530

    Update generated code and CRDs
    Run hack/update-codegen.sh to update generated client code, deepcopy functions, and CRD manifests. This aligns the generated code with recent dependency updates, including changes to finalizer management and context usage in informers.

--- pkg/client/informers/externalversions/pipeline/v1/taskrun.go
+++ pkg/client/informers/externalversions/pipeline/v1/taskrun.go
@@ -62,13 +62,25 @@ func NewFilteredTaskRunInformer(client versioned.Interface, namespace string, re
 				if tweakListOptions != nil {
 					tweakListOptions(&options)
 				}
-				return client.TektonV1().TaskRuns(namespace).List(context.TODO(), options)
+				return client.TektonV1().TaskRuns(namespace).List(context.Background(), options)
 			},
 			WatchFunc: func(options metav1.ListOptions) (watch.Interface, error) {
 				if tweakListOptions != nil {
 					tweakListOptions(&options)
 				}
-				return client.TektonV1().TaskRuns(namespace).Watch(context.TODO(), options)
+				return client.TektonV1().TaskRuns(namespace).Watch(context.Background(), options)
+			},
+			ListWithContextFunc: func(ctx context.Context, options metav1.ListOptions) (runtime.Object, error) {
+				if tweakListOptions != nil {
+					tweakListOptions(&options)
+				}
+				return client.TektonV1().TaskRuns(namespace).List(ctx, options)
+			},
+			WatchFuncWithContext: func(ctx context.Context, options metav1.ListOptions) (watch.Interface, error) {
+				if tweakListOptions != nil {
+					tweakListOptions(&options)
+				}
+				return client.TektonV1().TaskRuns(namespace).Watch(ctx, options)
 			},
 		},
 		&apispipelinev1.TaskRun{},
--- pkg/client/informers/externalversions/pipeline/v1beta1/customrun.go
+++ pkg/client/informers/externalversions/pipeline/v1beta1/customrun.go
@@ -62,13 +62,25 @@ func NewFilteredCustomRunInformer(client versioned.Interface, namespace string, 
 				if tweakListOptions != nil {
 					tweakListOptions(&options)
 				}
-				return client.TektonV1beta1().CustomRuns(namespace).List(context.TODO(), options)
+				return client.TektonV1beta1().CustomRuns(namespace).List(context.Background(), options)
 			},
 			WatchFunc: func(options v1.ListOptions) (watch.Interface, error) {
 				if tweakListOptions != nil {
 					tweakListOptions(&options)
 				}
-				return client.TektonV1beta1().CustomRuns(namespace).Watch(context.TODO(), options)
+				return client.TektonV1beta1().CustomRuns(namespace).Watch(context.Background(), options)
+			},
+			ListWithContextFunc: func(ctx context.Context, options v1.ListOptions) (runtime.Object, error) {
+				if tweakListOptions != nil {
+					tweakListOptions(&options)
+				}
+				return client.TektonV1beta1().CustomRuns(namespace).List(ctx, options)
+			},
+			WatchFuncWithContext: func(ctx context.Context, options v1.ListOptions) (watch.Interface, error) {
+				if tweakListOptions != nil {
+					tweakListOptions(&options)
+				}
+				return client.TektonV1beta1().CustomRuns(namespace).Watch(ctx, options)
 			},
 		},
 		&apispipelinev1beta1.CustomRun{},
