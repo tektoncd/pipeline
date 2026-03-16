0c1a6dea9bf5ba6533fa6ddc45f9e5d1cefc1d1f
Andrew Thorp <athorp@redhat.com>
2026-03-16 15:29:02 -0400

    fix(resolvers): validate data is Tekton object in resolver framework
    Validate that the data resolved by resolvers is a Pipeline, Task, or
    StepAction object before writing the data to the ResolutionRequest.
    
    Fundamentally ResolutionRequests are only supposed to resolve Tekton
    types (specifically Pipelines, Tasks, and StepActions). However they may
    fetch any object or string and mark the ResolutionRequest as "successful".
    Currently the data validation is only done when the "successful"
    ResolutionRequest is being injected into the parent Run object.
    
    Minimally validating the object before updating the ResolutionRequest
    takes some of the validation burden off of the Pipelines controller and
    may catch errors faster and ensures sensitive, non-tekton objects cannot
    be written into ResolutionRequests, which are considered less restricted

--- pkg/remoteresolution/resolver/git/resolver_test.go
+++ pkg/remoteresolution/resolver/git/resolver_test.go
@@ -341,22 +341,26 @@ type params struct {
 }
 
 func TestResolve(t *testing.T) {
+	objTemplate := "{\"apiVersion\": \"tekton.dev/v1\", \"kind\": \"%s\", \"metadata\": {\"name\": \"%s\"}}"
+	mainContent := fmt.Sprintf(objTemplate, "Pipeline", "released content in main branch and in tag v1")
+	oldBranchContent := fmt.Sprintf(objTemplate, "Pipeline", "oldcontent in test branch")
+	newBranchContent := fmt.Sprintf(objTemplate, "Pipeline", "new content in test branch")
 	// local repo set up for anonymous cloning
 	// ----
 	commits := []commitForRepo{{
 		Dir:      "foo/",
 		Filename: "old",
-		Content:  "old content in test branch",
+		Content:  oldBranchContent,
 		Branch:   "test-branch",
 	}, {
 		Dir:      "foo/",
 		Filename: "new",
-		Content:  "new content in test branch",
+		Content:  newBranchContent,
 		Branch:   "test-branch",
 	}, {
 		Dir:      "./",
 		Filename: "released",
-		Content:  "released content in main branch and in tag v1",
+		Content:  mainContent,
 		Tag:      "v1",
 	}}
 
@@ -420,7 +424,7 @@ func TestResolve(t *testing.T) {
 			url:        anonFakeRepoURL,
 		},
 		expectedCommitSHA: commitSHAsInAnonRepo[2],
-		expectedStatus:    resolution.CreateResolutionRequestStatusWithData([]byte("released content in main branch and in tag v1")),
+		expectedStatus:    resolution.CreateResolutionRequestStatusWithData([]byte(mainContent)),
 	}, {
 		name: "clone: revision is tag name",
 		args: &params{
@@ -429,7 +433,7 @@ func TestResolve(t *testing.T) {
 			url:        anonFakeRepoURL,
 		},
 		expectedCommitSHA: commitSHAsInAnonRepo[2],
-		expectedStatus:    resolution.CreateResolutionRequestStatusWithData([]byte("released content in main branch and in tag v1")),
+		expectedStatus:    resolution.CreateResolutionRequestStatusWithData([]byte(mainContent)),
 	}, {
 		name: "clone: revision is the full tag name i.e. refs/tags/v1",
 		args: &params{
@@ -438,7 +442,7 @@ func TestResolve(t *testing.T) {
 			url:        anonFakeRepoURL,
 		},
 		expectedCommitSHA: commitSHAsInAnonRepo[2],
-		expectedStatus:    resolution.CreateResolutionRequestStatusWithData([]byte("released content in main branch and in tag v1")),
+		expectedStatus:    resolution.CreateResolutionRequestStatusWithData([]byte(mainContent)),
 	}, {
 		name: "clone: revision is a branch name",
 		args: &params{
@@ -447,7 +451,7 @@ func TestResolve(t *testing.T) {
 			url:        anonFakeRepoURL,
 		},
 		expectedCommitSHA: commitSHAsInAnonRepo[1],
-		expectedStatus:    resolution.CreateResolutionRequestStatusWithData([]byte("new content in test branch")),
+		expectedStatus:    resolution.CreateResolutionRequestStatusWithData([]byte(newBranchContent)),
 	}, {
 		name: "clone: revision is a specific commit sha",
 		args: &params{
@@ -456,7 +460,7 @@ func TestResolve(t *testing.T) {
 			url:        anonFakeRepoURL,
 		},
 		expectedCommitSHA: commitSHAsInAnonRepo[0],
-		expectedStatus:    resolution.CreateResolutionRequestStatusWithData([]byte("old content in test branch")),
+		expectedStatus:    resolution.CreateResolutionRequestStatusWithData([]byte(oldBranchContent)),
 	}, {
 		name: "clone: file does not exist",
 		args: &params{
