0b66aa5a7c588a32ccf501d9fd80172fcfc0203a
aniruddhajadhav7 <aniruddha.jadhav7@icloud.com>
2026-04-27 21:23:09 +0530

    feat(tracing): add spans to TaskRun notifications controller
    Adds OpenTelemetry spans across the TaskRun notification event emission
    path: ReconcileKind, ReconcileRunObject, and EmitCloudEvents.
    
    Fixes #9781

--- pkg/reconciler/notifications/taskrun/reconciler.go
+++ pkg/reconciler/notifications/taskrun/reconciler.go
@@ -20,17 +20,31 @@ import (
 	"context"
 
 	bc "github.com/allegro/bigcache/v3"
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/codes"
+	"go.opentelemetry.io/otel/trace"
+
 	v1 "github.com/tektoncd/pipeline/pkg/apis/pipeline/v1"
 	taskrunreconciler "github.com/tektoncd/pipeline/pkg/client/injection/reconciler/pipeline/v1/taskrun"
 	"github.com/tektoncd/pipeline/pkg/reconciler/events/cloudevent"
 	"github.com/tektoncd/pipeline/pkg/reconciler/notifications"
+	"knative.dev/pkg/logging"
 	pkgreconciler "knative.dev/pkg/reconciler"
+)
+
+const (
+	// TracerName is the name of the tracer for the taskrun notifications reconciler.
+	TracerName = "TaskRunNotificationsReconciler"
 )
 
 // Reconciler implements controller.Reconciler for TaskRun resources.
 type Reconciler struct {
 	cloudEventClient cloudevent.CEClient
 	cacheClient      *bc.BigCache
+
+	// tracerProvider is used to create spans for distributed tracing.
+	tracerProvider trace.TracerProvider
 }
 
 // NewReconciler creates a new Reconciler with the given clients.
@@ -38,21 +52,44 @@ func NewReconciler(ceClient cloudevent.CEClient, cacheClient *bc.BigCache) *Reco
 	return &Reconciler{
 		cloudEventClient: ceClient,
 		cacheClient:      cacheClient,
+		tracerProvider:   otel.GetTracerProvider(),
 	}
 }
 
-func (c *Reconciler) GetCloudEventsClient() cloudevent.CEClient {
-	return c.cloudEventClient
+// GetCloudEventsClient returns the cloud event client.
+
+func (r *Reconciler) GetCloudEventsClient() cloudevent.CEClient {
+	return r.cloudEventClient
 }
 
-func (c *Reconciler) GetCacheClient() *bc.BigCache {
-	return c.cacheClient
+// GetCacheClient returns the cache client.
+func (r *Reconciler) GetCacheClient() *bc.BigCache {
+	return r.cacheClient
 }
 
 // Check that our Reconciler implements taskrunreconciler.Interface
 var _ taskrunreconciler.Interface = (*Reconciler)(nil)
 
-// ReconcileKind observes the resource conditions and triggers notifications accordingly.
-func (c *Reconciler) ReconcileKind(ctx context.Context, tr *v1.TaskRun) pkgreconciler.Event {
-	return notifications.ReconcileRunObject(ctx, c, tr)
+func (r *Reconciler) ReconcileKind(ctx context.Context, tr *v1.TaskRun) pkgreconciler.Event {
+
+	ctx, span := r.tracerProvider.Tracer(TracerName).Start(ctx, "ReconcileKind",
+		trace.WithAttributes(
+			attribute.String("taskrun.name", tr.Name),
+			attribute.String("taskrun.namespace", tr.Namespace),
+		),
+	)
+	defer span.End()
+
+	logger := logging.FromContext(ctx)
+	logger.Infof("Reconciling TaskRun notifications for %s/%s", tr.Namespace, tr.Name)
+
+	// We pass 'r' as it satisfies the EventClientsProvider interface.
+	err := notifications.ReconcileRunObject(ctx, r, tr)
+
+	if err != nil {
+		span.SetStatus(codes.Error, err.Error())
+		span.RecordError(err)
+	}
+
+	return err
 }
