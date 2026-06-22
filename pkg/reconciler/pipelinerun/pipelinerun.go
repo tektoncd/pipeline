ffc3f0522cab51b8ed997607a3a8ec0255539c31
Parameshwaran Krishnasamy <Parameshwaran.K@ibm.com>
2026-06-22 23:12:38 +0530

    fix(tracing): add missing namespace attribute to parent create spans Address Copilot review feedback: add namespace attribute to createChildPipelineRuns, createTaskRuns, and createCustomRuns spans for consistent queryability with their child span counterparts. Fixes #9785
    Signed-off-by: Parameshwaran Krishnasamy <Parameshwaran.K@ibm.com>

--- pkg/reconciler/pipelinerun/pipelinerun.go
+++ pkg/reconciler/pipelinerun/pipelinerun.go
@@ -1163,6 +1163,7 @@ func (c *Reconciler) createChildPipelineRuns(
 	defer span.End()
 	span.SetAttributes(
 		attribute.String("pipelinerun", pr.Name),
+		attribute.String("namespace", pr.Namespace),
 		attribute.String("pipelinetask", rpt.PipelineTask.Name),
 	)
 
@@ -1349,6 +1350,7 @@ func (c *Reconciler) createTaskRuns(ctx context.Context, rpt *resources.Resolved
 	defer span.End()
 	span.SetAttributes(
 		attribute.String("pipelinerun", pr.Name),
+		attribute.String("namespace", pr.Namespace),
 		attribute.String("pipelinetask", rpt.PipelineTask.Name),
 		attribute.Int("taskrun.count", len(rpt.TaskRunNames)),
 	)
@@ -1523,6 +1525,7 @@ func (c *Reconciler) createCustomRuns(ctx context.Context, rpt *resources.Resolv
 	defer span.End()
 	span.SetAttributes(
 		attribute.String("pipelinerun", pr.Name),
+		attribute.String("namespace", pr.Namespace),
 		attribute.String("pipelinetask", rpt.PipelineTask.Name),
 	)
 	var matrixCombinations []v1.Params
